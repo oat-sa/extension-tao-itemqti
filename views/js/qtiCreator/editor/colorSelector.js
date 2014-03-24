5c9ef0ddc7d9a9afc67679d2e3085d7ab59130ee
draber <draber@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-24 13:25:08 +0000

    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@19235 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ views/js/qtiCreator/editor/colorSelector.js
@@ -0,0 +1,39 @@
+define([
+    'jquery',
+    'farbtastic'
+], function ($, farbtastic) {
+    'use strict'
+
+    var colorSelector = function (selector) {
+//        var colorPicker = $(selector),
+//            target = selectBox.data('target'),
+//            $target = $(target),
+//            oldValues = {
+//                'background-color': $target.css('background-color'),
+//                color: $target.css('color')
+//            },
+//            propertySelector = colorPicker.parent().find('[data-role="color-picker-property"'),
+//            reset = function (property) {
+//                if (!oldValues[property]) {
+//                    return false;
+//                }
+//                styleEditor.apply(target, property, oldValues[property]);
+//            };
+//
+//        colorPicker.farbtastic(function () {
+//            styleEditor.apply(target, (propertySelector.val() || 'color'), this.color);
+//        });
+//
+//        return {
+//            resetBackgroundColor: function () {
+//                reset('background-color');
+//            },
+//            resetColor: function () {
+//                reset('color');
+//            }
+//        }
+    };
+
+    return colorSelector;
+});
+
