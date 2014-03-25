7b1d54c9edc073d83ed72d0d813a45855a5b014e
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-03-25 15:03:17 +0000

    * minor refactoring: introducing $original and buildContainer in widget
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@19257 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/choices/simpleChoice/choiceInteraction/states/states.js
+++
@@ -1,7 +0,0 @@
-define([
-    'taoQtiItem/qtiCreator/widgets/states/factory',
-    'taoQtiItem/qtiCreator/widgets/choices/states/states',
-    'taoQtiItem/qtiCreator/widgets/choices/simpleChoice/choiceInteraction/states/Choice'
-], function(factory, states){
-    return factory.createBundle(states, arguments);
-});
