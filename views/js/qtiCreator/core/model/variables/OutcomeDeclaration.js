aac63e21607d6bd1db63293b520e9bd627bc9a4c
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-04 21:11:51 +0000

    moved qtiCreator/core/model into qtiCreator/model
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@19449 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/core/model/variables/OutcomeDeclaration.js
+++
@@ -1,5 +0,0 @@
-define(['lodash', 'taoQtiItem/qtiCreator/core/model/mixin/editable', 'taoQtiItem/qtiItem/core/variables/OutcomeDeclaration'], function(_, editable, OutcomeDeclaration){
-    var methods = {};
-    _.extend(methods, editable);
-    return OutcomeDeclaration.extend(methods);
-});
