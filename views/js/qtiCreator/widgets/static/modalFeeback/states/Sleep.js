39a92626dc07035ec2af213506a5d6b684a520d3
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-14 11:36:27 +0000

    remove modalFeeback
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@19692 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/static/modalFeeback/states/Sleep.js
+++
@@ -1,5 +0,0 @@
-define(['taoQtiItem/qtiCreator/widgets/states/factory', 'taoQtiItem/qtiCreator/widgets/states/Sleep'], function(stateFactory, SleepState){
-    
-    return stateFactory.create(SleepState, function(){}, function(){});
-    
-});
