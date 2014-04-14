fa0015315a6223d40a1a1cc9f9b7278a24167a71
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-14 10:51:55 +0000

    modalFeedback added to commonRenderer
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@19687 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ views/js/qtiCommonRenderer/renderers/ModalFeedback.js
@@ -0,0 +1,26 @@
+define([
+    'lodash',
+    'tpl!taoQtiItem/qtiCommonRenderer/tpl/modalFeedback',
+    'taoQtiItem/qtiCommonRenderer/helpers/Helper',
+    'ui/modal'
+], function(_, tpl, Helper){
+
+    return {
+        qtiClass : 'modalFeedback',
+        template : tpl,
+        getContainer : Helper.getContainer,
+        render:function(modalFeedback, data){
+            
+            data = data || {};
+            
+            var $modal = $('#' + modalFeedback.getSerial()).modal();
+            
+            $modal.on('closed.modal', function(){
+               console.log('modal closesd'); 
+               if(_.isFunction(data.callback)){
+                   data.callback.call(this);
+               }
+            });
+        }
+    };
+});
---
+++ views/js/qtiCommonRenderer/tpl/modalFeedback.tpl
@@ -0,0 +1,4 @@
+<div id="{{serial}}" data-serial="{{serial}}" data-identifier="{{attributes.identifier}}" class="modal">
+    <h1 class="modal-title">{{attributes.title}}</h1>
+    <div class="modal-body">{{{body}}}</div>
+</div>
