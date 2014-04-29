b66eb87d8695efec22aaa38c6f37270570e11e00
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-04-29 09:23:23 +0000

    mathML: toggle mode, warning tooltip
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@19965 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/tpl/forms/static/math.tpl
+++ views/js/qtiCreator/tpl/forms/static/math.tpl
@@ -22,21 +22,18 @@
     <div class="tooltip-content">How the math expression should be edited.</div>
 </div>
 
-<div class="panel" data-role="latex">
+<div class="panel" data-role="latex" style="display:none;">
     <label for="">{{__ "Latex"}}</label>
     <span class="icon-help tooltipstered" data-tooltip="~ .tooltip-content:first" data-tooltip-theme="info"></span>
-    <div class="tooltip-content">The identifier of the choice. This identifier must not be used by any other choice or item variable</div>
+    <div class="tooltip-content">Edit math expression using LaTex, e.g. x = {-b \pm \sqrt{b^2-4ac} \over 2a}</div>
 
-    <input type="text" 
-           name="latex" 
-           value="{{latex}}" 
-           placeholder="x = {-b \pm \sqrt{b^2-4ac} \over 2a}" />
+    <input type="text" name="latex" value="{{latex}}"/>
 </div>
 
-<div class="panel" data-role="mathml">
+<div class="panel" data-role="mathml" style="display:none;">
     <label for="">{{__ "MathML"}}</label>
     <span class="icon-help tooltipstered" data-tooltip="~ .tooltip-content:first" data-tooltip-theme="info"></span>
-    <div class="tooltip-content">The identifier of the choice. This identifier must not be used by any other choice or item variable</div>
+    <div class="tooltip-content">Edit math expression using MathML</div>
 
     <textarea name="mathml">{{{mathml}}}</textarea>
     <div class="math-buffer" style="visibility:hidden;"></div>
--- views/js/qtiCreator/widgets/static/math/states/Active.js
+++ views/js/qtiCreator/widgets/static/math/states/Active.js
@@ -4,8 +4,9 @@ define([
     'taoQtiItem/qtiCreator/editor/MathEditor',
     'tpl!taoQtiItem/qtiCreator/tpl/forms/static/math',
     'taoQtiItem/qtiCreator/widgets/helpers/formElement',
-    'lodash'
-], function(stateFactory, Active, MathEditor, formTpl, formElement, _){
+    'lodash',
+    'i18n'
+], function(stateFactory, Active, MathEditor, formTpl, formElement, _, __){
 
     var _throttle = 300;
 
@@ -23,19 +24,44 @@ define([
         var _widget = this.widget,
             $form = _widget.$form,
             math = _widget.element,
-            mathML = math.mathML,
-            tex = math.getAnnotation('latex'),
-            display = math.attr('display') || 'inline';
+            mathML = math.mathML || '',
+            tex = math.getAnnotation('latex') || '',
+            display = math.attr('display') || 'inline',
+            editMode = 'latex';
+
+        if(!tex.trim() && mathML.trim()){
+            editMode = 'mathml';
+        }
 
         $form.html(formTpl({
             display : display,
-            editMode : 'latex',
+            editMode : editMode,
             latex : tex,
             mathml : mathML
         }));
 
-        var $math = $form.find('textarea[name=mathml]');
-        var $tex = $form.find('input[name=latex]');
+        //editMode
+        $form.find('select[name=editMode]').val(editMode);
+        $form.children('.panel[data-role="' + editMode + '"]').show();
+
+        //... init standard ui widget
+        formElement.initWidget($form);
+
+        this.initFormChangeListener();
+
+    };
+
+    MathActive.prototype.initFormChangeListener = function(){
+
+        var _widget = this.widget,
+            $form = _widget.$form,
+            math = _widget.element,
+            mathML = math.mathML,
+            tex = math.getAnnotation('latex'),
+            display = math.attr('display') || 'inline',
+            $math = $form.find('textarea[name=mathml]'),
+            $tex = $form.find('input[name=latex]');
+
 
         var mathEditor = new MathEditor({
             tex : tex,
@@ -44,9 +70,6 @@ define([
             buffer : $form.find('.math-buffer'),
             target : _widget.$original
         });
-
-        //... init standard ui widget
-        formElement.initWidget($form);
 
         //init data change callbacks
         formElement.initDataBinding($form, math, {
@@ -57,14 +80,16 @@ define([
                     m.removeAttr('display');
                 }
             },
-            editMode : function(){
-                //toggle form visibility
+            editMode : function(m, value){
+
+                _toggleMode($form, value);
+
+
             },
             latex : _.throttle(function(m, value){
 
-                mathEditor.setTex(value);
-                mathEditor.renderFromTex(function(){
-                    
+                mathEditor.setTex(value).renderFromTex(function(){
+
                     //saveTex
                     m.setAnnotation('latex', value);
 
@@ -75,20 +100,61 @@ define([
 
             }, _throttle),
             mathml : _.throttle(function(m, value){
-                
+
                 mathEditor.setMathML(value).renderFromMathML(function(){
-                    
+
                     //save mathML
                     m.setMathML(value);
-                    
+
                     //clear tex:
                     $tex.val('');
                     m.removeAnnotation('latex');
                 });
-                
+
             }, _throttle)
         });
+    };
 
+    var _toggleMode = function($form, mode){
+
+        var $mathPanel = $form.children('.panel[data-role="mathml"]'),
+            $texPanel = $form.children('.panel[data-role="latex"]'),
+            $tex = $form.find('input[name=latex]'),
+            $math = $form.find('textarea[name=mathml]');
+
+        //toggle form visibility
+        $mathPanel.hide();
+        $texPanel.hide();
+        if(mode === 'latex'){
+            $texPanel.show();
+        }else if(mode === 'mathml'){
+            $mathPanel.show();
+            if($tex.val()){
+                //show a warning here, stating that the content in TeX will be removed
+                if(!$math.hasClass('tooltipstered')){
+                    _createWarningTooltip($math);
+                }
+                $math.tooltipster('show');
+
+            }
+        }
+    };
+
+    var _createWarningTooltip = function($math){
+
+        var $content = $('<span>')
+            .html(__('No MathML conversion to LaTeX is currently available. If you editing MathML here, any LaTex code will be discarded.'));
+
+        $math.tooltipster({
+            theme : 'tao-warning-tooltip',
+            content : $content,
+            delay : 200,
+            trigger : 'custom'
+        });
+
+        $math.on('focus.mathwarning', function(){
+            $math.tooltipster('hide');
+        });
     };
 
     return MathActive;
