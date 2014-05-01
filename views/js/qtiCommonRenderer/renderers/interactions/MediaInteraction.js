3a47496ff374525a906e9149c4ee5fae2f08c1e5
mgeorgiev <mgeorgiev@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-01 08:48:54 +0000

    mediaInteraction renderer
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20004 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCommonRenderer/renderers/interactions/MediaInteraction.js
+++ views/js/qtiCommonRenderer/renderers/interactions/MediaInteraction.js
@@ -9,21 +9,174 @@ define([
     'taoQtiItem/qtiCommonRenderer/helpers/PciResponse',
     'taoQtiItem/qtiCommonRenderer/helpers/Helper',
     'mediaElement'
-], function($, _, __, tpl, pciResponse, Helper, MediaElementPlayer){
+], function($, _, __, tpl, pciResponse, Helper, MediaElementPlayer) {
+
+    var getMediaType = function(media) {
+        var type = '';
+        var mimetype = media.type;
+        if(mimetype !== ''){
+            if (mimetype.indexOf('youtube') != -1) {
+                type = 'video/youtube';
+            } else if(mimetype.indexOf('video') === 0){
+                type = 'video';
+            } else if(mimetype.indexOf('audio') === 0){
+                type = 'audio';
+            }
+        }
+        return type;
+    }
 
 
     /**
      * Init rendering, called after template injected into the DOM
      * All options are listed in the QTI v2.1 information model:
-     * http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10321
+     * http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10391
      * 
      * @param {object} interaction
      */
-    var render = function render(interaction){
+    var render = function render(interaction) {
 
         var $container = Helper.getContainer(interaction);
         var media = interaction.object.attributes;
 
+        
+        var mediaType = getMediaType(media);
+        var playFromPauseEvent = false;
+        var pauseFromClick = false;
+        
+        var mediaOptions = {
+            defaultVideoWidth: 480,
+            defaultVideoHeight: 270,
+            //videoWidth: -1,
+            //videoHeight: -1,
+            videoWidth: media.width,
+            videoHeight: media.height,
+            audioWidth: media.width ? media.width : 400,
+            audioHeight: 30,
+            //features: ['playpause', 'progress', 'current', 'duration', 'tracks', 'volume', 'fullscreen']
+            features: (mediaType=='audio') ? ['playpause', 'current', 'duration', 'volume'] : ['current', 'duration', 'volume'],
+            startVolume: 1,
+            loop: interaction.attributes.loop ? interaction.attributes.loop : false,
+            enableAutosize: true,
+            alwaysShowControls: true,
+            iPadUseNativeControls: false,
+            iPhoneUseNativeControls: false,
+            AndroidUseNativeControls: false,
+            alwaysShowHours: false,
+            //showTimecodeFrameCount: true,
+            //framesPerSecond: 25,
+            enableKeyboard: false,
+            pauseOtherPlayers: false,
+            //keyActions: []
+            
+            success: function(mediaElement, playerDom) {
+                var audioPlayPauseControls = $(playerDom).closest('div.mejs-container').find('.mejs-playpause-button');
+                
+                $(audioPlayPauseControls).on('click', function(event) {
+                    pauseFromClick = true;
+                    event.preventDefault();
+                    event.stopPropagation();
+                    return false;
+                });
+                
+                var bigPlayButtonLayerDetached = null;
+                var flashOverlayDiv = null;
+                
+                if (interaction.attributes.autostart && ((interaction.attributes.maxPlays==0) || $container.data('timesPlayed') < interaction.attributes.maxPlays) ) {
+                    if (mediaType == 'video/youtube') {
+                        mediaElement.addEventListener('canplay', function() {
+                            mediaElement.play();
+                        }, false);
+                    } else {
+                        mediaElement.play();
+                    }
+                }
+
+                
+                
+                mediaElement.addEventListener('ended', function(event) {
+                    // this event does not get fired on Chrome under Linux, this is fixed in Chrome version 35 which at this moment is in beta,
+                    //  there is a workaround by using the "timeupdate" event and the mediaElement.ended property,
+                    //  but it brings a whole lot more problems, also timeupdate is not fired when flash fallback is used
+                    $container.data('timesPlayed', $container.data('timesPlayed')+1);
+                    Helper.triggerResponseChangeEvent(interaction);
+                    if ((interaction.attributes.maxPlays==0) || $container.data('timesPlayed') < interaction.attributes.maxPlays) {
+                        if (mediaType=='audio') {
+                            //
+                        } else if (mediaType=='video' && mediaElement.pluginType!=='flash') {
+                            var PlayButtonPlaceholder = $(playerDom).closest('div.mejs-container').find('.mejs-layers');
+                            PlayButtonPlaceholder.append(bigPlayButtonLayerDetached);
+                        } else if (mediaType == 'video/youtube' || mediaElement.pluginType==='flash') {
+                            flashOverlayDiv.remove();
+                        }
+                    }
+                }, false);
+                
+                
+                mediaElement.addEventListener('play', function(event) {
+                    if (playFromPauseEvent == true) {
+                        playFromPauseEvent = false;
+                    } else {
+                        if ((interaction.attributes.maxPlays!=0) && $container.data('timesPlayed') >= interaction.attributes.maxPlays) {
+                            mediaElement.pause();
+                            mediaElement.setSrc('');
+                            if (mediaType == "video/youtube") {
+                                $(playerDom).empty();
+                            }
+                        } else {
+                            if (mediaType == 'audio') {
+                                //
+                            } else if (mediaType == 'video' && mediaElement.pluginType!=='flash') {
+                                bigPlayButtonLayerDetached = $(playerDom).closest('div.mejs-container').find('.mejs-overlay-play').detach();
+                            } else if(mediaType == 'video/youtube' || mediaElement.pluginType==='flash') {
+                                var controlsHeight = $(playerDom).closest('div.mejs-container').find('div.mejs-controls').outerHeight();
+                                $(playerDom).closest('div.mejs-container').find('.mejs-layers').append('<div class="flashOverlayDiv" style="background:#000; width: '+mediaOptions.videoWidth+'px; height: '+(mediaOptions.videoHeight-controlsHeight)+'px; z-iindex: 99; position:relative;"></div>');
+                                flashOverlayDiv = $(playerDom).closest('div.mejs-container').find('.mejs-layers').find('.flashOverlayDiv');
+                                flashOverlayDiv.css({'opacity': 0}); // need to have the background set to something and then set it to transparent with jquery because of... IE8 of course :)
+                            }
+                        }
+                    }
+                }, false);
+                
+                mediaElement.addEventListener('pause', function(event) {
+                    // there is a "pause" event fired at the end of a movie and we need to differentiate it from pause event caused by a click
+                    if (pauseFromClick == true) {
+                        playFromPauseEvent = true;
+                        pauseFromClick = false;
+                        mediaElement.play();
+                    }
+                });
+            }
+        };
+
+        
+        var meHtmlContainer = $container.children('.instruction-container').first();
+        
+        if (mediaType == 'video') {
+            var meTag = $('<video src="' + media.data + '" width="' + mediaOptions.videoWidth + 'px" height="' + mediaOptions.videoHeight + 'px"></video>').appendTo(meHtmlContainer);
+        } else if (mediaType == 'video/youtube') {
+            var meTag = $('<video width="' + mediaOptions.videoWidth + 'px" height="' + mediaOptions.videoHeight + 'px" preload="none"> '+
+                ' <source type="video/youtube" src="'+ media.data +'" /> ' +
+                '</video>').appendTo(meHtmlContainer);
+        } else if (mediaType == 'audio') {
+            var meTag = $('<audio src="' + media.data + '" width="' + mediaOptions.audioWidth + 'px" ></audio>').appendTo(meHtmlContainer);
+        }
+        
+        $(meTag).on('contextmenu', function(event) {
+            event.preventDefault();
+        });
+        
+        $(meTag).on('click', function(event) {
+            pauseFromClick = true;
+            event.preventDefault();
+            event.stopPropagation();
+            return false;
+        });
+        
+        
+        $container.on('responseSet', function(e, interaction, response){
+            new MediaElementPlayer(meTag, mediaOptions);
+        });
     };
 
 
@@ -33,12 +186,11 @@ define([
      * @param {Object} interaction
      * @returns {Array} of points
      */
-    var _getRawResponse = function _getRawResponse(interaction){
-        var response = [];
-    
+    var _getRawResponse = function _getRawResponse(interaction) {
+        var response = [ Helper.getContainer(interaction).data('timesPlayed') ];
         return response;
     };
- 
+
     /**
      * Set the response to the rendered interaction. 
      * 
@@ -53,14 +205,16 @@ define([
      * @param {object} interaction
      * @param {object} response
      */
-    var setResponse = function(interaction, response){
-        var responseValues;
-        if(response && interaction.paper){
-
+    var setResponse = function(interaction, response) {
+        if (response) {
             try {
-                //try to unserualize tthe pci response
+                //try to unserialize the pci response
+                var responseValues;
                 responseValues = pciResponse.unserialize(response, interaction);
-            } catch(e){}
+                Helper.getContainer(interaction).data('timesPlayed', responseValues[0]);
+            } catch (e) {
+                // something went wrong
+            }
         }
     };
 
@@ -78,12 +232,13 @@ define([
      * @param {object} interaction
      * @param {object} response
      */
-    var resetResponse = function resetResponse(interaction){
+    var resetResponse = function resetResponse(interaction) {
+        //Helper.getContainer(interaction).data('timesPlayed', 0);
     };
 
 
     /**
-     i* Return the response of the rendered interaction
+     * Return the response of the rendered interaction
      * 
      * The response format follows the IMS PCI recommendation :
      * http://www.imsglobal.org/assessment/pciv1p0cf/imsPCIv1p0cf.html#_Toc353965343  
@@ -94,7 +249,7 @@ define([
      * @param {object} interaction
      * @returns {object}
      */
-    var getResponse = function(interaction){
+    var getResponse = function(interaction) {
         return  pciResponse.serialize(_getRawResponse(interaction), interaction);
     };
 
@@ -103,13 +258,12 @@ define([
      * @exports qtiCommonRenderer/renderers/interactions/mediaInteraction
      */
     return {
-        qtiClass : 'mediaInteraction',
-        template : tpl,
-        render : render,
-        getContainer : Helper.getContainer,
-        setResponse : setResponse,
-        getResponse : getResponse,
-        resetResponse : resetResponse
+        qtiClass: 'mediaInteraction',
+        template: tpl,
+        render: render,
+        getContainer: Helper.getContainer,
+        setResponse: setResponse,
+        getResponse: getResponse,
+        resetResponse: resetResponse
     };
 });
-
