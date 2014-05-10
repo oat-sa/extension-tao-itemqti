df9071dbce8898550e76f7ad4f614f97e93a64ff
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-10 00:12:33 +0000

    proto simpleParser
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20183 62dfeb86-a93f-4914-9d13-15c0a160985d

---
+++ views/js/qtiCreator/helper/simpleParser.js
@@ -0,0 +1,13 @@
+define('jquery', function(){
+    
+    var parser = {
+        parse : function(xmlStr){
+            var $xml = $(xmlStr);
+            
+            
+        }
+    };
+    
+    return parser;
+});
+
