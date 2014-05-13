11ddfda389f9fc12d6bf55ee55d1b774e198cbeb
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-13 07:46:30 +0000

    match creator : disabled checkboxes
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20212 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/choices/simpleAssociableChoice/Widget.js
+++ views/js/qtiCreator/widgets/choices/simpleAssociableChoice/Widget.js
@@ -11,8 +11,12 @@ define([
 
         Widget.initCreator.call(this);
         
+        //matchInteraction:
         //need to wrap the container + tlb around a div because in mathInteraction, tlb cannot be position absolutely in <th>
         this.$container.wrapInner($('<div>', {'class' : 'inner-wrapper'}));
+        
+        //prevent checkbox from being selectable
+        this.$container.closest('table.matrix').find('input[type=checkbox]:enabled').prop('disabled', 'disabled');
     };
 
     return SimpleAssociableChoiceWidget;
