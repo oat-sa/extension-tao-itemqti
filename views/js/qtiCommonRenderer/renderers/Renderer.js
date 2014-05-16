c06cb731724bfffaf58d99de7e3c210850ad1baa
bertrand <bertrand@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-16 14:51:51 +0000

    update css module names
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20348 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCommonRenderer/renderers/Renderer.js
+++ views/js/qtiCommonRenderer/renderers/Renderer.js
@@ -1,8 +1,8 @@
 define([
     'taoQtiItem/qtiRunner/core/Renderer',
     'taoQtiItem/qtiCommonRenderer/renderers/config',
-    'css!taoQtiItem_css/qti.css',
-    'css!tao_css/tao-main-style.css'
+    'css!taoQtiItem_css/qti',
+    'css!tao_css/tao-main-style'// not until fonts are separated
 ], function(Renderer, config){
     return Renderer.build(config.locations, config.name);
 });
