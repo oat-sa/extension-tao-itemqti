c0f073af9ddc6fe708b1b59b12b0544219ff0419
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-16 14:47:15 +0000

    create item with an default outcome variable "SCORE"
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20347 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/helper/itemLoader.js
+++ views/js/qtiCreator/helper/itemLoader.js
@@ -4,36 +4,41 @@ define([
     'taoQtiItem/qtiItem/core/Loader',
     'taoQtiItem/qtiCreator/model/Item',
     'taoQtiItem/qtiCreator/model/qtiClasses'
-], function($, helpers, Loader, Item, qtiClasses) {
+], function($, helpers, Loader, Item, qtiClasses){
 
-    var _generateIdentifier = function(uri) {
+    var _generateIdentifier = function(uri){
         var pos = uri.lastIndexOf('#');
-        return uri.substr(pos);
+        return uri.substr(pos + 1);
     };
 
     var creatorLoader = {
-        loadItem: function(config, callback) {
+        loadItem : function(config, callback){
 
-            if (config.uri) {
+            if(config.uri){
                 $.ajax({
-                    url: helpers._url('getItemData', 'QtiCreator', 'taoQtiItem'),
-                    dataType: 'json',
-                    data: {
-                        uri: config.uri
+                    url : helpers._url('getItemData', 'QtiCreator', 'taoQtiItem'),
+                    dataType : 'json',
+                    data : {
+                        uri : config.uri
                     }
-                }).done(function(data) {
+                }).done(function(data){
 
-                    if (data.itemData && data.itemData.qtiClass === 'assessmentItem') {
+                    if(data.itemData && data.itemData.qtiClass === 'assessmentItem'){
 
                         var loader = new Loader().setClassesLocation(qtiClasses),
                             itemData = data.itemData;
 
-                        loader.loadItemData(itemData, function(item) {
+                        loader.loadItemData(itemData, function(item){
                             callback(item, this.getLoadedClasses());
                         });
-                    } else {
+                    }else{
 
                         var item = new Item().id(_generateIdentifier(config.uri));
+                        item.createOutcomeDeclaration({
+                            identifier : 'SCORE',
+                            cardinality : 'single',
+                            baseType : 'float'
+                        });
                         item.createResponseProcessing();
                         callback(item);
                     }
