1b39c6c9017e56cb9cac2fb1aff81cad9a716a72
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-19 14:49:59 +0000

    fix  preview of old item
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20401 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/editor/preview.js
+++ views/js/qtiCreator/editor/preview.js
@@ -4,7 +4,7 @@ define([
     'taoQtiItem/qtiCreator/helper/commonRenderer',
     'json!taoQtiItem/qtiCreator/editor/resources/device-list.json',
     'select2'
-], function ($, __, commonRenderer, deviceList) {
+], function($, __, commonRenderer, deviceList){
     'use strict'
 
     var overlay,
@@ -18,9 +18,9 @@ define([
      *
      * @returns {boolean}
      */
-    var createWidget = function (item) {
+    var createWidget = function(item){
 
-        if (!!overlay && overlay.length) {
+        if(!!overlay && overlay.length){
             return false;
         }
 
@@ -31,9 +31,9 @@ define([
          * @param type
          * @returns {void|*}
          */
-        var createTool = function (tool, type) {
+        var createTool = function(tool, type){
             return $('<li>', {
-                class: (type ? 'lft ' + type + '-only' : 'lft')
+                class : (type ? 'lft ' + type + '-only' : 'lft')
             }).append(tool);
         };
 
@@ -42,13 +42,13 @@ define([
          *
          * @returns {*|HTMLElement}
          */
-        var createDeviceSelector = function (type) {
+        var createDeviceSelector = function(type){
             var device,
                 devices,
                 option,
                 select = $('<select>', {
-                    class: type + '-device-selector'
-                });
+                class : type + '-device-selector'
+            });
 
 
             /*
@@ -57,8 +57,8 @@ define([
              * This is not ideal and should be changed in the future.
              * I have http://en.wikipedia.org/wiki/List_of_displays_by_pixel_density in mind but we
              * will need to figure what criteria to apply when generating the list.
-            */
-            switch (type) {
+             */
+            switch(type){
                 case 'mobile':
                     devices = deviceList['tablets'];
                     break;
@@ -67,20 +67,20 @@ define([
                     break;
             }
 
-            for (device in devices) {
-                if (devices.hasOwnProperty(device)) {
+            for(device in devices){
+                if(devices.hasOwnProperty(device)){
                     option = $('<option>', {
-                        value: [devices[device].width, devices[device].height].join(','),
-                        text: devices[device].label
+                        value : [devices[device].width, devices[device].height].join(','),
+                        text : devices[device].label
                     });
-                    if (currPreviewType === devices[device].label) {
+                    if(currPreviewType === devices[device].label){
                         option.prop('selected', true);
                     }
                     select.append(option);
                 }
             }
 
-            select.on('change', function() {
+            select.on('change', function(){
                 var elem = $(this),
                     val = elem.val().split(','),
                     animationSettings,
@@ -88,29 +88,29 @@ define([
                     container = $('.' + type + '-preview-container');
 
 
-                while (i--) {
+                while(i--){
                     val[i] = parseFloat(val[i]);
                 }
 
-                if (type === 'mobile' && currOrientation === 'portrait') {
+                if(type === 'mobile' && currOrientation === 'portrait'){
                     animationSettings = {
-                        width: val[1],
-                        height: val[0]
+                        width : val[1],
+                        height : val[0]
                     };
                 }
-                else {
+                else{
                     animationSettings = {
-                        width: val[0],
-                        height: val[1]
+                        width : val[0],
+                        height : val[1]
                     };
                 }
 
-                if (animationSettings.width === container.width()
-                    && animationSettings.height === container.height()) {
+                if(animationSettings.width === container.width()
+                    && animationSettings.height === container.height()){
                     return false;
                 }
 
-                container.animate(animationSettings, function () {
+                container.animate(animationSettings, function(){
                     currPreviewType = type;
                 });
             });
@@ -123,46 +123,46 @@ define([
          *
          * @returns {*|HTMLElement}
          */
-        var createOrientationSelector = function (mobilePreviewContainer) {
+        var createOrientationSelector = function(mobilePreviewContainer){
 
             mobilePreviewContainer = mobilePreviewContainer.find('.preview-container');
 
             var select = $('<select>', {
-                    class: 'mobile-orientation-selector'
-                }),
-                orientations = {
-                    landscape: __('Landscape'),
-                    portrait: __('Portrait')
-                },
-                orientation,
+                class : 'mobile-orientation-selector'
+            }),
+            orientations = {
+                landscape : __('Landscape'),
+                portrait : __('Portrait')
+            },
+            orientation,
                 option;
-            for (orientation in orientations) {
-                if (orientations.hasOwnProperty(orientation)) {
+            for(orientation in orientations){
+                if(orientations.hasOwnProperty(orientation)){
                     option = $('<option>', {
-                        value: orientation,
-                        text: orientations[orientation]
+                        value : orientation,
+                        text : orientations[orientation]
                     });
-                    if (currOrientation === orientation) {
+                    if(currOrientation === orientation){
                         option.prop('selected', true);
                     }
                     select.append(option);
                 }
             }
-            select.on('change', function () {
+            select.on('change', function(){
                 var newOrientation = $(this).val(),
                     animationSettings,
                     mobilePreviewFrame = $('.preview-mobile-frame');
 
-                if (newOrientation === currOrientation) {
+                if(newOrientation === currOrientation){
                     return false;
                 }
 
                 animationSettings = {
-                    height: mobilePreviewContainer.width(),
-                    width: mobilePreviewContainer.height()
+                    height : mobilePreviewContainer.width(),
+                    width : mobilePreviewContainer.height()
                 };
 
-                mobilePreviewContainer.animate(animationSettings, function () {
+                mobilePreviewContainer.animate(animationSettings, function(){
                     mobilePreviewFrame.removeClass('mobile-preview-' + currOrientation)
                         .addClass('mobile-preview-' + newOrientation);
 
@@ -179,13 +179,13 @@ define([
          *
          * @returns {*|HTMLElement}
          */
-        var createFeedback = function () {
+        var createFeedback = function(){
             var feedback = $('<div>', {
-                class: 'tbl-cell'
+                class : 'tbl-cell'
             });
             feedback.append($('<div>', {
-                class: 'feedback-info small',
-                html: '<span class="icon-info"/>' + __('Final rendering may differ from this preview!')
+                class : 'feedback-info small',
+                html : '<span class="icon-info"/>' + __('Final rendering may differ from this preview!')
             }));
             return feedback;
         };
@@ -196,14 +196,14 @@ define([
          * @param type
          * @returns {*|HTMLElement}
          */
-        var createPreviewFrame = function (type) {
+        var createPreviewFrame = function(type){
             var previewFrame = $('<div>', {
-                class: type + '-preview-frame ' + type + '-only preview-outer-frame'
+                class : type + '-preview-frame ' + type + '-only preview-outer-frame'
             });
             var previewContainer = $('<div>', {
-                class: type + '-preview-container preview-container'
+                class : type + '-preview-container preview-container'
             });
-            if (type === 'mobile') {
+            if(type === 'mobile'){
                 previewFrame.addClass('mobile-preview-' + currOrientation);
             }
             previewFrame.append(previewContainer);
@@ -216,19 +216,19 @@ define([
          * @param type
          * @returns {*}
          */
-        var createToggler = function (type) {
+        var createToggler = function(type){
             var toggler,
                 toggleText,
                 toggleTarget,
                 icon;
 
             // view togglers
-            if (type === 'mobile') {
+            if(type === 'mobile'){
                 toggleText = __('Switch to desktop');
                 toggleTarget = 'desktop';
                 icon = 'icon-desktop-preview';
             }
-            else {
+            else{
                 toggleText = __('Switch to mobile');
                 toggleTarget = 'mobile';
                 icon = 'icon-mobile-preview';
@@ -236,15 +236,15 @@ define([
 
 
             toggler = $('<span>', {
-                class: 'btn-info toggle-view small',
-                html: '<span class="' + icon + '"/>' + toggleText
+                class : 'btn-info toggle-view small',
+                html : '<span class="' + icon + '"/>' + toggleText
             }).prop('target', toggleTarget);
 
-            toggler.on('click', function () {
+            toggler.on('click', function(){
                 var target = $(this).prop('target'),
                     newClass = 'preview-' + target,
                     oldClass = newClass === 'preview-desktop' ? 'preview-mobile' : 'preview-desktop',
-                    targetContainer =  $('.' + target + '-preview-container');
+                    targetContainer = $('.' + target + '-preview-container');
 
                 overlay.removeClass(oldClass).addClass(newClass);
                 currPreviewType = target;
@@ -263,12 +263,12 @@ define([
          *
          * @returns {*|HTMLElement}
          */
-        var createCloser = function () {
+        var createCloser = function(){
             var closer = $('<span>', {
-                class: 'btn-info small',
-                html: __('Close') + ' <span class="icon-close r"/>'
+                class : 'btn-info small',
+                html : __('Close') + ' <span class="icon-close r"/>'
             });
-            closer.on('click', function () {
+            closer.on('click', function(){
                 commonRenderer.setContext($('.item-editor-item'));
                 overlay.fadeOut();
             });
@@ -280,50 +280,50 @@ define([
          * @param type
          * @returns {*|HTMLElement}
          */
-        var createPreviewHeading = function (type) {
+        var createPreviewHeading = function(type){
 
             return $('<h1>', {
-                class: type + '-preview-heading ' + type + '-only preview-heading tbl-cell',
-                text: (type === 'mobile') ? __('Mobile Preview') : __('Desktop Preview')
+                class : type + '-preview-heading ' + type + '-only preview-heading tbl-cell',
+                text : (type === 'mobile') ? __('Mobile Preview') : __('Desktop Preview')
             });
         };
 
         /**
          * Build the whole shebang
          */
-        (function () {
+        (function(){
             var types = ['mobile', 'desktop'],
                 iT = types.length,
                 previews = {},
                 headings = {},
                 form = $('<form>', {
-                    class: 'preview-utility-bar plain',
-                    autocomplete: 'off'
-                }),
-                formInner = $('<div>', {
-                    class: 'preview-utility-bar-inner tbl'
-                }),
-                canvas = $('<div>', {
-                    class: 'preview-canvas'
-                }),
-                feedback = createFeedback(),
+                class : 'preview-utility-bar plain',
+                autocomplete : 'off'
+            }),
+            formInner = $('<div>', {
+                class : 'preview-utility-bar-inner tbl'
+            }),
+            canvas = $('<div>', {
+                class : 'preview-canvas'
+            }),
+            feedback = createFeedback(),
                 deviceSelectors = {},
                 orientationSelector,
                 viewTogglers = {},
                 tools = $('<ul>', {
-                    class: 'plain tbl-cell clearfix'
-                }),
-                closer = createCloser();
+                class : 'plain tbl-cell clearfix'
+            }),
+            closer = createCloser();
 
             overlay = $('<div>', {
-                class: 'preview-overlay tao-scope overlay preview-' + currPreviewType
+                class : 'preview-overlay tao-scope overlay preview-' + currPreviewType
             });
             container = $('<div>', {
-                class: 'preview-container-outer'
+                class : 'preview-container-outer'
             });
 
 
-            while (iT--) {
+            while(iT--){
                 previews[types[iT]] = createPreviewFrame(types[iT]);
                 viewTogglers[types[iT]] = createToggler(types[iT]);
                 deviceSelectors[types[iT]] = createDeviceSelector(types[iT]);
@@ -356,15 +356,15 @@ define([
             overlay.append(container);
 
             overlay.find('select').select2({
-                minimumResultsForSearch: -1
+                minimumResultsForSearch : -1
             });
 
             $doc.find('body').append(overlay);
 
             overlay.hide();
 
-            $doc.keyup(function (e) {
-                if (e.keyCode == 27) {
+            $doc.keyup(function(e){
+                if(e.keyCode == 27){
                     closer.trigger('click');
                 }
             });
@@ -374,8 +374,13 @@ define([
     };
 
 
-    return (function ($) {
+    return (function($){
 
+        var destroy = function(){
+            $('.preview-overlay').remove();
+            overlay = null;
+            container = null;
+        };
 
         /**
          * Create preview
@@ -383,18 +388,23 @@ define([
          * @param launchers - buttons to launch preview
          * @param item
          */
-        var init = function (launchers, item) {
-
+        var init = function(launchers, item){
+            
+            destroy();
+            
             createWidget(item);
 
-            $(launchers).on('click', function () {
+            $(launchers).on('click', function(){
                 currPreviewType = $(this).data('preview-type') || 'desktop';
 
-                if (togglersByTarget[currPreviewType]) {
+                if(togglersByTarget[currPreviewType]){
                     togglersByTarget[currPreviewType].trigger('click');
                 }
-
-                overlay.fadeIn(function () {
+                
+                
+                console.log(overlay);
+                
+                overlay.fadeIn(function(){
                     overlay.height($doc.outerHeight());
                     overlay.find('select').trigger('change');
                 });
@@ -402,7 +412,7 @@ define([
         };
 
         return {
-            init: init
+            init : init
         }
     }($));
 });
