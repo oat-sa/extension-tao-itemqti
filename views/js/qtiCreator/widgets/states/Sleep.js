26b324ccb89215feb4c68211a8d979d6f09ec96f
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-22 11:01:43 +0000

    choice inner widget hover style
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20435 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/states/Sleep.js
+++ views/js/qtiCreator/widgets/states/Sleep.js
@@ -6,10 +6,10 @@ define(['taoQtiItem/qtiCreator/widgets/states/factory'], function(stateFactory){
         //add listener to display proper hover style
         $container.on('mouseenter.sleep', function(e){
             $container.addClass('hover');
-            $container.parent().trigger('mouseleave.sleep');
+            $container.parent().trigger('mouseleave');//note : don't trigger it with namespace otherwise, choice.on(mouseleave.choice) will not be triggered
         }).on('mouseleave.sleep', function(){
             $container.removeClass('hover');
-            $container.parent().trigger('mouseenter.sleep');
+            $container.parent().trigger('mouseenter');//note : same as mouseenter.sleep
         });
         
     }, function(){
