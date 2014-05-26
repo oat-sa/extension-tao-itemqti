e1319236c18079250fc68f37d88fc02da8b8b07a
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-26 11:27:20 +0000

    fixed deleting popup buttons
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20473 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/helpers/deletingState.js
+++ views/js/qtiCreator/widgets/helpers/deletingState.js
@@ -8,10 +8,6 @@ define(['lodash', 'jquery', 'tpl!taoQtiItem/qtiCreator/tpl/notifications/deletin
             _confirmDeletion($messageBox, 1000);
         }, _timeout);
 
-        $messageBox.on('click', function(e){
-            e.stopPropagation();
-        });
-
         $('body').on('mousedown.deleting', function(e){
             //confirm deleting whenever user interact with another object
             if(e.target !== $messageBox[0] && !$.contains($messageBox[0], e.target)){
