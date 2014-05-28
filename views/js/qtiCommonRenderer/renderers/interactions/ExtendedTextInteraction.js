b480e012c9f2d6517c54a7034bb25388f6c4ac24
jbogaerts <jbogaerts@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-28 08:37:47 +0000

    updated QTI CSS.
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20509 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCommonRenderer/renderers/interactions/ExtendedTextInteraction.js
+++ views/js/qtiCommonRenderer/renderers/interactions/ExtendedTextInteraction.js
@@ -28,6 +28,19 @@ define([
             }
         });
     };
+    
+    /**
+     * Whether or not multiple strings are expected from the candidate to
+     * compose a valid response.
+     * 
+     * @param {object} interaction
+     * @returns {boolean}
+     */
+    var _isMultiple = function(interaction) {
+        var attributes = interaction.getAttributes();
+        var response = interaction.getResponseDeclaration();
+        return !!(attributes.maxStrings && (response.attr('cardinality') === 'multiple' || response.attr('cardinality') === 'ordered'));
+    };
 
     /**
      * Init rendering, called after template injected into the DOM
@@ -37,82 +50,71 @@ define([
      * @param {object} interaction
      */
     var render = function(interaction){
-        var attributes = interaction.getAttributes(),
-            $container = interaction.getContainer(),
-            response = interaction.getResponseDeclaration(),
-            multiple = !!(attributes.maxStrings && (response.attr('cardinality') === 'multiple' || response.attr('cardinality') === 'ordered'));
-        var $el, expectedLength, expectedLines, placeholderType;       
+        var attributes = interaction.getAttributes();
+        var $container = interaction.getContainer();
+        var response = interaction.getResponseDeclaration();
+        var multiple = _isMultiple(interaction);
+        
+        var $el, expectedLength, expectedLines, placeholderType;
  
-        //if the input is textarea
-        if(!multiple){
+        // if the input is textarea
+        if (!multiple) {
             $el = $container.find('textarea');
             var ckeOptions = {
-               extraPlugins: 'confighelper'
+               extraPlugins: 'confighelper',
+               resize_enabled: false
             };
             
-           /* //set the width(cols) on textarea 
-            if(attributes.expectedLength){
-                expectedLength = parseInt(attributes.expectedLength, 10);
-                if(expectedLength > 0){
-                    //_.extend(ckeOptions, {width : expectedLength});
-                    //_.extend(ckeOptions, {resize_maxWidth : 500});
-                }
-            }
-
-            //set the height(lines) on textarea 
-            if(attributes.expectedLines){
-                expectedLines = parseInt(attributes.expectedLines, 10);
-                if(expectedLines > 0){
-                    //_.extend(ckeOptions, {height : expectedLines});
-                    //_.extend(ckeOptions, {resize_maxHeight : 500});
-                }
-            }
-
-            //setting the pattern mask for the textarea
-            if(attributes.patternMask){
-                _setPattern($el, attributes.patternMask);
-            } */
-            
             //setting the placeholder for the textarea
-            if(attributes.placeholderText){
+            if (attributes.placeholderText) {
                 $el.attr('placeholder', attributes.placeholderText);            
             }
 
-            ckEditor.on( 'instanceCreated', function( event ) {
-                var editor = event.editor,
-                    toolbarType = 'block';
+            // Enable ckeditor only if text format is 'xhtml'.
+            if (_getFormat(interaction) == 'xhtml') {
+                ckEditor.on('instanceCreated', function(event) {
+                    var editor = event.editor,
+                        toolbarType = 'block';
 
-                editor.on('configLoaded', function(e) {
-                    editor.config = ckConfigurator.getConfig(editor, toolbarType, ckeOptions);
+                    editor.on('configLoaded', function(e) {
+                        editor.config = ckConfigurator.getConfig(editor, toolbarType, ckeOptions);
+                    });
                 });
-            });
-          
-            //replace the textarea with ckEditor
-            ckEditor.replace(interaction.attr('identifier'));
-            
-        } else {
+              
+                //replace the textarea with ckEditor
+                ckEditor.replace(interaction.attr('identifier'));
+                $container.find('#text-container').addClass('solid');
+            }
+        } 
+        else {
             $el = $container.find('input');
             
             //setting the checking for minimum number of answers
-            if(attributes.minStrings){
+            if (attributes.minStrings) {
+                
                 //get the number of filled inputs
-                var _getNumStrings = function($element){
+                var _getNumStrings = function($element) {
+                    
                     var num = 0;
-                    $element.each(function(){
-                        if($(this).val() !== ''){
+                    
+                    $element.each(function() {
+                        if ($(this).val() !== '') {
                             num++;
                         }
                     });
+                    
                     return num;
                 };
+                
                 var minStrings = parseInt(attributes.minStrings);
 
-                if(minStrings>0){
-                    $el.on('blur', function(){
-                        setTimeout(function(){
+                if (minStrings > 0) {
+                    
+                    $el.on('blur', function() {
+                        setTimeout(function() {
                             //checking if the user was clicked outside of the input fields
-                            if(!$el.is(':focus') && _getNumStrings($el)<minStrings){
-                                Helper.appendNotification(interaction, __('The minimum number of answers is  ')+' : '+minStrings, 'warning');
+                            if (!$el.is(':focus') && _getNumStrings($el) < minStrings) {
+                                Helper.appendNotification(interaction, __('The minimum number of answers is ') + ' : ' + minStrings, 'warning');
                             }
                         }, 100);
                     });
@@ -120,25 +122,25 @@ define([
             }
             
             //set the fields width
-            if(attributes.expectedLength){
+            if (attributes.expectedLength) {
                 expectedLength = parseInt(attributes.expectedLength, 10);
 
-                if(expectedLength > 0){
-                    $el.each(function(){
+                if (expectedLength > 0) {
+                    $el.each(function() {
                         $(this).css('width', expectedLength + 'em');
                     });
                 }
             }
             
             //set the fileds pattern mask
-            if(attributes.patternMask){
-                $el.each(function(){
+            if (attributes.patternMask) {
+                $el.each(function() {
                     _setPattern($(this), attributes.patternMask);
                 });
             }
             
             //set the fileds placeholder
-            if(attributes.placeholderText){
+            if (attributes.placeholderText) {
                 /**
                  * The type of the fileds placeholder:
                  * multiple - set placeholder for each field
@@ -147,18 +149,19 @@ define([
                  */
                 placeholderType = 'first';
                 
-                if(placeholderType === 'multiple'){
-                    $el.each(function(){
+                if (placeholderType === 'multiple') {
+                    $el.each(function() {
                         $(this).attr('placeholder', attributes.placeholderText);
                     });
-                } else if (placeholderType === 'first'){
+                }
+                else if (placeholderType === 'first') {
                     $el.first().attr('placeholder', attributes.placeholderText);
                 }
             }
         }
     };
     
-    var resetResponse = function(interaction){
+    var resetResponse = function(interaction) {
         interaction.getContainer().find('input, textarea').val('');
     };
 
@@ -174,27 +177,31 @@ define([
      * @param {object} interaction
      * @param {object} response
      */
-    var setResponse = function(interaction, response){
+    var setResponse = function(interaction, response) {
 
-        var _setMultipleVal = function(identifier, value){
+        var _setMultipleVal = function(identifier, value) {
             interaction.getContainer().find('#'+identifier).val(value);
         };
         
-        var _setVal = function(value){
+        var _setVal = function(value) {
             interaction.getContainer().find('textarea').val(value);
         };
 
         var baseType = interaction.getResponseDeclaration().attr('baseType');
         
-        if(response.base && response.base[baseType] !== undefined){
+        if (response.base && response.base[baseType] !== undefined) {
             _setVal(response.base[baseType]);
-        } else if (response.list && response.list[baseType]){
-            for(var i in response.list[baseType]){
+        }
+        else if (response.list && response.list[baseType]) {
+            
+            for (var i in response.list[baseType]) {
                 var identifier = (response.list.identifier === undefined) ? '' : response.list.identifier[i];
                 _setMultipleVal(identifier + '_' + i, response.list[baseType][i]);
             }
-        } else {
-            throw new Error('wrong response format in argument: ');
+            
+        } 
+        else {
+            throw new Error('wrong response format in argument.');
         }
     };
 
@@ -210,49 +217,61 @@ define([
      * @param {object} interaction
      * @returns {object}
      */
-    var getResponse = function(interaction){
-        var $container = interaction.getContainer(),
-            attributes = interaction.getAttributes(),
-            responseDeclaration = interaction.getResponseDeclaration(),
-            baseType = responseDeclaration.attr('baseType'),
-            numericBase = attributes.base || 10,
-            multiple = !!(attributes.maxStrings && (responseDeclaration.attr('cardinality') === 'multiple' || responseDeclaration.attr('cardinality') === 'ordered')),
-            ret = multiple ? {list:{}} : {base:{}};
+    var getResponse = function(interaction) {
+        
+        var $container = interaction.getContainer();
+        var attributes = interaction.getAttributes();
+        var responseDeclaration = interaction.getResponseDeclaration();
+        var baseType = responseDeclaration.attr('baseType');
+        var numericBase = attributes.base || 10;
+        var multiple = !!(attributes.maxStrings && (responseDeclaration.attr('cardinality') === 'multiple' || responseDeclaration.attr('cardinality') === 'ordered'));
+        var ret = multiple ? {list:{}} : {base:{}};
     
-        if(multiple){
+        if (multiple) {
+            
             var values = [];
-            $container.find('input').each(function(i){
+            
+            $container.find('input').each(function(i) {
+                
                 $el = $(this);
                 
-                if(attributes.placeholderText && $el.val() === attributes.placeholderText){
+                if (attributes.placeholderText && $el.val() === attributes.placeholderText) {
                     values[i] = '';
-                } else {
-                    if(baseType==='integer'){
+                }
+                else {
+                    if (baseType === 'integer') {
                         values[i] = parseInt($el.val(), numericBase);
                         values[i] = isNaN(values[i]) ? '' : values[i];
-                    } else if(baseType==='float'){
+                    }
+                    else if(baseType === 'float') {
                         values[i] = parseFloat($el.val());
                         values[i] = isNaN(values[i]) ? '' : values[i];
-                    } else if(baseType==='string'){
+                    }
+                    else if(baseType === 'string') {
                         values[i] = $el.val();
                     }
                 }
             });
 
             ret.list[baseType] = values;
-        } else {
-            var value = '',
-                $el = $container.find('textarea');
+        } 
+        else {
             
-            if(attributes.placeholderText && $el.val() === attributes.placeholderText){
+            var value = '';
+            
+            if (attributes.placeholderText && _getTextareaValue(interaction) === attributes.placeholderText) {
                 value = '';
-            } else {
-                if(baseType==='integer'){
-                    value = parseInt($el.val(), numericBase);
-                } else if(baseType==='float'){
-                    value = parseFloat($el.val());
-                } else if(baseType==='string'){
-                    value = $el.val();
+            }
+            else {
+                
+                if (baseType === 'integer') {
+                    value = parseInt(_getTextareaValue(interaction), numericBase);
+                }
+                else if (baseType === 'float') {
+                    value = parseFloat(_getTextareaValue(interaction));
+                }
+                else if (baseType === 'string') {
+                    value = _getTextareaValue(interaction);
                 }
             }
 
@@ -261,6 +280,34 @@ define([
         
         return ret;
     };
+    
+    var _getTextareaValue = function(interaction) {
+        if (_getFormat(interaction) === 'xhtml') {
+            return _ckEditorData(interaction);
+        }
+        else {
+            return Helper.getContainer(interaction).find('textarea').val();
+        }
+    }
+    
+    var _ckEditorData = function(interaction) {
+        return ckEditor.instances[interaction.attr('identifier')].getData();
+    };
+    
+    var _getFormat = function(interaction) {
+        var format = interaction.attr('format');
+        
+        switch (format) {
+            case 'plain':
+            case 'xhtml':
+            case 'preFormatted':
+                return format;
+            break;
+            
+            default:
+                return 'plain';
+        }
+    }
 
     return {
         qtiClass : 'extendedTextInteraction',
--- views/js/qtiCommonRenderer/tpl/interactions/extendedTextInteraction.tpl
+++ views/js/qtiCommonRenderer/tpl/interactions/extendedTextInteraction.tpl
@@ -1,11 +1,13 @@
 <div class="qti-interaction qti-blockInteraction qti-extendedTextInteraction" data-serial="{{serial}}" data-qti-class="extendedTextInteraction">
     {{#if prompt}}{{{prompt}}}{{/if}}
+    <div class="notification-container"></div>
     {{#if multiple}}
     <div id="{{attributes.identifier}}">
         {{#maxStringLoop}}<input id="{{attributes.identifier}}_{{.}}" name="{{attributes.identifier}}_{{.}}"/><br />{{/maxStringLoop}}
     </div>
     {{else}}
-    <textarea id="{{attributes.identifier}}" name="{{attributes.identifier}}"></textarea>
+        <div id="text-container">
+            <textarea id="{{attributes.identifier}}" class="solid{{#if attributes.class}} attributes.class"{{/if}} cols="72"></textarea>
+        </div>
     {{/if}}
-    <div class="notification-container"></div>
 </div>
