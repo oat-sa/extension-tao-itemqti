4288cdfd1b1be749276da8ea24750c10f297e10c
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-05-30 15:25:40 +0000

    fixed #2639
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20625 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiXmlRenderer/renderers/interactions/UploadInteraction.js
+++ views/js/qtiXmlRenderer/renderers/interactions/UploadInteraction.js
@@ -1,4 +1,4 @@
-define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/element'], function(tpl){
+define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/interactions/blockInteraction'], function(tpl){
     return {
         qtiClass : 'uploadInteraction',
         template : tpl
