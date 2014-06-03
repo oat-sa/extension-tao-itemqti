b59d1bf98ef523671d3b523bb85ffc944e11ef12
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-03 08:24:59 +0000

    gapMatchInteraction : added body editor
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20670 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/interactions/gapMatchInteraction/states/Question.js
+++ views/js/qtiCreator/widgets/interactions/gapMatchInteraction/states/Question.js
@@ -2,10 +2,21 @@ define([
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/interactions/blockInteraction/states/Question',
     'taoQtiItem/qtiCreator/widgets/helpers/formElement',
-    'tpl!taoQtiItem/qtiCreator/tpl/forms/interactions/gapMatch'
-], function(stateFactory, Question, formElement, formTpl){
+    'tpl!taoQtiItem/qtiCreator/tpl/forms/interactions/gapMatch',
+    'taoQtiItem/qtiCreator/helper/htmlEditor',
+    'taoQtiItem/qtiCreator/editor/gridEditor/content',
+    'tpl!taoQtiItem/qtiCreator/tpl/toolbars/htmlEditorTrigger'
+], function(stateFactory, Question, formElement, formTpl, htmlEditor, contentHelper, toolbarTpl){
 
-    var GapMatchInteractionStateQuestion = stateFactory.extend(Question);
+    var GapMatchInteractionStateQuestion = stateFactory.extend(Question, function(){
+        
+        this.buildEditor();
+        
+    }, function(){
+        
+        this.destroyEditor();
+        
+    });
 
     GapMatchInteractionStateQuestion.prototype.initForm = function(){
 
@@ -24,6 +35,42 @@ define([
         });
         
     };
+    
+    GapMatchInteractionStateQuestion.prototype.buildEditor = function(){
 
+        var _widget = this.widget,
+            container = _widget.element.getBody(),
+            $editableContainer = _widget.$container.find('.qti-flow-container');
+
+        //@todo set them in the tpl
+        $editableContainer.attr('data-html-editable-container', true);
+
+        if(!htmlEditor.hasEditor($editableContainer)){
+            
+            var $tlb = $(toolbarTpl({
+                serial : _widget.serial,
+                state : 'question'
+            }));
+            
+            //add toolbar once only:
+            $editableContainer.append($tlb);
+            $tlb.show();
+            
+            htmlEditor.buildEditor($editableContainer, {
+                change : contentHelper.getChangeCallback(container),
+                data : {
+                    container : container,
+                    widget : _widget
+                }
+            });
+        }
+    };
+
+    GapMatchInteractionStateQuestion.prototype.destroyEditor = function(){
+        
+        //search and destroy the editor
+        htmlEditor.destroyEditor(this.widget.$container.find('.qti-flow-container'));
+    };
+    
     return GapMatchInteractionStateQuestion;
 });
