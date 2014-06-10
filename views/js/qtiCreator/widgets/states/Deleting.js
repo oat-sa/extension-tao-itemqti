67ecaeb593e95ce3d4f901c43c9560fe38d6fd53
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-10 15:02:10 +0000

    fixed col and row deleting
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20807 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/states/Deleting.js
+++ views/js/qtiCreator/widgets/states/Deleting.js
@@ -11,7 +11,8 @@ define([
 
         //array to store new col untis
         this.refactoredUnits = [];
-
+        this.updateBody = false;
+        
         //reference to the dom element(s) to be remove on delete
         this.$elementToRemove = this.getElementToRemove();
 
@@ -68,7 +69,7 @@ define([
         var $col = $container.parent();
         var $row = $col.parent('.grid-row');
         if($row.length){
-
+            this.updateBody = true;
             if($row.children().length === 1){
                 //if it is the only col in the row, hide the entire row
                 return $row;
@@ -78,6 +79,7 @@ define([
             }
         }else if($container.hasClass('grid-row')){
             //rubric block:
+            this.updateBody = true;
             return $container;
         }
     };
@@ -135,8 +137,10 @@ define([
     DeletingState.prototype.showWidget = function(){
 
         var $elt = this.$elementToRemove;
-        if($elt.length){
+        if($elt.length && $.contains(document, $elt[0])){
+            
             $elt.show();
+            
             if(_isCol($elt)){
                 //restore the other units:
                 _.each(this.refactoredUnits, function(col){
@@ -170,14 +174,14 @@ define([
     };
 
     DeletingState.prototype.deleteElement = function(){
-
-        var isRow = this.$elementToRemove.hasClass('grid-row');
         
-        this.widget.destroy();//remove what remains of the widget (almost nothing)
+        this.refactoredUnits = [];
+        
         this.$elementToRemove.remove();//remove html from the dom
+        this.widget.destroy();//remove what remains of the widget (almost nothing), call this after element remove
         this.widget.element.remove();//remove from model
         
-        if(isRow){
+        if(this.updateBody){
             //need to update item body
             var item = this.widget.element.getRelatedItem();
             var $item = item.data('widget').$container.find('.qti-itemBody');
