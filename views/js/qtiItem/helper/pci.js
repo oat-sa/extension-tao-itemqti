c486be399da6c1deed63a9a4a3a7553c673ef857
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-12 12:35:33 +0000

    fixed #2689
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20844 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiItem/helper/pci.js
+++ views/js/qtiItem/helper/pci.js
@@ -3,10 +3,10 @@ define(['lodash'], function(_){
     var pci = {
         getRawValues : function(pciVar){
             if(_.isPlainObject(pciVar)){
-                if(pciVar['base']){
-                    return _.values(pciVar['base']);
-                }else if(pciVar['list']){
-                    return _.values(pciVar['list']);
+                if(pciVar.base!== undefined){
+                    return _.values(pciVar.base);
+                }else if(pciVar.list){
+                    return _.values(pciVar.list);
                 }
             }
             throw 'unsupported type ';
