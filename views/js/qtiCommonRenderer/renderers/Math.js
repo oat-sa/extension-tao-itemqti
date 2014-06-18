e78049cdcc6cd6ea5ab97dff8d4382b4aeba03d1
ssipasseuth <ssipasseuth@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-18 13:38:47 +0000

    defering execution of math typesetting to fix some rendering issue in chrome
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20925 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCommonRenderer/renderers/Math.js
+++ views/js/qtiCommonRenderer/renderers/Math.js
@@ -1,11 +1,13 @@
-define(['tpl!taoQtiItem/qtiCommonRenderer/tpl/math', 'taoQtiItem/qtiCommonRenderer/helpers/Helper', 'mathJax'], function(tpl, Helper, MathJax){
+define(['tpl!taoQtiItem/qtiCommonRenderer/tpl/math', 'taoQtiItem/qtiCommonRenderer/helpers/Helper', 'mathJax', 'lodash'], function(tpl, Helper, MathJax, _){
     return {
         qtiClass : 'math',
         template : tpl,
         getContainer : Helper.getContainer,
         render : function(math, data){
             if(typeof(MathJax) !== 'undefined' && MathJax){
-                MathJax.Hub.Queue(["Typeset", MathJax.Hub, Helper.getContainer(math).parent()[0]]);
+                _.defer(function(){//defer execution fix some rendering issue in chrome
+                     MathJax.Hub.Queue(["Typeset", MathJax.Hub, Helper.getContainer(math).parent()[0]]);
+                });
             }
         }
     };
