eddb55b1774421b9b1a483c827dd4775e8d71417
jbout <jbout@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-06-20 14:41:44 +0000

    regenerated translations
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@20983 62dfeb86-a93f-4914-9d13-15c0a160985d

--- locales/ar-arb/messages.lang.php
+++ locales/ar-arb/messages.lang.php
@@ -7,7 +7,6 @@ $GLOBALS['__l10n']["A joint initiative of CRP Henri Tudor and the University of 
 $GLOBALS['__l10n']["A similar resource has already been versioned"]="مصدر مشابه تم تعيين اصداره بالفعل";
 $GLOBALS['__l10n']["Access denied. Please renew your authentication!"]="!تم رفض الوصول. يرجى تجديد المصادقة الخاصة بك";
 $GLOBALS['__l10n']["Actions"]="الأعمال";
-$GLOBALS['__l10n']["Add"]="إضافة";
 $GLOBALS['__l10n']["Add / Edit lists"]="إضافة / تعديل القوائم";
 $GLOBALS['__l10n']["Add a user"]="اضافة مستخدم";
 $GLOBALS['__l10n']["Add property"]="إضافة خاصية";
@@ -42,13 +41,13 @@ $GLOBALS['__l10n']["Deliveries"]="Deliveries";
 $GLOBALS['__l10n']["Don't set"]="لا تدرج";
 $GLOBALS['__l10n']["Download"]="تحميل";
 $GLOBALS['__l10n']["duplicate"]="تكرار";
-$GLOBALS['__l10n']["edit"]="تحرير";
 $GLOBALS['__l10n']["Edit"]="تحرير";
+$GLOBALS['__l10n']["edit"]="تحرير";
 $GLOBALS['__l10n']["Edit a user"]="تحرير مستخدم";
 $GLOBALS['__l10n']["Edit Comment"]="تحرير تعليق";
 $GLOBALS['__l10n']["Edit user"]="تحرير مستخدم";
-$GLOBALS['__l10n']["Element"]="العنصر";
 $GLOBALS['__l10n']["element"]="عنصر";
+$GLOBALS['__l10n']["Element"]="العنصر";
 $GLOBALS['__l10n']["element deleted"]="عنصر محذوف";
 $GLOBALS['__l10n']["Error"]="خطأ";
 $GLOBALS['__l10n']["Error during file import"]="خطأ أثناء استيراد الملف";
@@ -60,8 +59,8 @@ $GLOBALS['__l10n']["Filters"]="Filtres";
 $GLOBALS['__l10n']["Groups"]="مجموعات";
 $GLOBALS['__l10n']["Identifier"]="معرف";
 $GLOBALS['__l10n']["Inclusive (AND)"]="Inclusif (ET)";
-$GLOBALS['__l10n']["Interface Language"]="لغة الواجهة";
 $GLOBALS['__l10n']["Interface language"]="لغة الواجهة";
+$GLOBALS['__l10n']["Interface Language"]="لغة الواجهة";
 $GLOBALS['__l10n']["Invalid field length"]="طول الحقل غير صحيح";
 $GLOBALS['__l10n']["Invalid field range"]="Le rang du champ est invalide";
 $GLOBALS['__l10n']["Invalid file size"]="حجم الملف غير صحيح";
@@ -80,7 +79,6 @@ $GLOBALS['__l10n']["Lists"]="القوائم";
 $GLOBALS['__l10n']["Local Data (the local namespace containing only the data inserted by the users)"]="(البيانات المحلية (مساحة الاسم المحلي الذي يحتوي فقط على البيانات المدرجة من قبل المستخدمين";
 $GLOBALS['__l10n']["Login"]="الدخول";
 $GLOBALS['__l10n']["Manage data list"]="إدارة قائمة البيانات";
-$GLOBALS['__l10n']["Manage deliveries"]="Gérer les deliveries";
 $GLOBALS['__l10n']["Manage groups"]="إدارة المجموعات";
 $GLOBALS['__l10n']["Manage items"]="ادارة الاسئلة";
 $GLOBALS['__l10n']["Manage test takers"]="إدارة المتقدمين للاختبار";
@@ -136,7 +134,7 @@ $GLOBALS['__l10n']["Selection saved successfully"]="تم حفظ التحديد �
 $GLOBALS['__l10n']["Simple Mode"]="الطريقة البسيطة";
 $GLOBALS['__l10n']["Start upload"]="بدء التحميل";
 $GLOBALS['__l10n']["Supported export formats"]="صيغ الاستيراد المعتمدة";
-$GLOBALS['__l10n']["TAO Back Office"]="TAO: Outils d'administration";
+$GLOBALS['__l10n']["TAO Delivery Server"]="Serveur de delivery TAO";
 $GLOBALS['__l10n']["Tests"]="الاختبارات";
 $GLOBALS['__l10n']["The format of this field is not valid."]="تنسيق هذا الحقل غير صالح";
 $GLOBALS['__l10n']["The uploaded file is too large"]="الملف المحمول كبير جدا";
