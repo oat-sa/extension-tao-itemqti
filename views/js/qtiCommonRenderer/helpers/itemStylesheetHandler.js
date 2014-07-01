e3bed2e48ba4693f8165343358f67cade7f5abe9
draber <draber@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-07-01 14:41:02 +0000

    Bug #2833 Custom styles doesn't work in IE11
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@21188 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCommonRenderer/helpers/itemStylesheetHandler.js
+++ views/js/qtiCommonRenderer/helpers/itemStylesheetHandler.js
@@ -11,17 +11,23 @@ define([
 
              $('body').addClass('tao-scope');
 
+             // relative links with cache buster
             _(stylesheets).forEach(function(stylesheet){
                 link = (function() {
                     var _link = $(stylesheet.render()),
-                        _href = _link.attr('href');
+                        _href = _link.attr('href'),
+                        _sep  = _href.indexOf('?') > -1 ? '&' : '?';
+
                     if(_href.indexOf('/') === 0) {
                         _href = _href.slice(1);
                     }
-                    _link.attr('href', _href);
+                    _link.attr('href', _href + _sep + (new Date().getTime()).toString());
                     return _link;
                 }());
-                head.append(link);
+                // this bit seems to get IE to co-operate
+                setTimeout(function() {
+                    head.append(link);
+                }, 500)
             });
 
         };
