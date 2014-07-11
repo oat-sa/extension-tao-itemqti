270e2cb1c3bb6a4afe8629ea763b840685b43996
grigor <grigor@62dfeb86-a93f-4914-9d13-15c0a160985d>
2014-07-11 11:12:01 +0000

    match interaction fix (bug #2888)
    git-svn-id: http://vcs.taotesting.com/svn/oat/projects/taoQtiItem/trunk@21395 62dfeb86-a93f-4914-9d13-15c0a160985d

--- views/js/qtiCreator/widgets/interactions/matchInteraction/states/Map.js
+++ views/js/qtiCreator/widgets/interactions/matchInteraction/states/Map.js
@@ -1,9 +1,10 @@
 define([
+    'jquery',
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/states/Map',
     'taoQtiItem/qtiCreator/widgets/interactions/matchInteraction/ResponseWidget',
     'lodash'
-], function(stateFactory, Map, responseWidget, _){
+], function($, stateFactory, Map, responseWidget, _){
 
     var MatchInteractionStateMap = stateFactory.create(Map, function(){
 
@@ -16,6 +17,25 @@ define([
 
         //finally, apply defined correct response and response mapping:
         responseWidget.setResponse(interaction, _.values(response.getCorrect()));
+        
+       //change the correct state 
+       _widget.on('metaChange', function(meta){
+            if(meta.key === 'defineCorrect'){
+                if(meta.value){
+                    $('.match-interaction-area input[type="checkbox"]', _widget.$container)
+                        .removeProp('disabled') 
+                        .removeClass('disabled');
+                } else {
+                    $('.match-interaction-area input[type="checkbox"]', _widget.$container)
+                        .prop('disabled', true)
+                        .prop('checked', false)
+                        .addClass('disabled');
+                }
+                if(meta.value === false){
+                    response.setCorrect([]);
+                }
+            }
+        });
 
     }, function(){
 
