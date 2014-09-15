4929bb1c91218f176737129e8e898bc79e8794f7
ssipasseuth <sam@taotesting.com>
2014-09-15 15:11:30 +0200

    added abstract widget class for custom interaction

---
+++ views/js/qtiCreator/widgets/interactions/customInteraction/states/states.js
@@ -0,0 +1,6 @@
+define([
+    'taoQtiItem/qtiCreator/widgets/states/factory',
+    'taoQtiItem/qtiCreator/widgets/interactions/states/states'
+], function(factory, states){
+    return factory.createBundle(states, arguments);
+});
