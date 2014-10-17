60aa3d863f003f1e53de2f983a983e821a29f173
Bertrand Chevrier <bertrand@taotesting.com>
2014-10-17 09:57:04 +0200

    from section-height to qti editor

--- views/js/qtiCreator/editor/editor.js
+++ views/js/qtiCreator/editor/editor.js
@@ -1,5 +1,6 @@
 define([
     'jquery',
+    'lodash',
     //gui components
     'taoQtiItem/qtiCreator/editor/preview',
     'taoQtiItem/qtiCreator/editor/preparePrint',
@@ -9,11 +10,10 @@ define([
     'taoQtiItem/qtiCreator/editor/styleEditor/fontSizeChanger',
     'taoQtiItem/qtiCreator/editor/styleEditor/itemResizer',
     'taoQtiItem/qtiCreator/editor/styleEditor/styleEditor',
-    'taoQtiItem/qtiCreator/editor/styleEditor/styleSheetToggler',
-    //layout
-    'layout/section-height'
+    'taoQtiItem/qtiCreator/editor/styleEditor/styleSheetToggler'
 ], function(
     $,
+    _,
     preview,
     preparePrint,
     fontSelector,
@@ -21,8 +21,7 @@ define([
     fontSizeChanger,
     itemResizer,
     styleEditor,
-    styleSheetToggler,
-    sectionHeight
+    styleSheetToggler
     ){
 
     'use strict';
@@ -50,7 +49,9 @@ define([
      */
     var initGui = function(widget, config){
 
-        sectionHeight.setHeights();
+        updateHeight();
+        $(window).off('resize.qti-editor')
+            .on('resize.qti-editor', _.throttle(updateHeight, 50));
 
         initStyleEditor(widget, config);
 
@@ -65,6 +66,44 @@ define([
         $itemPanel.addClass('has-item');
         $labelSpan.text(config.$label);
         $actionGroups.show();
+    };
+
+    /**
+     * Update the height of the authoring tool
+     * @private 
+     */
+    var updateHeight = function updateHeight(){
+        var $itemEditorPanel = $('#item-editor-panel');
+        var $itemSidebars = $('.item-editor-sidebar');
+        var $contentPanel = $('#panel-authoring');
+        var $searchBar,
+            searchBarHeight,
+            footerTop,
+            contentWrapperTop,
+            remainingHeight;
+
+        if (!$contentPanel.length || !$itemEditorPanel.length) {
+            return;
+        }
+
+        $searchBar = $contentPanel.find('.search-action-bar');
+        searchBarHeight = $searchBar.outerHeight() + parseInt($searchBar.css('margin-bottom')) + parseInt($searchBar.css('margin-top'));
+
+        footerTop = (function() {
+            var $footer = $('body > footer'),
+                footerTop;
+            $itemSidebars.hide();
+            footerTop = $footer.offset().top;
+            $itemSidebars.show();
+            return footerTop;
+        }());
+        contentWrapperTop = $contentPanel.offset().top;
+        remainingHeight = footerTop - contentWrapperTop - $('.item-editor-action-bar').outerHeight();
+
+
+        // in the item editor the action bars are constructed slightly differently
+        $itemEditorPanel.find('#item-editor-scroll-outer').css({ minHeight: remainingHeight, maxHeight: remainingHeight, height: remainingHeight });
+        $itemSidebars.css({ minHeight: remainingHeight, maxHeight: remainingHeight, height: remainingHeight });
     };
 
     return {
