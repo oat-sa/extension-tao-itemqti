53b6ce6760063151c9f5075ccdcdd0f31cb39229
ssipasseuth <sam@taotesting.com>
2014-10-23 16:45:25 +0200

    integrating abstract portable elment manager controller

--- controller/AbstractPortableElementManager.php
+++ controller/AbstractPortableElementManager.php
@@ -18,20 +18,17 @@
  *
  */
 
-namespace oat\qtiItemPci\controller;
+namespace oat\taoQtiItem\controller;
 
 use \core_kernel_classes_Resource;
 use \tao_actions_CommonModule;
 use \common_exception_Error;
 use \tao_helpers_File;
 use \tao_helpers_Http;
-use \FileUploadException;
 use oat\taoQtiItem\model\qti\Service;
-use oat\qtiItemPci\model\CreatorRegistry;
-use oat\qtiItemPci\model\CreatorPackageParser;
 use oat\taoQtiItem\helpers\Authoring;
 
-class PciManager extends tao_actions_CommonModule
+abstract class AbstractPortableElementManager extends tao_actions_CommonModule
 {
     
     /**
@@ -39,111 +36,15 @@ class PciManager extends tao_actions_CommonModule
      */
     public function __construct(){
         parent::__construct();
-        $this->registry = new CreatorRegistry();
-    }
-
-    /**
-     * Returns the list of registered custom interactions and their data
-     */
-    public function getRegisteredInteractions(){
-
-        $returnValue = array();
-
-        $all = $this->registry->getRegisteredInteractions();
-
-        foreach($all as $pci){
-            $returnValue[$pci['typeIdentifier']] = $this->filterInteractionData($pci);
-        }
-
-        $this->returnJson($returnValue);
+        $this->registry = $this->getCreatorRegistry();
     }
     
     /**
-     * Remove security sensitive data to be sent to the client
+     * Return the registry used by this controller
      * 
-     * @param array $rawInteractionData
-     * @return array
+     * @return oat\taoQtiItem\model\CreatorRegistry
      */
-    protected function filterInteractionData($rawInteractionData){
-        
-        unset($rawInteractionData['directory']);
-        return $rawInteractionData;
-    }
-
-    /**
-     * Service to check if the uploaded file archive is a valid and non-existing one
-     * 
-     * JSON structure:
-     * {
-     *     "valid" : true/false (if is a valid package) 
-     *     "exists" : true/false (if the package is valid, check if the typeIdentifier is already used in the registry)
-     * }
-     */
-    public function verify(){
-
-        $result = array(
-            'valid' => false,
-            'exists' => false
-        );
-
-        $file = tao_helpers_Http::getUploadedFile('content');
-
-        $creatorPackageParser = new CreatorPackageParser($file['tmp_name']);
-        $creatorPackageParser->validate();
-        if($creatorPackageParser->isValid()){
-
-            $result['valid'] = true;
-
-            $manifest = $creatorPackageParser->getManifest();
-
-            $result['typeIdentifier'] = $manifest['typeIdentifier'];
-            $result['label'] = $manifest['label'];
-            $interaction = $this->registry->get($manifest['typeIdentifier']);
-            
-            if(!is_null($interaction)){
-                $result['exists'] = true;
-            }
-        }else{
-            $result['package'] = $creatorPackageParser->getErrors();
-        }
-
-        $this->returnJson($result);
-    }
-    
-    /**
-     * Add a new custom interaction from the uploaded zip package
-     */
-    public function add(){
-
-        //as upload may be called multiple times, we remove the session lock as soon as possible
-        session_write_close();
-
-        try{
-            $replace= true; //always set as "replaceable" and delegate decision to replace or not to the client side
-            $file = tao_helpers_Http::getUploadedFile('content');
-            $newInteraction = $this->registry->add($file['tmp_name'], $replace);
-
-            $this->returnJson($this->filterInteractionData($newInteraction));
-            
-        }catch(FileUploadException $fe){
-
-            $this->returnJson(array('error' => $fe->getMessage()));
-        }
-    }
-    
-    /**
-     * Delete a custom interaction from the registry
-     */
-    public function delete(){
-
-        $typeIdentifier = $this->getRequestParameter('typeIdentifier');
-        $this->registry->remove($typeIdentifier);
-        $ok = true;
-
-        $this->returnJson(array(
-            'success' => $ok
-        ));
-    }
+    abstract protected function getCreatorRegistry();
     
     /**
      * Get a file of a custom interaction
@@ -189,6 +90,16 @@ class PciManager extends tao_actions_CommonModule
     }
     
     /**
+     * Get the data of the implementation by its typeIdentifier
+     * 
+     * @param string $typeIdentifier
+     * @return array
+     */
+    protected function getImplementatioByTypeIdentifier($typeIdentifier){
+        return $this->registry->getDevImplementation($typeIdentifier);
+    }
+    
+    /**
      * Add required resources for a custom interaction (css, js) in the item directory
      * 
      * @throws common_exception_Error
@@ -202,26 +113,22 @@ class PciManager extends tao_actions_CommonModule
         $sharedLibRegistry = Service::singleton()->getSharedLibrariesRegistry();
         
         //find the interaction in the registry
-        $interaction = $this->registry->get($typeIdentifier);
-        if(is_null($interaction)){
-            $interaction = $this->registry->getDevImplementation($typeIdentifier);
-        }
-        if(is_null($interaction)){
+        $implementationData = $this->getImplementatioByTypeIdentifier($typeIdentifier);
+        if(is_null($implementationData)){
             throw new common_exception_Error('no pci found with the type identifier '.$typeIdentifier);
         }
         
         //get the root directory of the interaction
-        $directory = $interaction['directory'];
+        $directory = $implementationData['directory'];
         
         //get the lists of all required resources
-        $manifest = $interaction['manifest'];
+        $manifest = $implementationData['manifest'];
         $required = array($manifest['entryPoint']);
         
         //include libraries remotely only, so this block is temporarily disabled
         foreach($manifest['libraries'] as $lib){
             if(!$sharedLibRegistry->isRegistered($lib)){
                 $lib = preg_replace('/^.\//', '', $lib);
-//                $lib = $typeIdentifier . '/' . $lib; //add interaction namespace
                 $lib .= '.js';//add js extension
                 $required[] = $lib;
             }
