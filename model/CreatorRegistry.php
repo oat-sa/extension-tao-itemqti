cc8a15f00224b89a98ad16ff6371b0b9ba809b78
ssipasseuth <sam@taotesting.com>
2014-11-04 16:51:04 +0100

    moved addRequiredResources() logic to registry

--- model/CreatorRegistry.php
+++ model/CreatorRegistry.php
@@ -20,6 +20,11 @@
 
 namespace oat\taoQtiItem\model;
 
+use \core_kernel_classes_Resource;
+use \common_exception_Error;
+use oat\taoQtiItem\model\qti\Service;
+use oat\taoQtiItem\helpers\Authoring;
+
 /**
  * CreatorRegistry stores reference to 
  *
@@ -27,7 +32,7 @@ namespace oat\taoQtiItem\model;
  */
 abstract class CreatorRegistry
 {
-    
+
     /**
      * constructor
      */
@@ -35,25 +40,26 @@ abstract class CreatorRegistry
 
         $this->baseDevDir = $this->getBaseDevDir();
         $this->baseDevUrl = $this->getBaseDevUrl();
+        $this->sharedLibRegistry = Service::singleton()->getSharedLibrariesRegistry();
     }
-    
+
     /**
      * @return string - e.g. DIR_VIEWS/js/pciCreator/dev/
      */
     abstract protected function getBaseDevDir();
-    
+
     /**
      * @return string - e.g. BASE_WWW/js/pciCreator/dev/
      */
     abstract protected function getBaseDevUrl();
-    
+
     /**
      * get the hook file name to distinguish various implementation
      * 
      * @return string
      */
     abstract protected function getHookFileName();
-    
+
     /**
      * Get the entry point file path from the baseUrl
      * 
@@ -63,7 +69,7 @@ abstract class CreatorRegistry
     protected function getEntryPointFile($baseUrl){
         return $baseUrl.'/'.$this->getHookFileName();
     }
-    
+
     /**
      * Get PCI Creator hooks directly located at views/js/pciCreator/myCustomInteraction:
      * 
@@ -72,16 +78,16 @@ abstract class CreatorRegistry
     public function getDevImplementations(){
 
         $returnValue = array();
-        
+
         $hookFileName = $this->getHookFileName();
-        
+
         foreach(glob($this->baseDevDir.'*/'.$hookFileName.'.js') as $file){
 
             $dir = str_replace($hookFileName.'.js', '', $file);
             $manifestFile = $dir.$hookFileName.'.json';
-            
+
             if(file_exists($manifestFile)){
-                
+
                 $typeIdentifier = basename($dir);
                 $baseUrl = $this->baseDevUrl.$typeIdentifier.'/';
                 $manifest = json_decode(file_get_contents($manifestFile), true);
@@ -100,7 +106,7 @@ abstract class CreatorRegistry
 
         return $returnValue;
     }
-    
+
     /**
      * Get PCI Creator hook located at views/js/{{hookFileName}}/$typeIdentifier
      * 
@@ -108,7 +114,7 @@ abstract class CreatorRegistry
      * @return array
      */
     public function getDevImplementation($typeIdentifier){
-        
+
         //@todo : re-implement it to be more optimal
         $devImplementations = $this->getDevImplementations();
         foreach($devImplementations as $impl){
@@ -118,7 +124,7 @@ abstract class CreatorRegistry
         }
         return null;
     }
-    
+
     /**
      * Get the path to the directory of a the Creator located at views/js/{{hookFileName}}/
      * 
@@ -134,4 +140,62 @@ abstract class CreatorRegistry
             throw new \common_Exception('the type identifier cannot be found');
         }
     }
+
+    /**
+     * Get the data of the implementation by its typeIdentifier
+     * 
+     * @param string $typeIdentifier
+     * @return array
+     */
+    protected function getImplementatioByTypeIdentifier($typeIdentifier){
+        return $this->getDevImplementation($typeIdentifier);
+    }
+
+    /**
+     * Add required resources for a custom interaction (css, js) in the item directory
+     * 
+     * @param string $typeIdentifier
+     * @param \core_kernel_classes_Resource $item
+     * @throws common_exception_Error
+     */
+    public function addRequiredResources($typeIdentifier, core_kernel_classes_Resource $item){
+        
+        //find the interaction in the registry
+        $implementationData = $this->getImplementatioByTypeIdentifier($typeIdentifier);
+        if(is_null($implementationData)){
+            throw new common_exception_Error('no implementation found with the type identifier '.$typeIdentifier);
+        }
+
+        //get the root directory of the interaction
+        $directory = $implementationData['directory'];
+
+        //get the lists of all required resources
+        $manifest = $implementationData['manifest'];
+        $required = array($manifest['entryPoint']);
+
+        //include libraries remotely only, so this block is temporarily disabled
+        foreach($manifest['libraries'] as $lib){
+            if(!$this->sharedLibRegistry->isRegistered($lib)){
+                $lib = preg_replace('/^.\//', '', $lib);
+                $lib .= '.js'; //add js extension
+                $required[] = $lib;
+            }
+        }
+
+        //include custom interaction specific css in the item
+        if(isset($manifest['css'])){
+            $required = array_merge($required, array_values($manifest['css']));
+        }
+
+        //include media in the item
+        if(isset($manifest['media'])){
+            $required = array_merge($required, array_values($manifest['media']));
+        }
+
+        //add them to the rdf item
+        $resources = Authoring::addRequiredResources($directory, $required, $typeIdentifier, $item, '');
+        
+        return $resources;
+    }
+
 }
