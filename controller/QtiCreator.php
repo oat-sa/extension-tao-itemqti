afe79127202ba77179e63abfadae28cb7f55ea14
antoinerobin <antoine.robin@vesperiagroup.com>
2014-11-17 16:10:10 +0100

    get the right file on item authoring

--- controller/QtiCreator.php
+++ controller/QtiCreator.php
@@ -22,6 +22,7 @@
 namespace oat\taoQtiItem\controller;
 
 use \core_kernel_classes_Resource;
+use oat\taoMediaManager\model\SimpleFileManagement;
 use oat\taoQtiItem\model\qti\Service;
 use oat\taoQtiItem\helpers\Authoring;
 use \taoItems_models_classes_ItemsService;
@@ -136,6 +137,7 @@ class QtiCreator extends tao_actions_CommonModule
             $relPath = urldecode($this->getRequestParameter('relPath'));
 
             $this->renderFile($rdfItem, $relPath, $lang);
+
         }
     }
 
@@ -143,7 +145,24 @@ class QtiCreator extends tao_actions_CommonModule
 
         $folder = taoItems_models_classes_ItemsService::singleton()->getItemFolder($item, $lang);
         if(tao_helpers_File::securityCheck($path, true)){
-            $filename = $folder.$path;
+            if(strpos($path, '/') === 0){
+                $path = substr($path, 1);
+            }
+
+            $identifier = substr($path, 0, strpos($path, '/'));
+            $subPath = substr($path, strpos($path, '/') + 1);
+
+            if($identifier === '' || $identifier === 'local'){
+                $filename = $folder.$subPath;
+            }
+            else if($identifier === 'mediamanager'){
+                $fileManager = new SimpleFileManagement();
+                $filename = $fileManager->retrieveFile($subPath);
+            }
+            else{
+                $filename = $folder.$path;
+            }
+
             //@todo : find better way to to this
             //load amd module
             if(!file_exists($filename) && file_exists($filename.'.js')){
