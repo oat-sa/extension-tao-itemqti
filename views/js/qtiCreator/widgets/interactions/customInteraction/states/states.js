9af255ca4905214d0372c4a7ff8849f3db32606f
ssipasseuth <sam@taotesting.com>
2014-11-28 13:37:04 +0100

    added new question state for custom interaction with integration of color picker

--- views/js/qtiCreator/widgets/interactions/customInteraction/states/states.js
+++ views/js/qtiCreator/widgets/interactions/customInteraction/states/states.js
@@ -1,6 +1,7 @@
 define([
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/interactions/states/states',
+    'taoQtiItem/qtiCreator/widgets/interactions/customInteraction/states/Question',
     'taoQtiItem/qtiCreator/widgets/interactions/customInteraction/states/Sleep'
 ], function(factory, states){
     return factory.createBundle(states, arguments);
