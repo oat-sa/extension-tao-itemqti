180b965b9d0daaa46483af56f9dad9f4d97494ea
Bertrand Chevrier <bertrand@taotesting.com>
2014-12-18 17:02:24 +0100

    update qti item runner build

--- views/build/grunt/bundle.js
+++ views/build/grunt/bundle.js
@@ -67,6 +67,7 @@ module.exports = function(grunt) {
             baseUrl : '../js',
             mainConfigFile : './config/requirejs.build.js',
             //optimize: "none",
+            findNestedDependencies : true,
             uglify2: {
                 mangle : false,
                 output: {
@@ -81,7 +82,7 @@ module.exports = function(grunt) {
             inlineCss : true,
             paths : { 'taoQtiItem' : root + '/taoQtiItem/views/js', 'taoQtiItemCss' :  root + '/taoQtiItem/views/css', 'taoItems' : root + '/taoItems/views/js', taoCss : root + '/tao/views/css'},
             include: runtimeLibs.concat(['tpl', 'json']),
-            exclude : ['mathJax', 'mediaElement'],
+            exclude : ['mathJax'],
             name: "taoQtiItem/runner/qtiItemRunner",
             out: "output/qtiItemRunner.min.js"
         }
