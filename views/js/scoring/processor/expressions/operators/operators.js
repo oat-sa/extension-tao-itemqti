bab075884708ab04eba8f3874862d2ef5acf0494
Michael <Komarouski@1pt.com>
2015-02-09 18:22:53 +0300

    introduction of gt operator

--- views/js/scoring/processor/expressions/operators/operators.js
+++ views/js/scoring/processor/expressions/operators/operators.js
@@ -22,10 +22,11 @@
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
 define([
+    'taoQtiItem/scoring/processor/expressions/operators/gt',
     'taoQtiItem/scoring/processor/expressions/operators/product',
     'taoQtiItem/scoring/processor/expressions/operators/subtract',
     'taoQtiItem/scoring/processor/expressions/operators/sum'
-], function(product, subtract, sum ){
+], function( gt, product, subtract, sum ){
     'use strict';
 
     /**
@@ -47,6 +48,7 @@ define([
      * @exports taoQtiItem/scoring/processor/expressions/operators/operators
      */
     return {
+        "gt"        : gt,
         "product"   : product,
         "subtract"  : subtract,
         "sum"       : sum
