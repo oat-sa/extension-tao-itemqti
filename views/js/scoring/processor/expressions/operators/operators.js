a2c94445ee27ca5e53319d86c369f33a55ccd92c
Michael <Komarouski@1pt.com>
2015-02-09 16:01:57 +0300

    introduction of min operator

--- views/js/scoring/processor/expressions/operators/operators.js
+++ views/js/scoring/processor/expressions/operators/operators.js
@@ -22,10 +22,11 @@
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
 define([
+    'taoQtiItem/scoring/processor/expressions/operators/min',
     'taoQtiItem/scoring/processor/expressions/operators/product',
     'taoQtiItem/scoring/processor/expressions/operators/subtract',
     'taoQtiItem/scoring/processor/expressions/operators/sum'
-], function(product, subtract, sum ){
+], function( min, product, subtract, sum ){
     'use strict';
 
     /**
@@ -47,6 +48,7 @@ define([
      * @exports taoQtiItem/scoring/processor/expressions/operators/operators
      */
     return {
+        "min"       : min,
         "product"   : product,
         "subtract"  : subtract,
         "sum"       : sum
