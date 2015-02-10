d635bca22d8d9649add0622e7584a8ecdf5d9058
ssipasseuth <sam@taotesting.com>
2015-02-10 14:00:17 +0100

    removed call for sizeFinder

--- views/js/qtiCommonRenderer/renderers/ModalFeedback.js
+++ views/js/qtiCommonRenderer/renderers/ModalFeedback.js
@@ -2,10 +2,9 @@ define([
     'lodash',
     'tpl!taoQtiItem/qtiCommonRenderer/tpl/modalFeedback',
     'taoQtiItem/qtiCommonRenderer/helpers/Helper',
-    'taoQtiItem/qtiCommonRenderer/helpers/sizeFinder',
     'ui/waitForMedia',
     'ui/modal'
-], function(_, tpl, Helper, sizeFinder){
+], function(_, tpl, Helper){
 
     'use strict';
 
@@ -14,8 +13,7 @@ define([
         template : tpl,
         getContainer : Helper.getContainer,
         minHeight : 200,
-        minWidth : 400,
-        maxWidth : 800,
+        width : 600,
         render : function(modalFeedback, data){
 
             data = data || {};
@@ -23,27 +21,25 @@ define([
             var $modal = Helper.getContainer(modalFeedback);
 
             $modal.waitForMedia(function(){
-                
-                //when we are sure that media is loaded:
-                sizeFinder.measure($modal, function(size){
 
-                    $modal.modal({
-                        startClosed : false,
-                        minHeight : Math.max(size.height, modalFeedbackRenderer.minHeight),
-                        width : Math.max(Math.min(size.width, modalFeedbackRenderer.maxWidth), modalFeedbackRenderer.minWidth)
-                    });
+                //when we are sure that media is loaded:
+                $modal.on('opened.modal', function(){
 
                     //set item body height
                     var $itemBody = Helper.getContainer(modalFeedback.getRelatedItem()).children('.qti-itemBody');
-                    if($modal.height() > $itemBody.height()){
-                        $itemBody.height($modal.height());
+                    var requiredHeight = $modal.outerHeight() + parseInt($modal.css('top'));
+                    if(requiredHeight > $itemBody.height()){
+                        $itemBody.height(requiredHeight);
                     }
 
-                    $modal.on('closed.modal', function(){
-                        if(_.isFunction(data.callback)){
-                            data.callback.call(this);
-                        }
-                    });
+                }).on('closed.modal', function(){
+                    if(_.isFunction(data.callback)){
+                        data.callback.call(this);
+                    }
+                }).modal({
+                    startClosed : false,
+                    minHeight : modalFeedbackRenderer.minHeight,
+                    width : modalFeedbackRenderer.width
                 });
             });
 
