b27cbbc6c60aedfaddb052daf5d7beab2331ba76
Michael <Komarouski@1pt.com>
2015-02-19 18:11:14 +0300

    integerToFloat operator introduced

--- views/js/scoring/processor/expressions/operators/operators.js
+++ views/js/scoring/processor/expressions/operators/operators.js
@@ -26,6 +26,7 @@ define([
     'taoQtiItem/scoring/processor/expressions/operators/divide',
     'taoQtiItem/scoring/processor/expressions/operators/gt',
     'taoQtiItem/scoring/processor/expressions/operators/gte',
+    'taoQtiItem/scoring/processor/expressions/operators/integerToFloat',
     'taoQtiItem/scoring/processor/expressions/operators/isNull',
     'taoQtiItem/scoring/processor/expressions/operators/lt',
     'taoQtiItem/scoring/processor/expressions/operators/lte',
@@ -41,7 +42,7 @@ define([
     'taoQtiItem/scoring/processor/expressions/operators/subtract',
     'taoQtiItem/scoring/processor/expressions/operators/sum',
     'taoQtiItem/scoring/processor/expressions/operators/truncate'
-], function( and, divide, gt, gte, isNull, lt, lte, match, max, min, not, or, power, product, round, substring, subtract, sum, truncate ){
+], function( and, divide, gt, gte, integerToFloat, isNull, lt, lte, match, max, min, not, or, power, product, round, substring, subtract, sum, truncate ){
     'use strict';
 
     /**
@@ -64,24 +65,25 @@ define([
      * @exports taoQtiItem/scoring/processor/expressions/operators/operators
      */
     return {
-        "and"       : and,
-        "divide"    : divide,
-        "gt"        : gt,
-        "gte"       : gte,
-        "isNull"    : isNull,
-        "lt"        : lt,
-        "lte"       : lte,
-        "match"     : match,
-        "max"       : max,
-        "min"       : min,
-        "not"        : not,
-        "or"        : or,
-        "power"     : power,
-        "product"   : product,
-        "round"     : round,
-        "substring" : substring,
-        "subtract"  : subtract,
-        "sum"       : sum,
-        "truncate"  : truncate
+        "and"            : and,
+        "divide"         : divide,
+        "gt"             : gt,
+        "gte"            : gte,
+        "integerToFloat" : integerToFloat,
+        "isNull"         : isNull,
+        "lt"             : lt,
+        "lte"            : lte,
+        "match"          : match,
+        "max"            : max,
+        "min"            : min,
+        "not"            : not,
+        "or"             : or,
+        "power"          : power,
+        "product"        : product,
+        "round"          : round,
+        "substring"      : substring,
+        "subtract"       : subtract,
+        "sum"            : sum,
+        "truncate"       : truncate
     };
 });
