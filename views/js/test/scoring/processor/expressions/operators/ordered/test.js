0f44be69c4b14ab1433443e47ab882ee594be38c
Michael <Komarouski@1pt.com>
2015-02-20 12:00:29 +0300

    ordered operator introduced TAO-415

---
+++ views/js/test/scoring/processor/expressions/operators/ordered/test.js
@@ -0,0 +1,81 @@
+define([
+    'lodash',
+    'taoQtiItem/scoring/processor/expressions/operators/ordered'
+], function(_, orderedProcessor){
+    'use strict';
+    
+    module('API');
+
+    QUnit.test('structure', function(assert){
+        assert.ok(_.isPlainObject(orderedProcessor), 'the processor expose an object');
+        assert.ok(_.isFunction(orderedProcessor.process), 'the processor has a process function');
+        assert.ok(_.isArray(orderedProcessor.operands), 'the processor has operands');
+    });
+
+    module('Process');
+
+    var dataProvider = [{
+        title : 'ordered integer',
+        operands : [{
+            cardinality : 'ordered',
+            baseType : 'integer',
+            value: [2, 3, 7]
+        },{
+            cardinality : 'single',
+            baseType : 'integer',
+            value: 5
+        }],
+        expectedResult : {
+            cardinality : 'ordered',
+            baseType : 'integer',
+            value: [2, 3, 7, 5]
+        }
+    },{
+        title : 'ordered integer with nulls',
+        operands : [{
+            cardinality : 'ordered',
+            baseType : 'integer',
+            value: [2, 3, 7]
+        },null,{
+            cardinality : 'single',
+            baseType : 'integer',
+            value: 5
+        },{
+            cardinality : 'single',
+            baseType : 'integer',
+            value: 5
+        }],
+        expectedResult : {
+            cardinality : 'ordered',
+            baseType : 'integer',
+            value: [2, 3, 7, 5, 5]
+        }
+    },{
+        title : 'different baeTypes',
+        operands : [{
+            cardinality : 'ordered',
+            baseType : 'float',
+            value: [2, 3, 7]
+        },{
+            cardinality : 'single',
+            baseType : 'integer',
+            value: [5]
+        }],
+        expectedResult : null
+    },{
+        title : 'null operand',
+        operands : [null,null],
+        expectedResult : null
+    },{
+        title : 'no operands',
+        operands : [],
+        expectedResult : null
+    }];
+
+    QUnit
+      .cases(dataProvider)
+      .test('ordered ', function(data, assert){
+        orderedProcessor.operands = data.operands;
+        assert.deepEqual(orderedProcessor.process(), data.expectedResult, 'The ordered is correct');
+    });
+});
