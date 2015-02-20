7e78eb1f2dee192aa31447916be30b735fb81731
Michael <Komarouski@1pt.com>
2015-02-20 15:33:59 +0300

    multiple operator introduced TAO-415

--- views/js/scoring/processor/expressions/operators/operators.js
+++ views/js/scoring/processor/expressions/operators/operators.js
@@ -32,6 +32,7 @@ define([
     'taoQtiItem/scoring/processor/expressions/operators/match',
     'taoQtiItem/scoring/processor/expressions/operators/max',
     'taoQtiItem/scoring/processor/expressions/operators/min',
+    'taoQtiItem/scoring/processor/expressions/operators/multiple',
     'taoQtiItem/scoring/processor/expressions/operators/not',
     'taoQtiItem/scoring/processor/expressions/operators/or',
     'taoQtiItem/scoring/processor/expressions/operators/power',
@@ -41,7 +42,7 @@ define([
     'taoQtiItem/scoring/processor/expressions/operators/subtract',
     'taoQtiItem/scoring/processor/expressions/operators/sum',
     'taoQtiItem/scoring/processor/expressions/operators/truncate'
-], function( and, divide, gt, gte, isNull, lt, lte, match, max, min, not, or, power, product, round, substring, subtract, sum, truncate ){
+], function( and, divide, gt, gte, isNull, lt, lte, match, max, min, multiple, not, or, power, product, round, substring, subtract, sum, truncate ){
     'use strict';
 
     /**
@@ -64,24 +65,25 @@ define([
      * @exports taoQtiItem/scoring/processor/expressions/operators/operators
      */
     return {
-        "and"       : and,
-        "divide"    : divide,
-        "gt"        : gt,
-        "gte"       : gte,
-        "isNull"    : isNull,
-        "lt"        : lt,
-        "lte"       : lte,
-        "match"     : match,
-        "max"       : max,
-        "min"       : min,
-        "not"        : not,
-        "or"        : or,
-        "power"     : power,
-        "product"   : product,
-        "round"     : round,
-        "substring" : substring,
-        "subtract"  : subtract,
-        "sum"       : sum,
-        "truncate"  : truncate
+        "and"           : and,
+        "divide"        : divide,
+        "gt"            : gt,
+        "gte"           : gte,
+        "isNull"        : isNull,
+        "lt"            : lt,
+        "lte"           : lte,
+        "match"         : match,
+        "max"           : max,
+        "min"           : min,
+        "multiple"      : multiple,
+        "not"           : not,
+        "or"            : or,
+        "power"         : power,
+        "product"       : product,
+        "round"         : round,
+        "substring"     : substring,
+        "subtract"      : subtract,
+        "sum"           : sum,
+        "truncate"      : truncate
     };
 });
