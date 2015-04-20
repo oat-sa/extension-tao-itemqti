5a8f3410bb950b03d520b1515ae1181d01c337a9
Joel Bout <joel@taotesting.com>
2015-04-20 16:20:14 +0200

    More elaborate isUrl check

--- views/js/qtiItem/helper/util.js
+++ views/js/qtiItem/helper/util.js
@@ -19,8 +19,14 @@ define(['lodash'], function(_){
             if(src){
 
                 src = src.replace(/^\//, '');
-
-                if(!src.match(/^http/i)){
+                
+                var pattern = new RegExp('^(https?:\\/\\/)?'+ // protocol
+                        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|'+ // domain name
+                        '((\\d{1,3}\\.){3}\\d{1,3}))'+ // OR ip (v4) address
+                        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*'+ // port and path
+                        '(\\?[;&a-z\\d%_.~+=-]*)?'+ // query string
+                        '(\\#[-a-z\\d_]*)?$','i'); // fragment locator
+                if(!pattern.test(src)){
 
                     if(baseUrl && !baseUrl.match(/\/$/)){
                         baseUrl += '/';
