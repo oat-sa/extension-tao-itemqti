f4a16f2278e46189d8e4adfb4b9ec6ef2d400251
ssipasseuth <sam@taotesting.com>
2015-04-21 13:32:40 +0200

    fixed xinclude xml renderer

--- views/js/qtiXmlRenderer/renderers/Include.js
+++ views/js/qtiXmlRenderer/renderers/Include.js
@@ -1,6 +1,16 @@
-define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/element'], function(tpl){
+define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/include'], function(tpl){
     return {
         qtiClass : 'include',
-        template : tpl
+        template : tpl,
+        getData : function(xi, data){
+
+            var ns = xi.getNamespace();
+
+            if(ns && ns.name){
+                data.tag = ns.name + ':include';
+            }
+
+            return data;
+        }
     };
 });
