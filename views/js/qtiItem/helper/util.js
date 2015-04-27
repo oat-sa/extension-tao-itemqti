bbcbabfbb5382f9a527cc29a76b1f17e60ad1708
ssipasseuth <sam@taotesting.com>
2015-04-28 00:14:11 +0200

    fix addMarkupNamespace, to ignore already namespaced tag name

--- views/js/qtiItem/helper/util.js
+++ views/js/qtiItem/helper/util.js
@@ -90,7 +90,14 @@ define(['lodash'], function(_){
         },
         addMarkupNamespace : function addMarkupNamespace(markup, ns){
             if(ns) {
-                markup = markup.replace(/<(\/)?([a-z]+)(\s?)([^><]*)>/g, '<$1' + ns + ':$2$3$4>');
+                markup = markup.replace(/<(\/)?([a-z:]+)(\s?)([^><]*)>/g, function($0, $1, $2, $3, $4){
+                    if($2.indexOf(':')>0){
+                        return $0;
+                    }
+                    $1 = $1 || '';
+                    $3 = $3 || '';
+                    return '<'+ $1 + ns + ':'+$2+$3+$4+'>';
+                });
                 return markup;
             }
             return markup;
