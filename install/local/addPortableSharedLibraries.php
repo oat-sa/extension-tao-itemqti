6d6b2b9b8958ce1a93f02359f7e46627b4316e94
hutnikau <hutnikau@1pt.com>
2015-05-07 17:34:25 +0300

    sts event manager

--- install/local/addPortableSharedLibraries.php
+++ install/local/addPortableSharedLibraries.php
@@ -44,3 +44,4 @@ $registry->registerFromFile('OAT/interact', $installBasePath . '/OAT/interact.js
 $registry->registerFromFile('OAT/interact-rotate', $installBasePath . '/OAT/interact-rotate.js');
 $registry->registerFromFile('OAT/sts/transform-helper', $installBasePath . '/OAT/sts/transform-helper.js');
 $registry->registerFromFile('OAT/handlebars', $installBasePath . '/OAT/handlebars.js');
+$registry->registerFromFile('OAT/stsEventManager', $installBasePath . '/OAT/sts/stsEventManager.js');
