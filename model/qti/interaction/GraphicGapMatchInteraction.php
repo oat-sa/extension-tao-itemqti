39d1871a6b47a4d81ebf850282f67fe8cfcf8e24
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-05-11 16:00:10 +0200

    missing return statement

--- model/qti/interaction/GraphicGapMatchInteraction.php
+++ model/qti/interaction/GraphicGapMatchInteraction.php
@@ -88,6 +88,7 @@ class GraphicGapMatchInteraction extends GraphicInteraction
     public function toArray($filterVariableContent = false, &$filtered = array()){
         $data = parent::toArray($filterVariableContent, $filtered);
         $data['gapImgs'] = $this->getArraySerializedElementCollection($this->getGapImgs(), $filterVariableContent, $filtered);
+        return $data;
     }
 
     public static function getTemplateQti(){
