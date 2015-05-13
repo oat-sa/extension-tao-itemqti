4661dac29ce0a1615b19a489e3ba720eb5f1dca0
ssipasseuth <sam@taotesting.com>
2015-05-13 16:28:42 +0200

    encode image source before saving xml

--- views/js/qtiXmlRenderer/renderers/Img.js
+++ views/js/qtiXmlRenderer/renderers/Img.js
@@ -1,6 +1,10 @@
-define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/element'], function(tpl){
+define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/element', 'lodash'], function(tpl, _){
     return {
         qtiClass : 'img',
-        template : tpl
+        template : tpl,
+        getData : function(img, data){
+            data.attributes.src = _.escape(data.attributes.src);
+            return data;
+        }
     };
 });
