ee3b7be0a19e8a37c9eb97407a839dc3f011f938
ssipasseuth <sam@taotesting.com>
2015-05-29 11:16:40 +0200

    added method getAccessElementsByInclusionOrder()

--- views/js/apipCreator/api/apipItem.js
+++ views/js/apipCreator/api/apipItem.js
@@ -19,7 +19,7 @@
 define([
     'lodash',
     'taoQtiItem/apipCreator/helper/parser',
-    'taoQtiItem/apipCreator/helper/'
+    'taoQtiItem/apipCreator/helper/serializer'
 ], function(_, parser, serializer){
 
     'use strict';
@@ -31,57 +31,58 @@ define([
      * @returns {Object}
      */
     function ApipItem(apipItemXML){
-
-        //apipItem is an private variable in this scope, which holds the authoring model
-        var apipItem = parser.parse(apipItemXML);
-
-        /**
-         * Get a clone of the parsed item body
-         * This will be used to generate the (main) item selecting view for the apip authoring tool
-         * 
-         * @returns {Object}
-         */
-        function getItemBodyModel(){
-            return _.cloneDeep(apipItem.itemBody);
-        }
-
-        /**
-         * Find the qti element identified by its serial
-         * 
-         * @param {String} qtiElementSerial
-         * @returns {Object}
-         */
-        function getQtiElementBySerial(qtiElementSerial){
-            return {};
-        }
-
-        /**
-         * Get the access element identified by its serial
-         * 
-         * @param {String} accessElementSerial
-         * @returns {Object}
-         */
-        function getAccessElementBySerial(accessElementSerial){
-            return {};
-        }
-
-        /**
-         * Serialize the authoring model into XML for saving
-         * 
-         * @returns {String}
-         */
-        function toXML(){
-            return serializer.serialize(apipItem);
-        }
-
-        return {
-            getItemBodyModel : getItemBodyModel,
-            getQtiElementBySerial : getQtiElementBySerial,
-            getAccessElementBySerial : getAccessElementBySerial,
-            toXML : toXML
-        };
-
+        this.apipItem = parser.parse(apipItemXML);
     }
+
+    /**
+     * Get a clone of the parsed item body
+     * This will be used to generate the (main) item selecting view for the apip authoring tool
+     * 
+     * @returns {Object}
+     */
+    ApipItem.prototype.getItemBodyModel = function getItemBodyModel(){
+        return _.cloneDeep(this.apipItem.itemBody);
+    };
+
+    /**
+     * Find the qti element identified by its serial
+     * 
+     * @param {String} qtiElementSerial
+     * @returns {Object}
+     */
+    ApipItem.prototype.getQtiElementBySerial = function getQtiElementBySerial(qtiElementSerial){
+        return {};
+    };
+
+    /**
+     * Get the access element identified by its serial
+     * 
+     * @param {String} accessElementSerial
+     * @returns {Object}
+     */
+    ApipItem.prototype.getAccessElementBySerial = function getAccessElementBySerial(accessElementSerial){
+        return {};
+    };
+
+    /**
+     * Get the sorted array of accessElements referenced in the inclusion order
+     * The accessElements are sorted according to the order attribute in the inclusionOrder
+     * 
+     * @param {String} inclusionOrderType
+     * @returns {Array}
+     */
+    ApipItem.prototype.getAccessElementsByInclusionOrder = function getAccessElementsByInclusionOrder(inclusionOrderType){
+        return [];
+    };
+
+    /**
+     * Serialize the authoring model into XML for saving
+     * 
+     * @returns {String}
+     */
+    ApipItem.prototype.createAccessElementInfo = function toXML(){
+        return serializer.serialize(this.apipItem);
+    };
 
     return ApipItem;
 });
