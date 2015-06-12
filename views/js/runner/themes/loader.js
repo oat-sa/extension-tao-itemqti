21450fb3994cf4625149a4109a274aa8cae344d4
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2015-06-12 21:45:50 +0200

    disable link before attaching to prevent chrome strange behavior

--- views/js/runner/themes/loader.js
+++ views/js/runner/themes/loader.js
@@ -166,6 +166,7 @@ define(['jquery', 'lodash'], function($, _){
             load : function load(){
                 _.forEach(styles, function($link, id){
                     if(!isAttached(id)){
+/bin/bash: q : commande introuvable
                         $container.append($link);
                     }
                     if(id !== 'base' && id !== defaultTheme){
