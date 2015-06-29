b14d93b3dc0fe69cc02a0a5b9df8103dae8fd963
ssipasseuth <sam@taotesting.com>
2015-06-29 15:24:26 +0200

    update choice identifier in the <input>

--- views/js/qtiCreator/widgets/helpers/identifier.js
+++ views/js/qtiCreator/widgets/helpers/identifier.js
@@ -1,7 +1,6 @@
 define(['lodash'], function(_){
 
     var _updateChoiceIdentifierInResponse = function(response, oldId, newId){
-
         
         var escapedOldId = oldId.replace(/([.-])/g, '\\$1'),//escape spec characters allowed in the standard and that is meaningful in regex
             regex = new RegExp('([^\\s]*\\s+|^)(' + escapedOldId + ')(\\s+[^\\s]*|$)');//prepare the regex to watch the oldId to be replaced
@@ -36,7 +35,9 @@ define(['lodash'], function(_){
                 
             //FIXME some choices may not have a renderer, so we catch the thrown exception silently
             try{
-                choice.getContainer(null, choice.qtiClass+'.'+interaction.qtiClass).attr('data-identifier', choice.id());
+                var $choiceContainer = choice.getContainer(null, choice.qtiClass+'.'+interaction.qtiClass);
+                $choiceContainer.attr('data-identifier', choice.id());
+                $choiceContainer.find('input').val(choice.id());
             } catch(ex){}
 
         }
