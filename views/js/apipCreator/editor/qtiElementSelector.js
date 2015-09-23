28c6040cd0f97eba171da2448682de7dfeb39fe4
ssipasseuth <sam@taotesting.com>
2015-09-23 11:44:15 +0200

    added more event triggers

--- views/js/apipCreator/editor/qtiElementSelector.js
+++ views/js/apipCreator/editor/qtiElementSelector.js
@@ -169,6 +169,8 @@ define([
                 activateElement($element, $container);
             }
 
+        }).on('click'+_ns, function(){
+            deactivateElement($currentActive, $container);
         });
 
         return {
