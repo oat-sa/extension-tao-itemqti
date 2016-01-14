2c4bf7778881316585c2a3b61b7efccb64de670c
ssipasseuth <sam@taotesting.com>
2016-01-14 09:45:35 +0100

    unit test : added file content verification

--- test/update/ItemUpdateInlineFeedbackTest.php
+++ test/update/ItemUpdateInlineFeedbackTest.php
@@ -17,35 +17,52 @@
  * Copyright (c) 2016 (original work) Open Assessment Technologies SA ;
  *
  */
+
 namespace oat\taoQtiItem\test\update;
 
 use oat\tao\test\TaoPhpUnitTestRunner;
 use oat\taoQtiItem\scripts\update\ItemUpdateInlineFeedback;
 
-/**
- * Description of ItemUpdateInlineFeedback
- *
- * @author sam
- */
 class ItemUpdateInlineFeedbackTest extends TaoPhpUnitTestRunner
 {
+
     /**
      * tests initialization
      * load qti service
      */
-    public function setUp(){
+    public function setUp()
+    {
         TaoPhpUnitTestRunner::initTest();
     }
 
-    public function testProcess(){
-        $itemUpdater = new ItemUpdateInlineFeedback(ROOT_PATH . 'taoQtiItem/test/update/samples/itemData');
-        $itemUpdater->process();
-        $checkedFiles = $itemUpdater->getCheckedFiles();
-        $modifiedFiles = array_keys(array_filter($checkedFiles, function($v){return $v;}));
+    public function testProcess()
+    {
+        $itemUpdater   = new ItemUpdateInlineFeedback(ROOT_PATH.'taoQtiItem/test/update/samples/itemData');
+        $items = $itemUpdater->process();
+        $checkedFiles  = $itemUpdater->getCheckedFiles();
+        $modifiedFiles = array_keys(array_filter($checkedFiles, function($v) {
+                return $v;
+            }));
 
-        var_dump($checkedFiles, $modifiedFiles);
-        
-        $this->assertEquals(1, count($modifiedFiles));
-        $this->assertTrue(strpos($modifiedFiles[0], 'taoQtiItem/test/update/samples/itemData/i1452699358831159_hasModal_willChange/itemContent/en-US/qti.xml') > 0);
+        $this->assertEquals(138, count($checkedFiles));
+        $this->assertEquals(2, count($modifiedFiles));
+        $this->assertTrue(strpos($modifiedFiles[0],
+                'taoQtiItem/test/update/samples/itemData/i1452759848383063_hasModal_willChange/itemContent/en-US/qti.xml') > 0);
+        $this->assertTrue(strpos($modifiedFiles[1],
+                'taoQtiItem/test/update/samples/itemData/i1452699358831159_hasModal_willChange/itemContent/en-US/qti.xml') > 0);
+
+        $item1 = $items[$modifiedFiles[0]];
+        $item2 = $items[$modifiedFiles[1]];
+
+        $this->assertTrue($item1 instanceof \oat\taoQtiItem\model\qti\Item);
+        $this->assertTrue($item2 instanceof \oat\taoQtiItem\model\qti\Item);
+
+        $itemStr1 = $item1->toXML();
+        $itemStr2 = $item2->toXML();//note : item2 is clone of item1
+
+        //compare the content of the items after update
+        $resultFile = dirname(__FILE__).'/samples/updateResult.xml';
+        $this->assertEquals(file_get_contents($resultFile), trim($itemStr1));
+        $this->assertEquals(file_get_contents($resultFile), trim($itemStr2));
     }
 }
