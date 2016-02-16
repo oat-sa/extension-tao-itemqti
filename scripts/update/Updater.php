17098a85e26eb86fe4a10f8e9d6fc9a3b6843882
antoinerobin <antoine.robin@vesperiagroup.com>
2016-02-16 10:02:58 +0100

    validate items and test with right xsd

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -218,6 +218,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $validation = array(
                 'default' => array(
                     __DIR__.'/../../model/qti/data/imscp_v1p1.xsd',
+                    __DIR__.'/../../model/qti/data/apipv1p0/Core_Level/Package/apipv1p0_imscpv1p2_v1p0.xsd'
                 )
             );
             $ext->setConfig('manifestValidation', $validation);
