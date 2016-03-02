792ebf8cfddfb3eb5ef895ea9bebb63283cc6d08
ssipasseuth <sam@taotesting.com>
2016-03-02 21:49:29 +0100

    escape special char in choice editing

--- views/js/qtiCreator/widgets/interactions/containerInteraction/states/Question.js
+++ views/js/qtiCreator/widgets/interactions/containerInteraction/states/Question.js
@@ -1,4 +1,22 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ */
 define([
+    'lodash',
     'jquery',
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/interactions/blockInteraction/states/Question',
@@ -7,8 +25,10 @@ define([
     'taoQtiItem/qtiCreator/widgets/helpers/content',
     'taoQtiItem/qtiCreator/widgets/helpers/textWrapper',
     'tpl!taoQtiItem/qtiCreator/tpl/toolbars/htmlEditorTrigger'
-], function($, stateFactory, Question, htmlEditor, gridContentHelper, htmlContentHelper, textWrapper, toolbarTpl){
-
+], function(_, $, stateFactory, Question, htmlEditor, gridContentHelper, htmlContentHelper, textWrapper, toolbarTpl){
+    
+    'use strict';
+    
     var ContainerInteractionStateQuestion = stateFactory.extend(Question, function(){
 
         this.buildEditor();
@@ -113,7 +133,7 @@ define([
 
                 newGapWidget.changeState('question');
                 textWrapper.create($editable);
-                gapModel.afterCreate(widget, newGapWidget, text);
+                gapModel.afterCreate(widget, newGapWidget, _.escape(text));
             });
             
         }).on('mouseup', function(e){
