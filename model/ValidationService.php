15ff01ce9240e567b12f7c7576f9ffda310e9a95
antoinerobin <antoine.robin@vesperiagroup.com>
2016-03-02 10:25:36 +0100

    add doc, get correct path in the constructor

--- model/ValidationService.php
+++ model/ValidationService.php
@@ -30,35 +30,63 @@ class ValidationService extends ConfigurableService
 
     private $contentValidation = array(
         'http://www.imsglobal.org/xsd/imsqti_v2p0' => array(
-            __DIR__.'/qti/data/qtiv2p0/imsqti_v2p0.xsd'
+            '/qti/data/qtiv2p0/imsqti_v2p0.xsd'
         ),
         'http://www.imsglobal.org/xsd/apip/apipv1p0/qtiitem/imsqti_v2p1' => array(
-                __DIR__.'/qti/data/apipv1p0/Core_Level/Package/apipv1p0_qtiitemv2p1_v1p0.xsd'
+                '/qti/data/apipv1p0/Core_Level/Package/apipv1p0_qtiitemv2p1_v1p0.xsd'
         ),
         'default' => array(
-                __DIR__.'/qti/data/qtiv2p1/imsqti_v2p1.xsd'
+                '/qti/data/qtiv2p1/imsqti_v2p1.xsd'
         )
     );
 
     private $manifestValidation = array(
         'default' => array(
-                __DIR__.'/qti/data/imscp_v1p1.xsd',
-                __DIR__.'/qti/data/apipv1p0/Core_Level/Package/apipv1p0_imscpv1p2_v1p0.xsd'
+                '/qti/data/imscp_v1p1.xsd',
+                '/qti/data/apipv1p0/Core_Level/Package/apipv1p0_imscpv1p2_v1p0.xsd'
         )
     );
 
+    public function __construct(array $options = array())
+    {
+        parent::__construct($options);
+        foreach($this->contentValidation as $key => &$array){
+            foreach($array as &$value){
+                $value = __DIR__.$value;
+            }
+        }
+        foreach($this->manifestValidation as $key => &$array){
+            foreach($array as &$value){
+                $value = __DIR__.$value;
+            }
+        }
+    }
+
+    /**
+     * @param string $key the namespace of content to validate
+     * @return array of schema for content validation
+     */
     public function getContentValidationSchema($key){
         $validationArray = $this->getContentValidation();
 
         return $this->getSchemas($validationArray, $key);
     }
 
+    /**
+     * @param string $key the namespace of manifest to validate
+     * @return array of schema for manifest validation
+     */
     public function getManifestValidationSchema($key){
         $validationArray = $this->getManifestValidation();
 
         return $this->getSchemas($validationArray, $key);
     }
 
+    /**
+     * @param array $validationArray list of xsds for namespaces
+     * @param string $key the namespace
+     * @return array schemas for validation
+     */
     protected function getSchemas($validationArray, $key){
         if(isset($validationArray[$key])){
             return $validationArray[$key];
@@ -67,10 +95,16 @@ class ValidationService extends ConfigurableService
         return $validationArray['default'];
     }
 
+    /**
+     * @return array
+     */
     protected function getContentValidation(){
         return $this->contentValidation;
     }
 
+    /**
+     * @return array
+     */
     protected function getManifestValidation(){
         return $this->manifestValidation;
     }
