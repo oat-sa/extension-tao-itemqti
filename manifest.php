55ede25677d54b85cb1b4195bebab92f8d792b67
Christophe Noël <christophe@taotesting.com>
2016-03-21 16:36:57 +0100

    bumped version to 2.12.9

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return array(
     'label' => 'QTI item model',
 	'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '2.12.8',
+    'version' => '2.12.9',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
 	    'taoItems' => '>=2.6'
--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -208,6 +208,9 @@ class Updater extends \common_ext_ExtensionUpdater
         if($currentVersion === '2.12.7'){
             $currentVersion = '2.12.8';
         }
+        if($currentVersion === '2.12.8'){
+            $currentVersion = '2.12.9';
+        }
         return $currentVersion;
     }
 
