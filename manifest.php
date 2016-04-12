550e8d71c7ea6a0df9130ad8ea40972f57def8a9
Alexander Zagovorichev <zagovorichev@1pt.com>
2016-04-12 13:17:33 +0300

    Fixed version in manifest

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return array(
     'label' => 'QTI item model',
 	'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '2.21.0',
+    'version' => '2.20.2',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
 	    'taoItems' => '>=2.13',
