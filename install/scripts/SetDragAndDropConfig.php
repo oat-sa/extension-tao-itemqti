6b144c219588edfdc02d3df4edb5a695fbf0fe15
Christophe Noël <christophe@taotesting.com>
2016-05-03 14:23:27 +0200

    removed old tests

--- install/scripts/SetDragAndDropConfig.php
+++ install/scripts/SetDragAndDropConfig.php
@@ -28,7 +28,7 @@ class SetDragAndDropConfig extends \common_ext_action_InstallAction
         ClientLibConfigRegistry::getRegistry()->register(
             'taoQtiItem/qtiCommonRenderer/renderers/config',
             array(
-                'gapMatchDragAndDrop' => false
+                'gapMatchDragAndDrop' => true
             )
         );
         return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Drag and drop configuration saved');
