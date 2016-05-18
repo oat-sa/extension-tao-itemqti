b1bacacefcff40468df78a5c0a7a61537ef719e0
antoinerobin <antoine.robin@vesperiagroup.com>
2016-05-18 16:30:01 +0200

    update version number and required taoItem version

--- manifest.php
+++ manifest.php
@@ -26,10 +26,10 @@ return array(
     'label' => 'QTI item model',
 	'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '2.12.14',
+    'version' => '2.12.15',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
-	    'taoItems' => '>=2.9.7'
+	    'taoItems' => '>=2.9.8'
 	),
 	'models' => array(
 		'http://www.tao.lu/Ontologies/TAOItem.rdf'
