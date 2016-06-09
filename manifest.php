1aeffd67bf1518ac01cb6e5e905120b986c166fe
Christophe Noël <christophe@taotesting.com>
2016-06-09 17:04:02 +0200

    bumped version to 2.12.18

--- manifest.php
+++ manifest.php
@@ -26,7 +26,7 @@ return array(
     'label' => 'QTI item model',
 	'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '2.12.17',
+    'version' => '2.12.18',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
 	    'taoItems' => '>=2.9.8'
