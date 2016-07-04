c5426fe9fe63b0099c16588da429d8485e8faf3d
Alexander Zagovorichev <zagovorichev@gmail.com>
2016-07-04 16:32:36 +0300

    increase ext version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label' => 'QTI item model',
 	'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '2.37.0',
+    'version' => '2.37.1',
 	'author' => 'Open Assessment Technologies',
 	'requires' => array(
         'taoItems' => '>=2.17.0',
