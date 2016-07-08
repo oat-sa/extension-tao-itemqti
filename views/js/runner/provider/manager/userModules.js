40962b78d30c05e435a420bb2d4058bdd142ec92
Christophe Noël <christophe@taotesting.com>
2016-07-08 16:10:02 +0200

    userModules loaded after item rendering

---
+++ views/js/runner/provider/manager/userModules.js
@@ -0,0 +1,58 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA ;
+ */
+/**
+ * @author Christophe Noël <christophe@taotesting.com>
+ */
+define([
+    'jquery',
+    'lodash',
+    'module'
+], function ($, _, module) {
+    'use strict';
+
+    var userModules;
+    var config = module.config();
+
+    if (config && config.userModules && _.isArray(config.userModules)) {
+        userModules = config.userModules;
+    }
+
+    return {
+        load: function load() {
+            return new Promise(function(resolve) {
+                require(userModules, function() {
+                    _.forEach(arguments, function(dependency) {
+                        if (dependency && _.isFunction(dependency.exec)) {
+                            dependency.exec();
+                        }
+                    });
+                    resolve();
+                });
+            });
+        },
+
+        /**
+         * allow override of modules for testing purposes
+         * this shouldn't be used outside of testing
+         * @param newUserModules todo: complete
+         */
+        setConfig: function setConfig(testConfig) {
+            config = testConfig;
+        }
+    };
+});
--- views/js/runtime/qtiBootstrap.js
+++ views/js/runtime/qtiBootstrap.js
@@ -22,8 +22,9 @@ define([
     'taoQtiItem/qtiRunner/core/QtiRunner',
     'taoQtiItem/qtiCommonRenderer/renderers/Renderer',
     'iframeNotifier',
-    'core/history'
-], function($, _, module, QtiRunner, Renderer, iframeNotifier, history){
+    'core/history',
+    'taoQtiItem/runner/provider/manager/userModules'
+], function($, _, module, QtiRunner, Renderer, iframeNotifier, history, userModules){
     'use strict';
 
     //fix backspace going back into the history
@@ -60,35 +61,30 @@ define([
 
                     qtiRunner.renderItem(undefined, function() {
 
-                    	//runtime user functions
-                    	var config = module.config();
-                    	if (config && config.userModules && _.isArray(config.userModules)) {
-                    		require(config.userModules, function() {
-                                _.forEach(arguments, function(dependency) {
-                                    if (dependency && _.isFunction(dependency.exec)) {
-                                        dependency.exec();
-                                    }
+                        userModules.load()
+                        .then(function() {
+                           //exec user scripts
+                            if (_.isArray(runnerContext.userScripts)) {
+                                require(runnerContext.userScripts, function() {
+                                    _.forEach(arguments, function(dependency) {
+                                        if (_.isFunction(dependency.exec)) {
+                                            dependency.exec.call(null, runnerContext.userVars);
+                                        }
+                                    });
                                 });
+                            }
+
+                            iframeNotifier.parent('itemloaded');
+
+                            //IE9/10 loose the iframe focus, so we force getting it back.
+                            _.defer(function(){
+                                window.focus();
                             });
-                        }
-
-                       //exec user scripts
-                        if (_.isArray(runnerContext.userScripts)) {
-                            require(runnerContext.userScripts, function() {
-                                _.forEach(arguments, function(dependency) {
-                                    if (_.isFunction(dependency.exec)) {
-                                        dependency.exec.call(null, runnerContext.userVars);
-                                    }
-                                });
-                            });
-                        }
-
-                        iframeNotifier.parent('itemloaded');
-
-                        //IE9/10 loose the iframe focus, so we force getting it back.
-                        _.defer(function(){
-                            window.focus();
+                        })
+                        .catch(function(err) {
+                            throw new Error('error', 'Error in user modules : ' + err.message);
                         });
+
                     });
                 });
             });
