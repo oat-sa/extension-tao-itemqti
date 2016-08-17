e1e9d37e52c2a5054978890e313db79ac4690f00
= <jerome@taotesting.com>
2016-08-17 15:16:03 +0200

    Bump version.

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label' => 'QTI item model',
     'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '3.8.0',
+    'version' => '3.9.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=2.20.0',
