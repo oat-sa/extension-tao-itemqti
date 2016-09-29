f97e1bc1b7548434afe7f3f6eb4e206b0b857b66
christophe garcia <christophe.garcia@taotesting.com>
2016-09-29 12:40:18 +0200

    fix empty label and bump version

--- model/qti/ImportService.php
+++ model/qti/ImportService.php
@@ -122,7 +122,7 @@ class ImportService extends tao_models_classes_GenerisService
         $itemService->setItemModel($rdfItem, new core_kernel_classes_Resource(ItemModel::MODEL_URI));
 
         //set the label
-        if($qtiModel->hasAttribute('label')) {
+        if($qtiModel->hasAttribute('label') && !empty($qtiModel->getAttributeValue('label'))) {
             $label = $qtiModel->getAttributeValue('label');
         } else {
             $label = $qtiModel->getAttributeValue('title');
