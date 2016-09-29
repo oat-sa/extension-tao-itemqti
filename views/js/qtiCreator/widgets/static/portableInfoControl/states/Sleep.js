086e76a39865538a0e56d780e62dde5df776a0db
Sam Sipasseuth <sam@taotesting.com>
2016-09-29 09:19:24 +0200

    enable info control preoperties panel

--- views/js/qtiCreator/widgets/static/portableInfoControl/states/Sleep.js
+++ views/js/qtiCreator/widgets/static/portableInfoControl/states/Sleep.js
@@ -1,17 +1,24 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2016 (original work) Open Assessment Technologies SA ;
+ */
 define([
     'taoQtiItem/qtiCreator/widgets/states/factory',
-    'taoQtiItem/qtiCreator/widgets/states/Sleep',
-    'tpl!taoQtiItem/qtiCreator/tpl/notifications/widgetOverlay'
-], function(stateFactory, SleepState, overlayTpl){
-
-    return stateFactory.extend(SleepState, function(){
-
-        //add transparent protective layer
-//        this.widget.$container.append(overlayTpl());
-
-    }, function(){
-
-        //remove transparent protective layer
-//        this.widget.$container.children('.overlay').remove();
-    });
+    'taoQtiItem/qtiCreator/widgets/states/Sleep'
+], function(stateFactory, SleepState){
+    'use strict';
+    return stateFactory.extend(SleepState);
 });
