8376f910c0488a9666543444be13d876ac484cdd
antoinerobin <antoine.robin@vesperiagroup.com>
2016-10-04 14:37:39 +0200

    update version number

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -434,7 +434,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->getServiceManager()->register(ItemCategoriesService::SERVICE_ID, $categoriesService);
             $this->setVersion('5.8.0');
         }
-        $this->skip('5.8.0', '5.8.2');
+        $this->skip('5.8.0', '5.9.0');
     }
 
 }
