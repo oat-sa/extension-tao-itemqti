ee06ec219080233f66ba2fca20350a761e65d643
christophe garcia <christophe.garcia@taotesting.com>
2016-10-06 16:50:57 +0200

    use items constant

--- model/Listener/ItemUpdater.php
+++ model/Listener/ItemUpdater.php
@@ -38,11 +38,11 @@ class ItemUpdater {
      */
     public static function catchItemRdfUpdatedEvent(ItemRdfUpdatedEvent $event) {
         $rdfItem = new core_kernel_classes_Resource($event->getItemUri());
-        $type = $rdfItem->getProperty('http://www.tao.lu/Ontologies/TAOItem.rdf#ItemModel');
+        $type = $rdfItem->getProperty(TAO_ITEM_MODEL_PROPERTY);
         /*@var $directory \oat\oatbox\filesystem\Directory */
         $directory = taoItems_models_classes_ItemsService::singleton()->getItemDirectory($rdfItem);
         $itemModel = $rdfItem->getPropertyValues($type);
-        if($directory->exists() && in_array('http://www.tao.lu/Ontologies/TAOItem.rdf#QTI', $itemModel)  ) {
+        if($directory->exists() && in_array(TAO_ITEM_MODEL_QTI, $itemModel)  ) {
             /* @var $file File */
             $file = $directory->getFile(Service::QTI_ITEM_FILE);
                 
