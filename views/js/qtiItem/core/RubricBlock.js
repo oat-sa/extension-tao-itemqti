856147bc692e5d2048ec814c8f44a9b6d5ffe3bd
Sam Sipasseuth <sam@taotesting.com>
2016-10-11 14:47:22 +0200

    fixed isEmpty() method for item

--- views/js/qtiItem/core/RubricBlock.js
+++ views/js/qtiItem/core/RubricBlock.js
@@ -1,7 +1,10 @@
 define(['taoQtiItem/qtiItem/core/Element', 'taoQtiItem/qtiItem/mixin/Container'], function(Element, Container){
 
     var RubricBlock = Element.extend({
-        qtiClass : 'rubricBlock'
+        qtiClass : 'rubricBlock',
+        isEmpty : function isEmpty(){
+            return !(this.bdy && this.bdy.body());
+        }
     });
     
     Container.augment(RubricBlock);
