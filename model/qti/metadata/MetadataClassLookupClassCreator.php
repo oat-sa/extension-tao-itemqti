a92d7081c8b974f06619ba62737c9c72a0ceeefd
= <jerome@taotesting.com>
2016-10-13 11:30:02 +0200

    Missing interface.

---
+++ model/qti/metadata/MetadataClassLookupClassCreator.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2016 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
+
+namespace oat\taoQtiItem\model\qti\metadata;
+
+/**
+ * MetadataClassLookup interface.
+ * 
+ * All classes claiming at being able to 
+ * 
+ * * lookup for a target import Ontology Class
+ * * AND create a target import Ontology Class if no suitable one is found
+ * 
+ * for a given item must implement this interface.
+ * 
+ * @author Jérôme Bogaerts <jerome@taotesting.com>
+ *
+ */
+interface MetadataClassLookupClassCreator extends MetadataClassLookup
+{    
+    /**
+     * Whether or not a class has been created.
+     * 
+     * Invoke this method to know whether or not the target import Ontology class
+     * identified during the last invokation of MetadataClassLookup::lookup() method
+     * had to be created on purpose.
+     * 
+     * @return false|\core_kernel_classes_Class
+     */
+    public function classCreated();
+}
