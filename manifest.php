59d53f4711628e495d330330df5b4892d5a108d8
Sam Sipasseuth <sam@taotesting.com>
2016-10-14 17:05:54 +0200

    bump version

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label' => 'QTI item model',
     'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '5.12.1',
+    'version' => '5.13.0',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=2.22.1',
