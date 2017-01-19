0f75de522d82a8dab7dd9bde41925726b6024794
Dieter Raber <dieter@taotesting.com>
2017-01-19 18:32:21 +0100

    complete crud implementation

--- model/themes/ItemThemeInstaller.php
+++ model/themes/ItemThemeInstaller.php
@@ -20,84 +20,199 @@
 
 namespace oat\taoQtiItem\model\themes;
 
-use oat\tao\model\ThemeNotFoundException;
 use oat\tao\model\ThemeRegistry;
 
-trait ItemThemeInstaller
+\console::setMode('file' , 'theme-installer.log');
+
+class ItemThemeInstaller
 {
 
-    /**
-     * @var
-     */
     private $extensionId;
 
+    private $registry;
 
     /**
-     * @param $themes
-     * @param $currentTheme
+     * ItemThemeInstaller constructor.
+     *
+     * @param $extensionId
+     */
+    public function __construct($extensionId) {
+        $this->extensionId = $extensionId;
+        $this->registry = ThemeRegistry::getRegistry();
+    }
+
+    /**
+     * Remove themes from the configuration
+     *
+     * @param array|string $themeIds
      *
      * @return \common_report_Report
      */
-    public function install($themes, $currentTheme) {
-
-        $extensionId = $this->getExtensionId();
-
-        // avoid collision with homonymous themes from other extensions
-        $currentTheme = $extensionId . ucfirst($currentTheme);
-
-        // this helps when running the script multiple times during debugging
-        try {
-            ThemeRegistry::getRegistry()->unregisterTheme($currentTheme);
-        } catch (ThemeNotFoundException $e) {
-            \common_Logger::d('theme ' . $currentTheme . ' is not registered, cannot unregister');
+    public function remove($themeIds) {
+        $themeIds = (array)$themeIds;
+        foreach($themeIds as $themeId) {
+            $prefixedId = $this->getPrefixedThemeId($themeId);
+            if(!$this->themeExists($prefixedId)) {
+                continue;
+            }
+            $this->registry->unregisterTheme($prefixedId);
         }
-
-        $this->addThemes($themes);
-        $this->setDefault($currentTheme);
-
-        // TAO theme would usually be removed from the stack if custom themes are used
-        // Make sure another theme has been set to default in this case.
-        try {
-            ThemeRegistry::getRegistry()->unregisterTheme('tao');
-        } catch (ThemeNotFoundException $e) {
-            \common_Logger::d('tao theme is not registered, cannot unregister');
-        }
-
-        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Item themes registered');
+        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Item themes removed: ' . implode(',', $themeIds));
     }
 
-    public function addThemes($themes) {
-        $extensionId = $this->getExtensionId();
+
+    /**
+     * @param array $themes
+     *
+     * @return \common_report_Report
+     */
+    public function add(array $themes) {
 
         foreach($themes as $themeId => $label) {
-            ThemeRegistry::getRegistry()->registerTheme(
-                $extensionId . ucfirst($themeId),
-                $label,
-                implode(DIRECTORY_SEPARATOR, array($extensionId, 'views', 'css', 'themes', 'items', $themeId, 'theme.css')),
-                array('items')
-            );
+            $prefixedId =  $this->getPrefixedThemeId($themeId);
+            if($this->themeExists($prefixedId)) {
+                continue;
+            }
+            $this->register($themeId, $label);
         }
+
+        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Item themes registered: ' . implode(',', array_keys($themes)));
     }
+
+
+    /**
+     * @param array $themes
+     *
+     * @return \common_report_Report
+     */
+    public function update(array $themes) {
+        foreach($themes as $themeId => $label) {
+            $this->remove($themeId);
+            $this->register($themeId, $label);
+        }
+        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Item themes updated: ' . implode(',', array_keys($themes)));
+    }
+
 
     /**
      * Set the current theme
      *
-     * @param $currentTheme
+     * @param $themeId
+     *
+     * @return boolean|\common_report_Report
      */
-    public function setDefault($currentTheme) {
-        ThemeRegistry::getRegistry()->setDefaultTheme('items', $currentTheme);
+    public function setDefault($themeId) {
+        $prefixedId =  $this->getPrefixedThemeId($themeId);
+        if(!$this->themeExists($prefixedId)) {
+            return new \common_report_Report(\common_report_Report::TYPE_ERROR, $themeId . ' not installed, could not set to default');
+        }
+        $this->registry->setDefaultTheme('items', $prefixedId);
+        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Default item theme set to ' . $themeId);
+    }
+
+
+    /**
+     * Reset the item theme registry to its initial values
+     *
+     * @return bool|\common_report_Report
+     */
+    public function reset() {
+        $map = $this->registry->getMap();
+        if(empty($map['items']['available'])) {
+            return false;
+        }
+        foreach($map['items']['available'] as $theme) {
+            // exclude themes that don't belong to this customer
+            if($theme['id'] === 'tao' || 0 !== strpos($theme['id'], $this->extensionId)) {
+                continue;
+            }
+            $this->registry->unregisterTheme($theme['id']);
+        }
+        // get the now updated map
+        $map = $this->registry->getMap();
+
+        $taoTheme = array(
+            'id' => 'tao',
+            'name' => 'TAO',
+            'path' => $this->getStylesheetPath('tao')
+        );
+
+        if(!$this->themeExists('tao')) {
+            array_unshift($map['items']['available'], $taoTheme);
+        }
+
+        $this->registry->set('items', array(
+            'base'  => $map['items']['base'],
+            // potential other themes have not been removed
+            'available' => $map['items']['available'],
+            'default' => 'tao'
+        ));
+
+        return new \common_report_Report(\common_report_Report::TYPE_SUCCESS, 'Removed ' . $this->extensionId . ' themes, restored TAO default');
     }
 
     /**
-     * extract extension from calling class
+     * Is a theme already registered?
+     *
+     * @param $themeId
+     *
+     * @return bool
+     */
+    public function themeExists($themeId) {
+        // while this seem to be obsolete in most cases
+        // it can be useful when the function is called from the outside
+        $prefixedId = $this->getPrefixedThemeId($themeId);
+        $map = $this->registry->getMap();
+        if(empty($map['items']['available'])) {
+            return false;
+        }
+        foreach($map['items']['available'] as $theme) {
+            if($theme['id'] === $prefixedId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    /**
+     * Prefix theme id base on extension id from calling class. Pass through if already prefixed
+     *
+     * @param $themeId
      *
      * @return string
      */
-    protected function getExtensionId() {
-        if(is_null($this->extensionId)) {
-            strtok(get_class($this), '\\');
-            $this->extensionId = strtok('\\');
+    protected function getPrefixedThemeId($themeId) {
+        if($themeId === 'tao') {
+            return $themeId;
         }
-        return $this->extensionId;
+        if(preg_match('~^' . $this->extensionId . '[A-Z]~', $themeId)) {
+            return $themeId;
+        }
+        return $this->extensionId . ucfirst($themeId);
+    }
+
+    /**
+     * @param $themeId
+     *
+     * @return string
+     */
+    protected function getStylesheetPath($themeId) {
+        return $themeId === 'tao'
+            ? 'taoQtiItem/views/css/themes/default.css'
+            : sprintf('%s/views/css/themes/items/%s/theme.css', $this->extensionId, $themeId);
+    }
+
+    /**
+     * @param $themeId
+     * @param $label
+     */
+    protected function register($themeId, $label) {
+        $this->registry->registerTheme(
+            $this->getPrefixedThemeId($themeId),
+            $label,
+            $this->getStylesheetPath($themeId),
+            array('items')
+        );
     }
 }
