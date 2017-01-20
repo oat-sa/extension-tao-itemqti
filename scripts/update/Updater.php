bb3c024769fd5f24c245cb333133caede70547a5
Gyula Szucs <gyula@taotesting.com>
2017-01-20 13:26:44 +0100

    Versioning

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -446,7 +446,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('6.8.2');
         }
 
-        $this->skip('6.8.2', '6.8.6');
+        $this->skip('6.8.2', '6.8.7');
     }
 
 }
