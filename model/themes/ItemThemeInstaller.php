726f00aa0759781fec7144c04ec21b80d6fb70e0
Dieter Raber <dieter@taotesting.com>
2017-01-25 14:16:38 +0100

    Update ItemThemeInstaller.php

--- model/themes/ItemThemeInstaller.php
+++ model/themes/ItemThemeInstaller.php
@@ -94,6 +94,8 @@ use oat\tao\model\ThemeRegistry;
  * $itemThemeInstaller->reset();
  * </code>
  *
+ * During development you often have situations where you want to run the installer/updater multiple times. You can do
+ * this will all methods without risking errors.
  *
  * @package oat\taoQtiItem\model\themes
  */
