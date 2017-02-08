e2266a0107246440b59369316dd4219fb1fea4af
antoinerobin <antoine.robin@vesperiagroup.com>
2017-02-08 09:21:09 +0100

    add some translations

--- manifest.php
+++ manifest.php
@@ -29,7 +29,7 @@ return array(
     'label' => 'QTI item model',
     'description' => 'TAO QTI item model',
     'license' => 'GPL-2.0',
-    'version' => '6.13.0',
+    'version' => '6.13.1',
     'author' => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=2.26.0',
