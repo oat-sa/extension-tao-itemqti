803b145930a04778d7ff816f96f51678a34e9988
siwane <camille@taotesting.com>
2017-03-30 12:57:31 +0000

    Throw userReadableException at export in case of empty item to have proper error message

--- model/qti/Service.php
+++ model/qti/Service.php
@@ -27,6 +27,7 @@ use oat\taoQtiItem\model\portableElement\model\PortableElementModel;
 use oat\taoQtiItem\model\portableElement\model\PortableModelRegistry;
 use oat\taoQtiItem\model\portableElement\PortableElementService;
 use oat\taoQtiItem\model\portableElement\storage\PortableElementRegistry;
+use oat\taoQtiItem\model\qti\exception\ExportException;
 use oat\taoQtiItem\model\qti\exception\QtiModelException;
 use oat\taoQtiItem\model\qti\exception\XIncludeException;
 use oat\taoQtiItem\model\qti\metadata\MetadataRegistry;
@@ -93,7 +94,7 @@ class Service extends tao_models_classes_Service
         } catch (FileNotFoundException $e) {
             // fail silently, since file might not have been created yet
             // $returnValue is then NULL.
-            common_Logger::d('item('.$item->getUri().') is empty, newly created?');
+            throw new ExportException($item->getLabel(), 'Item('.$item->getUri().') is empty and cannot be exported');
         }
         
         return $returnValue;
