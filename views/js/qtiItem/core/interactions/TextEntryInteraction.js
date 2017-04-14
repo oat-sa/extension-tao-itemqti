3bb923c4752cca4f8300085e70bed96cae486870
Sam Sipasseuth <sam@taotesting.com>
2017-04-14 09:44:18 +0200

    text entry normal maximum with pattern mask

--- views/js/qtiItem/core/interactions/TextEntryInteraction.js
+++ views/js/qtiItem/core/interactions/TextEntryInteraction.js
@@ -10,19 +10,36 @@ define([
             var responseDeclaration = this.getResponseDeclaration();
             var template = responseHelper.getTemplateNameFromUri(responseDeclaration.template);
             var max, scoreMaps;
+            var self = this;
 
-            //TODO check pattern mask to see if the correct response match it
+            /**
+             * Check that a response is possible or not according to the defined patternmask
+             * @param {String} value
+             * @returns {Boolean}
+             */
+            var isPossibleResponse = function isPossibleResponse(value){
+                var patternMask = self.attr('patternMask');
+                if(patternMask){
+                    return !!value.match(new RegExp(patternMask));
+                }else{
+                    //no restriction by pattern so always possible
+                    return true;
+                }
+            };
+
             if (template === 'MATCH_CORRECT') {
                 if(!responseDeclaration.correctResponse || (_.isArray(responseDeclaration.correctResponse) && !responseDeclaration.correctResponse.length)){
                     //no correct response defined -> score always zero
                     max = 0;
                 }else{
-                    max = 1;
+                    max = isPossibleResponse(responseDeclaration.correctResponse[0]) ? 1 : 0;
                 }
             }else if(template === 'MAP_RESPONSE') {
 
                 //calculate the maximum reachable score by choice map
-                scoreMaps = _.values(responseDeclaration.mapEntries);
+                scoreMaps = _.values(_.filter(responseDeclaration.mapEntries, function(score, key){
+                    return isPossibleResponse(key);
+                }));
                 max = _(scoreMaps).map(function (v) {
                     return parseFloat(v);
                 }).max();
