b3f9da3b244b6aca2726a8308831a935f44917a1
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2017-05-18 17:48:02 +0200

    enable to set the response of a selectpoint interaction

--- views/js/qtiCommonRenderer/renderers/interactions/SelectPointInteraction.js
+++ views/js/qtiCommonRenderer/renderers/interactions/SelectPointInteraction.js
@@ -13,11 +13,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2014 (original work) Open Assessment Technlogies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2014-2017 (original work) Open Assessment Technlogies SA (under the project TAO-PRODUCT);
  *
  */
 
 /**
+ * The Common Render for the Select Point Interaction
+ *
  * @author Bertrand Chevrier <bertrand@taotesting.com>
  */
 define([
@@ -34,6 +36,69 @@ define([
     'use strict';
 
     /**
+     * Get the responses from the interaction
+     * @private
+     * @param {Object} interaction
+     * @returns {Array} of points
+     */
+    var getRawResponse = function getRawResponse(interaction){
+        var points = [];
+        interaction.paper.forEach(function(element){
+            var point = element.data('point');
+            if(typeof point === 'object' && point.x && point.y){
+                points.push([Math.round(point.x), Math.round(point.y)]);
+            }
+        });
+        return points;
+    };
+
+    var addPoint = function addPoint(interaction, point){
+        var maxChoices      = interaction.attr('maxChoices');
+        var pointChange     = function pointChange(target){
+            if(interaction.isTouch && target && target.getBBox){
+                graphic.createTouchCircle(interaction.paper, target.getBBox());
+            }
+            containerHelper.triggerResponseChangeEvent(interaction);
+            instructionMgr.validateInstructions(interaction, { target : target });
+        };
+
+        if(maxChoices > 0 && getRawResponse(interaction).length >= maxChoices){
+            instructionMgr.validateInstructions(interaction);
+        } else {
+            graphic.createTarget(interaction.paper, {
+                point : point,
+                create : pointChange,
+                remove : pointChange
+            });
+        }
+    };
+
+    /**
+     * Make the image clickable and place targets at the given position.
+     * @private
+     * @param {object} interaction
+     */
+    var enableSelection = function enableSelection(interaction){
+        var $container      = containerHelper.get(interaction);
+        var $imageBox       = $container.find('.main-image-box');
+        var isResponsive    = $container.hasClass('responsive');
+        var image           = interaction.paper.getById('bg-image-' + interaction.serial);
+
+        interaction.paper.isTouch = false;
+
+        //used to see if we are in a touch context
+        image.touchstart(function(){
+            interaction.paper.isTouch = true;
+            image.untouchstart();
+        });
+
+        //get the point on click
+        image.click(function imageClicked(event){
+            addPoint(interaction, graphic.getPoint(event, interaction.paper, $imageBox, isResponsive));
+        });
+    };
+
+    /**
      * Init rendering, called after template injected into the DOM
      * All options are listed in the QTI v2.1 information model:
      * http://www.imsglobal.org/question/qtiv2p1/imsqti_infov2p1.html#element10321
@@ -43,7 +108,7 @@ define([
     var render = function render(interaction){
         var self = this;
 
-        return new Promise(function(resolve, reject){
+        return new Promise(function(resolve){
             var $container = containerHelper.get(interaction);
             var background = interaction.object.attributes;
 
@@ -61,14 +126,14 @@ define([
             });
 
             //enable to select the paper to position a target
-            _enableSelection(interaction);
+            enableSelection(interaction);
 
             //set up the constraints instructions
             instructionMgr.minMaxChoiceInstructions(interaction, {
                 min: interaction.attr('minChoices'),
                 max: interaction.attr('maxChoices'),
                 choiceCount : false,
-                getResponse : _getRawResponse,
+                getResponse : getRawResponse,
                 onError : function(data){
                     if(data){
                         graphic.highlightError(data.target, 'success');
@@ -76,74 +141,6 @@ define([
                 }
             });
         });
-    };
-
-    /**
-     * Make the image clickable and place targets at the given position.
-     * @private
-     * @param {object} interaction
-     */
-    var _enableSelection = function _enableSelection(interaction){
-        var maxChoices      = interaction.attr('maxChoices');
-        var $container      = containerHelper.get(interaction);
-        var $imageBox       = $container.find('.main-image-box');
-        var isResponsive    = $container.hasClass('responsive');
-        var image           = interaction.paper.getById('bg-image-' + interaction.serial);
-        var isTouch         = false;
-
-        //used to see if we are in a touch context
-        image.touchstart(function(){
-            isTouch = true;
-            image.untouchstart();
-        });
-
-        //get the point on click
-        image.click(function imageClicked(event){
-
-            if(maxChoices > 0 && _getRawResponse(interaction).length >= maxChoices){
-                instructionMgr.validateInstructions(interaction);
-                return;
-            }
-
-            //get the current mouse point, even on a responsive paper
-            var point = graphic.getPoint(event, interaction.paper, $imageBox, isResponsive);
-
-            //add the point to the paper
-            graphic.createTarget(interaction.paper, {
-                point : point,
-                create : changePoint,
-                remove : function pointRemoved (){
-                    changePoint();
-                }
-            });
-        });
-
-        /**
-         * When there is point added or reomved
-         */
-        function changePoint(target){
-            if(isTouch && target){
-                graphic.createTouchCircle(interaction.paper, target.getBBox());
-            }
-            containerHelper.triggerResponseChangeEvent(interaction);
-            instructionMgr.validateInstructions(interaction, {target : target});
-        }
-    };
-    /**
-     * Get the responses from the interaction
-     * @private
-     * @param {Object} interaction
-     * @returns {Array} of points
-     */
-    var _getRawResponse = function _getRawResponse(interaction){
-        var points = [];
-        interaction.paper.forEach(function(element){
-            var point = element.data('point');
-            if(typeof point === 'object' && point.x && point.y){
-                points.push([Math.round(point.x), Math.round(point.y)]);
-            }
-        });
-        return points;
     };
 
     /**
@@ -165,24 +162,17 @@ define([
 
             try{
                 responseValues = pciResponse.unserialize(response, interaction);
-            }catch(e){
+            } catch(e) {
+                return;
             }
 
-            if(_.isArray(responseValues)){
-                _(responseValues)
-                    .flatten()
-                    .map(function(value, index){
-                    if(index % 2 === 0){
-                        return {x : value, y : responseValues[index + 1]};
-                    }
+            _(responseValues)
+                .filter(function(point){
+                    return _.isArray(point) && point.length === 2;
                 })
-                .filter(_.isObject)
                 .forEach(function(point){
-                   graphic.createTarget(interaction.paper, {
-                        point : point
-                   });
+                    addPoint(interaction, { x : point[0], y : point[1] });
                 });
-            }
         }
     };
 
@@ -223,9 +213,7 @@ define([
      * @returns {object}
      */
     var getResponse = function(interaction){
-        var raw = _getRawResponse(interaction);
-        var response = pciResponse.serialize(_getRawResponse(interaction), interaction);
-        return response;
+        return pciResponse.serialize(getRawResponse(interaction), interaction);
     };
 
     /**
@@ -273,7 +261,6 @@ define([
      * @returns {Object} the interaction current state
      */
     var getState = function getState(interaction){
-        var $container;
         var state =  {};
         var response =  interaction.getResponse();
 
@@ -289,16 +276,16 @@ define([
      * @exports qtiCommonRenderer/renderers/interactions/SelectPointInteraction
      */
     return {
-        qtiClass : 'selectPointInteraction',
-        template : tpl,
-        render : render,
-        getContainer : containerHelper.get,
-        setResponse : setResponse,
-        getResponse : getResponse,
-        resetResponse : resetResponse,
-        destroy : destroy,
-        setState : setState,
-        getState : getState
+        qtiClass:      'selectPointInteraction',
+        template:      tpl,
+        render:        render,
+        getContainer:  containerHelper.get,
+        setResponse:   setResponse,
+        getResponse:   getResponse,
+        resetResponse: resetResponse,
+        destroy:       destroy,
+        setState:      setState,
+        getState:      getState
     };
 });
 
