409ad1e894fd88fc2cb069b7871c7dcd1b30b9c3
Sam Sipasseuth <sam@taotesting.com>
2017-05-24 09:16:38 +0200

    added equivalent mimetypes

--- views/js/qtiCommonRenderer/helpers/uploadMime.js
+++ views/js/qtiCommonRenderer/helpers/uploadMime.js
@@ -21,7 +21,7 @@ define([
 ], function (_, __) {
     "use strict";
 
-    return {
+    var uploadMime = {
         /**
          * @TODO these mime types are not up-to-date, in particular the MS ones
          * refer to http://filext.com/faq/office_mime_types.php
@@ -29,25 +29,25 @@ define([
          */
         getMimeTypes : function getMimeTypes() {
             return [
-                {"mime": "application/zip", "label": __("ZIP archive")},
+                {"mime": "application/zip", "label": __("ZIP archive"), equivalent:["application/x-zip-compressed"]},
                 {"mime": "text/plain", "label": __("Plain text")},
                 {"mime": "application/pdf", "label": __("PDF file")},
                 {"mime": "image/jpeg", "label": __("JPEG image")},
                 {"mime": "image/png", "label": __("PNG image")},
                 {"mime": "image/gif", "label": __("GIF image")},
                 {"mime": "image/svg+xml", "label": __("SVG image")},
-                {"mime": "audio/mpeg", "label": __("MPEG audio")},
+                {"mime": "audio/mpeg", "label": __("MPEG audio"), equivalent:["audio/mp3"]},
                 {"mime": "audio/x-ms-wma", "label": __("Windows Media audio")},
-                {"mime": "audio/x-wav", "label": __("WAV audio")},
+                {"mime": "audio/x-wav", "label": __("WAV audio"), equivalent:["audio/wav"]},
                 {"mime": "video/mpeg", "label": __("MPEG video")},
                 {"mime": "video/mp4", "label": __("MP4 video")},
                 {"mime": "video/quicktime", "label": __("Quicktime video")},
                 {"mime": "video/x-ms-wmv", "label": __("Windows Media video")},
                 {"mime": "video/x-flv", "label": __("Flash video")},
-                {"mime": "text/csv", "label": __("CSV file")},
-                {"mime": "application/msword", "label": __("Microsoft Word - doc")},
-                {"mime": "application/vnd.ms-excel", "label": __("Microsoft Excel - xls")},
-                {"mime": "application/vnd.ms-powerpoint", "label": __("Microsoft Powerpoint - ppt")},
+                {"mime": "text/csv", "label": __("CSV file"), equivalent:["application/vnd.ms-excel"]},
+                {"mime": "application/msword", "label": __("Microsoft Word")},
+                {"mime": "application/vnd.ms-excel", "label": __("Microsoft Excel")},
+                {"mime": "application/vnd.ms-powerpoint", "label": __("Microsoft Powerpoint")},
                 {"mime": "application/vnd.openxmlformats-officedocument.wordprocessingml.document", "label": __("Microsoft Word - docx")},
                 {"mime": "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", "label": __("Microsoft Excel - xlsx")},
                 {"mime": "application/vnd.openxmlformats-officedocument.presentationml.presentation", "label": __("Microsoft Powerpoint - pptx")},
@@ -95,6 +95,8 @@ define([
         getExpectedTypes : function getExpectedTypes(interaction) {
             var classes = interaction.attr('class') || '';
             var types = [];
+            var mimes = uploadMime.getMimeTypes();
+            var equivalents = [];
             if (interaction.attr('type')) {
                 types.push(interaction.attr('type'));
             } else {
@@ -102,8 +104,18 @@ define([
                     types.push(type.replace('_', '/').trim());
                 });
             }
-            return types;
+
+            // add in equivalent mimetypes to the list of expected types
+            _.forEach(types, function(mime){
+                var mimeData = _.find(mimes, {mime:mime});
+                if(mimeData && _.isArray(mimeData.equivalent)){
+                    equivalents = _.union(equivalents, mimeData.equivalent);
+                }
+            });
+
+            return _.union(types, equivalents);
         }
-    }
-        ;
+    };
+
+    return uploadMime;
 });
