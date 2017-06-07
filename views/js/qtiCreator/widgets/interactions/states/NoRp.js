f07f0959208c1198d29049e8f13d35a52417cf2a
Sam Sipasseuth <sam@taotesting.com>
2017-06-07 09:30:34 +0200

    add/remove outcome SCORE when toggling no rp mode

--- views/js/qtiCreator/widgets/interactions/states/NoRp.js
+++ views/js/qtiCreator/widgets/interactions/states/NoRp.js
@@ -1,8 +1,35 @@
 define([
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/states/NoRp',
-    'lodash'
-], function(stateFactory, NoRp, _){
+    'taoQtiItem/qtiCreator/helper/xmlRenderer'
+], function(stateFactory, NoRp, xmlRenderer){
     'use strict';
-    return stateFactory.create(NoRp, _.noop, _.noop);
+
+    return stateFactory.create(NoRp, function initStateNoRp(){
+        var interaction = this.widget.element;
+        var item = interaction.getRelatedItem();
+        var rp = item.responseProcessing;
+        var rpXml = xmlRenderer.render(rp);
+
+        //remove outcome SCORE if there is no response processing
+        if(!rpXml.trim()){
+            item.removeOutcome('SCORE');
+        }
+
+    }, function exitStateNoRp(){
+        var interaction = this.widget.element;
+        var item = interaction.getRelatedItem();
+        var outcomeScore = item.getOutcomeDeclaration('SCORE');
+        var rp = item.responseProcessing;
+        var rpXml = xmlRenderer.render(rp);
+
+        //create the outcome score if rp required
+        if(rpXml && !outcomeScore){
+            outcomeScore = item.createOutcomeDeclaration({
+                cardinality : 'single',
+                baseType : 'float'
+            });
+            outcomeScore.buildIdentifier('SCORE', false);
+        }
+    });
 });
