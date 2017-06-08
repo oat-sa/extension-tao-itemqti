d081863a2f1faba4d7bd1399f69bf3776b9b96ec
Sam Sipasseuth <sam@taotesting.com>
2017-06-08 14:31:33 +0200

    fixed the no rp state layout for text entry and inline choice interactions

--- views/js/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/states.js
+++ views/js/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/states.js
@@ -3,7 +3,8 @@ define([
     'taoQtiItem/qtiCreator/widgets/interactions/inlineInteraction/states/states',
     'taoQtiItem/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/Question',
     'taoQtiItem/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/Correct',
-    'taoQtiItem/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/Map'
+    'taoQtiItem/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/Map',
+    'taoQtiItem/qtiCreator/widgets/interactions/inlineChoiceInteraction/states/NoRp'
 ], function(factory, states){
     'use strict';
     return factory.createBundle(states, arguments);
