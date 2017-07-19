4bb69718edc007c0bdc6bd723781ca1fe33325c6
Sam Sipasseuth <sam@taotesting.com>
2017-07-19 14:55:55 +0200

    check if mathjax is present

--- views/js/portableElementLib/OAT/util/math.js
+++ views/js/portableElementLib/OAT/util/math.js
@@ -1,15 +1,16 @@
 define(['taoQtiItem/portableElementLib/jquery_2_1_1', 'mathJax'], function($, MathJax){
-
+    'use strict';
     return {
         render : function render($container){
-
-            $container.find('math').each(function(){
-
-                var $math = $(this);
-                $math.wrap($('<span>', {'class' : 'math-renderer'}));
-                var $wrap = $math.parent('.math-renderer');
-                MathJax.Hub.Queue(["Typeset", MathJax.Hub, $wrap[0]]);
-            });
+            if(MathJax){
+                $container.find('math').each(function(){
+                    var $wrap;
+                    var $math = $(this);
+                    $math.wrap($('<span>', {'class' : 'math-renderer'}));
+                    $wrap = $math.parent('.math-renderer');
+                    MathJax.Hub.Queue(["Typeset", MathJax.Hub, $wrap[0]]);
+                });
+            }
         }
     };
 });
