92b91e4725fddb1de744baf42e8d64a502685985
Christophe Noël <christophe@taotesting.com>
2017-07-28 13:48:33 +0200

    removed comment

--- views/js/qtiItem/core/Table.js
+++ views/js/qtiItem/core/Table.js
@@ -21,32 +21,11 @@
 define([
     'taoQtiItem/qtiItem/core/Element',
     'taoQtiItem/qtiItem/mixin/ContainerTable'
-    /*,
-    'lodash',
-    'taoQtiItem/qtiItem/helper/rendererConfig'
-    */
-], function(Element, Container /*, _, rendererConfig*/){
+], function(Element, Container){
     'use strict';
 
     var Table = Element.extend({
         qtiClass : 'table'
-        /*,
-        render : function(){
-
-            var args = rendererConfig.getOptionsFromArguments(arguments),
-                renderer = args.renderer || this.getRenderer(),
-                defaultData = {};
-
-            defaultData.attributes = {
-                src : renderer.resolveUrl(this.attr('src'))
-            };
-
-            return this._super(_.merge(defaultData, args.data), args.placeholder, args.subclass, renderer);
-        },
-        isEmpty : function(){
-            return (!this.attr('src'));
-        }
-        */
     });
 
     Container.augment(Table);
