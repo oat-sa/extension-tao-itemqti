96177612b2218b90f8e80cea77e10e5a18c9023a
Sam Sipasseuth <sam@taotesting.com>
2017-09-19 16:08:49 +0200

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -439,6 +439,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('10.0.0');
         }
 
-        $this->skip('10.0.0', '10.0.1');
+        $this->skip('10.0.0', '10.0.2');
     }
 }
