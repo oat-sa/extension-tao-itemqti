95676f97bb7c577328f88d323a0ee0f0119adcdd
bugalot <jerome@taotesting.com>
2017-09-25 00:15:10 +0200

    Bump to version 10.0.5

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '10.0.4',
+    'version'     => '10.0.5',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=4.2.4',
