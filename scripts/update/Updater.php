74667863620967ead048e673e463da2c466c561c
Aleksej Tikhanovich <aleksej@taotesting.com>
2017-09-26 10:01:51 +0300

    Bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -439,6 +439,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('10.0.0');
         }
 
-        $this->skip('10.0.0', '10.1.2');
+        $this->skip('10.0.0', '10.1.3');
     }
 }
