c5ce6e64aa4ef0974dd75db5d904f4c32fe49fdd
Christophe Noël <christophe@taotesting.com>
2017-10-09 13:51:11 +0200

    updated tao dependency

--- manifest.php
+++ manifest.php
@@ -32,7 +32,7 @@ return array(
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=4.2.4',
-        'tao'      => '>=12.17.0'
+        'tao'      => '>=13.2.0'
     ),
     'models' => array(
         'http://www.tao.lu/Ontologies/TAOItem.rdf'
