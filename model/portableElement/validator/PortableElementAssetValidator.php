a36537ab89001ae1539f6608bc6670d9849c873b
Sam Sipasseuth <sam@taotesting.com>
2017-10-13 00:15:44 +0200

    remove logger

--- model/portableElement/validator/PortableElementAssetValidator.php
+++ model/portableElement/validator/PortableElementAssetValidator.php
@@ -100,8 +100,6 @@ abstract class PortableElementAssetValidator implements Validatable
         $files = [];
         foreach ($assets as $key => $asset) {
             $constraints = $this->getAssetConstraints($key);
-//            \common_Logger::w(print_r($object, true));
-//            \common_Logger::w(print_r($constraints, true));
             foreach ($constraints as $constraint) {
                 if (! isset($asset[$constraint])) {
                     if ($this->isOptionalConstraint($key, $constraint)) {
