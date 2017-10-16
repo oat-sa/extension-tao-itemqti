c97bbcdf9589d1583baf829abf4bfd7a5bff0380
Ionut Padurariu <ionut@taotesting.com>
2017-10-16 11:19:13 +0200

    return empty array in case no values are available

--- model/Export/Extractor/Strategy/ColumnStrategy.php
+++ model/Export/Extractor/Strategy/ColumnStrategy.php
@@ -57,6 +57,10 @@ class ColumnStrategy implements Strategy
      */
     public function toArray()
     {
+        if (empty($this->dataArray)) {
+            return [];
+        }
+
         if ($this->hasOnlyOneProperty) {
             return $this->dataArray;
         }
