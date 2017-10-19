01de05b9c929f18b6071520fb714de2c3a25c04b
Sam Sipasseuth <sam@taotesting.com>
2017-10-19 09:10:12 +0200

    code formatting

--- views/js/qtiXmlRenderer/renderers/interactions/PortableCustomInteraction.js
+++ views/js/qtiXmlRenderer/renderers/interactions/PortableCustomInteraction.js
@@ -1,3 +1,20 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2017 Open Assessment Technologies SA;
+ */
 define([
     'lodash',
     'tpl!taoQtiItem/qtiXmlRenderer/tpl/interactions/portableCustomInteraction/main',
@@ -15,9 +32,7 @@ define([
     return {
         qtiClass : 'customInteraction',
         template : tpl,
-        getData : function(interaction, data){
-            console.log(data);
-
+        getData : function getData(interaction, data){
             var markupNs = interaction.getMarkupNamespace();
             data.markup = util.addMarkupNamespace(interaction.markup, markupNs ? markupNs.name : '');
 
