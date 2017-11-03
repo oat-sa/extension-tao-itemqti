6f9cfae62c4e801406455ddaf522ac55102b5970
Ilya Yarkavets <ilya.yarkavets@1pt.com>
2017-11-03 16:08:37 +0300

    Version bump

--- manifest.php
+++ manifest.php
@@ -28,7 +28,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '10.9.5',
+    'version'     => '10.9.6',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=4.2.4',
