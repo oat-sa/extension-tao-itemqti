f04e30765ad06dca5963f9c3209ab9497bc8392c
Joel Bout <joel@taotesting.com>
2017-12-18 19:10:47 +0100

    lib QTI update

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -486,7 +486,7 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('11.4.0');
         }
 
-        $this->skip('11.4.0', '11.5.0');
+        $this->skip('11.4.0', '12.0.0');
 
     }
 }
