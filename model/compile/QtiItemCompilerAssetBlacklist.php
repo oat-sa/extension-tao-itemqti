9ff1798ec7c1ab542f0b3fbc8b01b76fdff410e7
antoine <antoine@taotesting.com>
2018-01-30 09:24:48 +0100

    add a way to retrieve correct url

--- model/compile/QtiItemCompilerAssetBlacklist.php
+++ model/compile/QtiItemCompilerAssetBlacklist.php
@@ -21,6 +21,7 @@
 
 namespace oat\taoQtiItem\model\compile;
 
+use oat\awsTools\AwsClient;
 use oat\oatbox\service\ConfigurableService;
 
 /**
@@ -36,6 +37,11 @@ class QtiItemCompilerAssetBlacklist extends ConfigurableService
     const SERVICE_ID = 'taoQtiItem/compileBlacklist';
 
     const BLACKLIST = 'blacklist';
+    const CLOUDFRONT_PATTERN = 'cloudFrontPattern';
+    const CLOUDFRONT_EXPIRATION = 'cloudFrontExpiration';
+    const CLOUDFRONT_KEYPAIR = 'cloudFrontKeyPair';
+    const CLOUDFRONT_KEYFILE = 'cloudFrontKeyFile';
+    const CLOUDFRONT_KEYTMPFILE = '/tmp/cloudfront.pem';
 
     private $blacklist = [];
 
@@ -81,5 +87,49 @@ class QtiItemCompilerAssetBlacklist extends ConfigurableService
     public function setBlacklist($blacklist)
     {
         $this->blacklist = $blacklist;
+    }
+
+    public function hasCloudFrontAssets($content)
+    {
+        if($this->hasOption(self::CLOUDFRONT_PATTERN) && preg_match('#https?:\\\/\\\/[\w]*\.cloudfront\.net\\\/[^"]+#',$content) === 1){
+            return true;
+        }
+        return false;
+    }
+
+    public function replaceCloudFrontAssets($content)
+    {
+        /** @var AwsClient $awsClient */
+        $awsClient = $this->getServiceLocator()->get('generis/awsClient');
+        $matches = [];
+        preg_match_all('#https?:\\\/\\\/[\w]*\.cloudfront\.net\\\/[^"]+#',$content, $matches);
+        if(!file_exists(self::CLOUDFRONT_KEYTMPFILE) && $this->hasOption(self::CLOUDFRONT_KEYFILE)){
+            $s3Client = $awsClient->getS3Client();
+//            file_put_contents(self::CLOUDFRONT_KEYTMPFILE, );
+            chmod(self::CLOUDFRONT_KEYTMPFILE, 700);
+        }
+
+        if(!empty($matches[0])){
+            $changed = preg_replace('#\\\/#','/', $matches[0]);
+            foreach ($changed as $key => $match){
+                $cloudFront = $awsClient->getCloudFrontClient();
+
+                $resourceUrl = $match.'?user='.\common_session_SessionManager::getSession()->getUserLabel();
+                $expires = time() + ($this->hasOption(self::CLOUDFRONT_EXPIRATION)) ? $this->getOption(self::CLOUDFRONT_EXPIRATION) : 3600;
+
+                $signedUrlCannedPolicy = $cloudFront->getSignedUrl(array(
+                    'private_key' => self::CLOUDFRONT_KEYTMPFILE,
+                    'key_pair_id' => self::CLOUDFRONT_KEYPAIR,
+                    'url'     => $resourceUrl,
+                    'expires' => $expires,
+                ));
+
+                $url = str_replace('"', '', json_encode($signedUrlCannedPolicy));
+                $content = str_replace($matches[0][$key], $url, $content);
+
+            }
+        }
+
+        return $content;
     }
 }
