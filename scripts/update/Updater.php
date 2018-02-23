06316fbcf967600e2afa470cbf4afc606de7d267
bugalot <jerome@taotesting.com>
2018-02-23 14:14:42 +0100

    Bump version 13.3.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -535,6 +535,6 @@ class Updater extends \common_ext_ExtensionUpdater
 
             $this->setVersion('13.1.0');
         }
-        $this->skip('13.1.0', '13.2.0');
+        $this->skip('13.1.0', '13.3.0');
     }
 }
