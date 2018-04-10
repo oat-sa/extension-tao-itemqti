1a6ac411fe516e7660e7085d8c0d4f7b36f72254
Dieter Raber <dieter@taotesting.com>
2018-04-10 16:14:05 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '14.0.0',
+    'version'     => '14.0.1',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=4.2.4',
