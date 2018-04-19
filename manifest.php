eddbb1bf354291640ae1a9d3adcec5d1bfd36742
bugalot <jerome@taotesting.com>
2018-04-20 00:33:55 +0200

    Bump version 14.2.0

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '14.1.0',
+    'version'     => '14.2.0',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=4.2.4',
