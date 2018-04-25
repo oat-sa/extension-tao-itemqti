eb3cf75946bdcebb8ea231171cecbfe427376517
Alexander Zagovorychev <zagovorichev@gmail.com>
2018-04-25 13:25:20 +0300

    fixed updater

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -467,7 +467,8 @@ class Updater extends \common_ext_ExtensionUpdater
                     'excludedProperties' => array(
                         taoItems_models_classes_ItemsService::PROPERTY_ITEM_CONTENT,
                         taoItems_models_classes_ItemsService::PROPERTY_ITEM_MODEL,
-                        taoItems_actions_form_RestItemForm::PROPERTY_ITEM_CONTENT_SRC,
+                        // constant was moved, and to not broke updater we placed value here
+                        'http://www.tao.lu/Ontologies/TAOItem.rdf#ItemContentSourceName',
                         TaoOntology::PROPERTY_LOCK,
                     ),
                 )
