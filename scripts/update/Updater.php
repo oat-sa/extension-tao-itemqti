5ef0134a5f8719249fd61edc0724f63c20f03da8
Aleksej Tikhanovich <aleksej@taotesting.com>
2018-05-14 10:07:42 +0300

    Bump version to v15.1.0

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -545,6 +545,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('13.4.0');
         }
         
-        $this->skip('13.4.0', '15.0.0');
+        $this->skip('13.4.0', '15.1.0');
     }
 }
