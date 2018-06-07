5ce0ce6f8eaab6153a8d774971e0dacfa9271ab7
Sam Sipasseuth <sam@taotesting.com>
2018-06-07 18:12:33 +0200

    removes empty string as correct response for text entry

--- views/js/qtiCreator/widgets/interactions/textEntryInteraction/states/Correct.js
+++ views/js/qtiCreator/widgets/interactions/textEntryInteraction/states/Correct.js
@@ -1,3 +1,21 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2018 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
+ */
 define([
     'jquery',
     'taoQtiItem/qtiCreator/widgets/states/factory',
@@ -6,8 +24,8 @@ define([
     'lodash',
     'i18n'
 ], function($, stateFactory, Correct, instructionMgr, _, __){
-
-    var TextEntryInteractionStateCorrect = stateFactory.create(Correct, function(){
+    'use strict';
+    var TextEntryInteractionStateCorrect = stateFactory.create(Correct, function start(){
 
         var $container = this.widget.$container,
             response = this.widget.element.getResponseDeclaration(),
@@ -16,11 +34,15 @@ define([
         $container.find('tr[data-edit=correct] input[name=correct]').focus().val(correct);
         $container.on('keyup.correct', 'tr[data-edit=correct] input[name=correct]', function(){
             var value = $(this).val();
-            response.setCorrect(value);
+            if(value){
+                response.setCorrect(value);
+            }else{
+                response.resetCorrect();
+            }
         });
         instructionMgr.appendInstruction(this.widget.element, __('Please type the correct response in the box below.'));
 
-    }, function(){
+    }, function exit(){
 
         this.widget.$container.off('.correct');
         instructionMgr.removeInstructions(this.widget.element);
