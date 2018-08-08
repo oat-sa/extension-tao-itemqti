e5b2f7b7ced34f10ab9725b239c0faa6613c9792
alexey <ale441v@gmail.com>
2018-08-08 17:38:46 +0300

    Fixed tests

---
+++ test/unit/model/Export/Extractor/Strategy/StrategyFactoryTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace oat\taoQtiItem\test\unit\model\Export\Extractor\Strategy;
+
+use oat\taoQtiItem\model\Export\Extractor\Strategy\DefaultStrategy;
+use oat\taoQtiItem\model\Export\Extractor\Strategy\ColumnStrategy;
+use oat\taoQtiItem\model\Export\Extractor\Strategy\Strategy;
+use oat\taoQtiItem\model\Export\Extractor\Strategy\StrategyFactory;
+use PHPUnit\Framework\TestCase;
+
+class StrategyFactoryTest extends TestCase
+{
+    public function testFactoryBuildsStrategy()
+    {
+        $this->assertInstanceOf(Strategy::class, StrategyFactory::create([], 'column', []));
+    }
+
+    public function testFactoryBuildsDefaultStrategy()
+    {
+        $this->assertInstanceOf(DefaultStrategy::class, StrategyFactory::create(['valuesAsColumns' => false], 'column', []));
+        $this->assertInstanceOf(DefaultStrategy::class, StrategyFactory::create([], 'column', []));
+    }
+
+    public function testFactoryBuildsColumnStrategy()
+    {
+        $this->assertInstanceOf(ColumnStrategy::class, StrategyFactory::create(['valuesAsColumns' => true], 'column', []));
+    }
+}
