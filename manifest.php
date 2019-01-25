4f5c3d69aa0fde46b52e9e457565cf4ecf833dcf
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-01-25 11:20:05 +0100

    update tao-core dependency

--- manifest.php
+++ manifest.php
@@ -37,7 +37,7 @@ return array(
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=6.3.0',
-        'tao'      => '>=23.0.0',
+        'tao'      => '>=24.0.0',
         'generis'  => '>=8.1.3',
     ),
     'models' => array(
