d5ebf192e959032c99d7a6c043154770691a1cc6
Martin Nicholson <martin@taotesting.com>
2019-03-13 22:06:25 +0100

    Version bump

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '19.0.0',
+    'version'     => '19.1.0',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=6.6.0',
