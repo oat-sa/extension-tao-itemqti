68eec08b562a9b784bf3f2488a3d670c376223dc
Bertrand Chevrier <chevrier.bertrand@gmail.com>
2019-04-12 18:39:10 +0200

    add missing headers and cleanup

--- views/js/qtiCreator/editor/gridEditor/draggable.js
+++ views/js/qtiCreator/editor/gridEditor/draggable.js
@@ -1,10 +1,27 @@
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2014-2019 (original work) Open Assessment Technologies SA ;
+ *
+ */
 define([
     'jquery',
     'lodash',
     'taoQtiItem/qtiCreator/helper/qtiElements',
     'taoQtiItem/qtiCreator/editor/gridEditor/droppable',
     'taoQtiItem/lib/jqueryui_dragdrop'
-    //'taoQtiItem/lib/jquery.ui.draggable.min'
 ], function($, _, QtiElements, droppable){
     "use strict";
     var _insertableDefaultsOptions = {
