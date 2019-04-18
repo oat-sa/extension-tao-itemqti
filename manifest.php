81c9c78198b06a2c4a2edf1a1f92398c09d1dc1a
Hanna Dzmitryieva <bziondik@gmail.com>
2019-04-18 17:04:20 +0300

    bump version

--- manifest.php
+++ manifest.php
@@ -33,7 +33,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '19.8.1',
+    'version'     => '19.9.0',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=6.6.0',
