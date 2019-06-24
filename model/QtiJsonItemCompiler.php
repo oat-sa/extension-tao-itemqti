724833b1b0e40639141555f362caed2ba3bb4ff4
bugalot <jerome@taotesting.com>
2019-06-24 10:45:47 +0200

    Removing empty item compilation failure as it is too much greedy.

--- model/QtiJsonItemCompiler.php
+++ model/QtiJsonItemCompiler.php
@@ -86,7 +86,7 @@ class QtiJsonItemCompiler extends QtiItemCompiler
         // retrieve the media assets
         try {
             $qtiItem = $this->retrieveAssets($item, $language, $publicDirectory);
-
+            
             // @todo find a better solution to eliminate empty items.
             /*if (count($qtiItem->getBody()->getElements()) === 0) {
                 return new common_report_Report(common_report_Report::TYPE_ERROR, 'The item has an empty body.');
