2fce775630b9d59bce005e83dfd530e250c2c73d
jsconan <jsconan@gmail.com>
2019-07-17 11:21:56 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '21.1.1',
+    'version'     => '21.2.0',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=9.0.0',
