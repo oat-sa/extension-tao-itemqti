e9b48c67b154c8b4722b64fa87c4c54c6522bcad
Ferenc Kiss <ferenc@taotesting.com>
2019-08-09 11:12:30 +0200

    version bump

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -438,6 +438,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('21.0.0');
         }
 
-        $this->skip('21.0.0', '21.3.1');
+        $this->skip('21.0.0', '21.3.2');
     }
 }
