305dc15b36d6c2eed5b002999e9e4778ad2ff391
zagovorichev <zagovorichev@gmail.com>
2019-12-17 15:49:48 +0300

    bumped major version according to the required dependency

--- manifest.php
+++ manifest.php
@@ -34,7 +34,7 @@ return array(
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '22.1.0',
+    'version'     => '23.0.0',
     'author'      => 'Open Assessment Technologies',
     'requires' => array(
         'taoItems' => '>=10.1.0',
