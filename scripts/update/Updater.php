5868e512ca03c7ec42e235fc43c531b8a32386a7
Hanna Dzmitryieva <bziondik@gmail.com>
2020-02-18 15:23:56 +0300

    bump version

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -434,6 +434,6 @@ class Updater extends \common_ext_ExtensionUpdater
             $this->setVersion('21.0.0');
         }
 
-        $this->skip('21.0.0', '23.3.2');
+        $this->skip('21.0.0', '23.3.3');
     }
 }
