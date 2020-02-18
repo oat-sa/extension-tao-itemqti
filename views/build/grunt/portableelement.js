d18fc021978036422dd98521b5117cb2afb04ebc
Ihar Suleimanau <igor.salevan@gmail.com>
2020-02-18 11:41:53 +0300

    Added the license agreement.

--- views/build/grunt/portableelement.js
+++ views/build/grunt/portableelement.js
@@ -1,4 +1,22 @@
 /**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017-2020 (original work) Open Assessment Technologies SA;
+ */
+
+/**
  * Register the portable element compilation task for a given TAO extension
  *
  * @example compile all portable element in the extension qtiItemPci
@@ -9,6 +27,8 @@
  *
  * @example compile only the likertScaleInteraction in the extension qtiItemPci using short param
  * grunt portableelement -e=qtiItemPci -i=likertScaleInteraction
+ *
+ * @param {Object} grunt - the grunt object (by convention)
  */
 module.exports = function (grunt) {
     'use strict';
