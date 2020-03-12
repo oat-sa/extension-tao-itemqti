86b905f76d723ef970d01bb87b1eddb36aafb38c
Julien Sébire <julien@taotesting.com>
2020-03-12 17:54:59 +0100

    Removed useless use statement.

--- manifest.php
+++ manifest.php
@@ -28,7 +28,6 @@ use oat\taoQtiItem\scripts\install\SetItemModel;
 use oat\taoQtiItem\scripts\install\SetUpQueueTasks;
 use oat\taoQtiItem\scripts\install\RegisterItemCompilerBlacklist;
 use oat\taoQtiItem\scripts\install\RegisterNpmPaths;
-use oat\taoQtiItem\scripts\install\SetMetadataServiceConfig;
 
 $extpath = dirname(__FILE__) . DIRECTORY_SEPARATOR;
 $taopath = dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'tao' . DIRECTORY_SEPARATOR;
