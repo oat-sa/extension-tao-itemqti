91c53bdf13990483f94628a9a68fd471d3ae9d6e
Juan Luis Gutierrez Dos Santos <juanluis.gutierrezdossantos@taotesting.com>
2020-04-07 13:46:44 +0200

    Bump version

--- manifest.php
+++ manifest.php
@@ -36,7 +36,7 @@ return [
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '23.9.3',
+    'version'     => '23.9.4',
     'author'      => 'Open Assessment Technologies',
     'requires' => [
         'taoItems' => '>=10.1.0',
