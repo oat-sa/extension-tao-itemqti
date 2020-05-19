6f97b75b9c0f28c58b7561263667f663e9e6b50d
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-05-19 10:25:37 +0200

    added unit test

---
+++ test/unit/model/event/UpdatedItemEventDispatcherTest.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
+ */
+
+declare(strict_types=1);
+
+namespace oat\taoQtiItem\test\unit\model\event;
+
+use core_kernel_classes_Resource;
+use oat\oatbox\event\EventManager;
+use oat\taoItems\model\event\ItemUpdatedEvent;
+use oat\taoQtiItem\model\qti\event\UpdatedItemEventDispatcher;
+use oat\taoQtiItem\model\qti\Item;
+use oat\taoQtiItem\model\qti\parser\IncludedElementIdsExtractor;
+use PHPUnit\Framework\MockObject\MockObject;
+use oat\generis\test\TestCase;
+
+class UpdatedItemEventDispatcherTest extends TestCase
+{
+    /** @var UpdatedItemEventDispatcher */
+    private $subject;
+
+    /** @var IncludedElementIdsExtractor|MockObject */
+    private $idsExtractor;
+
+    /** @var EventManager|MockObject */
+    private $eventManager;
+
+    protected function setUp(): void
+    {
+        $this->subject = new UpdatedItemEventDispatcher();
+        $this->idsExtractor = $this->createMock(IncludedElementIdsExtractor::class);
+        $this->eventManager = $this->createMock(EventManager::class);
+        $this->subject->setServiceLocator(
+            $this->getServiceLocatorMock(
+                [
+                    IncludedElementIdsExtractor::class => $this->idsExtractor,
+                    EventManager::SERVICE_ID => $this->eventManager
+                ]
+            )
+        );
+    }
+
+    public function testDispatch(): void
+    {
+        $ids = [1];
+        $itemUri = 'uri';
+
+        $rdfItem = $this->createMock(core_kernel_classes_Resource::class);
+        $rdfItem->method('getUri')
+            ->willReturn($itemUri);
+
+        $item = $this->createMock(Item::class);
+
+        $this->eventManager
+            ->method('trigger')
+            ->with(
+                new ItemUpdatedEvent(
+                    $itemUri,
+                    [
+                        'includeElementIds' => $ids
+                    ]
+                )
+            );
+
+        $this->idsExtractor
+            ->method('extract')
+            ->with($item)
+            ->willReturn($ids);
+
+        $this->assertNull($this->subject->dispatch($item, $rdfItem));
+    }
+}
+
