19c26646603b6289eae21b971a56c5807bc668cd
Gabriel Felipe Soares <gabrielfs7@gmail.com>
2020-05-20 00:36:21 -0700

    Update model/qti/parser/IncludedElementIdsExtractor.php
    Co-authored-by: Mikhail Kamarouski <4569734+boajer@users.noreply.github.com>

--- model/qti/parser/IncludedElementIdsExtractor.php
+++ model/qti/parser/IncludedElementIdsExtractor.php
@@ -18,6 +18,8 @@
  * Copyright (c) 2020 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  */
 
+declare(strict_types=1);
+
 namespace oat\taoQtiItem\model\qti\parser;
 
 use oat\oatbox\service\ConfigurableService;
