b5e960d3a466a2232ca494a8aa1a84f3c481825b
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2020-06-03 11:54:24 +0200

    remove unnecessary bootstrap

--- test/integration/bootstrap.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once dirname(__FILE__) . '/../../../tao/includes/class.Bootstrap.php';
-require_once dirname(__FILE__) . '/../../../config/generis.conf.php';
--- test/integration/model/qti/parser/XmlToItemParserTest.php
+++ test/integration/model/qti/parser/XmlToItemParserTest.php
@@ -32,8 +32,6 @@ class XmlToItemParserTest extends TestCase
 
     protected function setUp(): void
     {
-        require_once __DIR__ . '/../../../bootstrap.php';
-
         $this->subject = new XmlToItemParser();
     }
 
