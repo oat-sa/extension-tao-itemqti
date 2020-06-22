ad7faec2c0b63ef6a1da86843ff5f6acc06030ac
Aleh Hutnikau <aleh.hutnikau@1pt.com>
2020-06-22 13:55:38 +0300

    Deprecate updater file

--- scripts/update/Updater.php
+++ scripts/update/Updater.php
@@ -68,6 +68,7 @@ use oat\oatbox\event\EventManager;
 /**
  *
  * @author Sam <sam@taotesting.com>
+ * @deprecated use migrations instead. See https://github.com/oat-sa/generis/wiki/Tao-Update-Process
  */
 class Updater extends \common_ext_ExtensionUpdater
 {
@@ -472,5 +473,10 @@ class Updater extends \common_ext_ExtensionUpdater
         }
 
         $this->skip('23.12.0', '25.1.0');
+        
+        //Updater files are deprecated. Please use migrations.
+        //See: https://github.com/oat-sa/generis/wiki/Tao-Update-Process
+
+        $this->setVersion($this->getExtension()->getManifest()->getVersion());
     }
 }
