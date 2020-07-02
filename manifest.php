64f82f353af52528729390c2e99f5c4ff7f67164
Bartlomiej Marszal <bartlomiej@taotesting.com>
2020-07-02 10:36:13 +0200

    version bump

--- manifest.php
+++ manifest.php
@@ -43,7 +43,7 @@ return [
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '25.2.1',
+    'version'     => '25.2.2',
     'author'      => 'Open Assessment Technologies',
     'requires' => [
         'taoItems' => '>=10.6.0',
