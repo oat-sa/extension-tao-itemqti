c84e743d4bf4d35db36f0dc0124b58369bc65b70
Ivan Klimchuk <ivan.klimchuk@1pt.com>
2020-07-27 10:06:02 +0300

    Fixed some issues with use statements and other code style issues

--- model/qti/feedback/FeedbackInline.php
+++ model/qti/feedback/FeedbackInline.php
@@ -1,6 +1,5 @@
 <?php
-
-/*
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -16,14 +15,9 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
  * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *
- *
  */
 
 namespace oat\taoQtiItem\model\qti\feedback;
-
-use oat\taoQtiItem\model\qti\feedback\FeedbackInline;
-use oat\taoQtiItem\model\qti\feedback\Feedback;
 
 /**
  * The QTI_FeedbackInline
@@ -36,7 +30,6 @@ use oat\taoQtiItem\model\qti\feedback\Feedback;
  */
 class FeedbackInline extends Feedback
 {
-
     /**
      * the QTI tag name as defined in QTI standard
      *
