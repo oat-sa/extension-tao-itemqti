44c962a84caff3f8c50b82379535575faf7739cc
gitromba <mihaly.hajdu@taotesting.com>
2020-09-11 15:54:24 +0200

    test(ci): detailed code coverage test

--- test/unit/model/qti/TestClassTest.php
+++ test/unit/model/qti/TestClassTest.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace oat\taoQtiItem\test\unit\model\qti;
+
+use oat\generis\test\TestCase;
+use oat\taoQtiItem\model\qti\TestClass;
+
+class TestClassTest extends TestCase
+{
+    public function testConstructor()
+    {
+        $this->assertInstanceOf(TestClass::class, new TestClass(13));
+    }
+
+    public function testAdd()
+    {
+        $testClass = new TestClass(13);
+        $testClass->add(13);
+
+        $this->assertSame('26', $testClass->toString());
+    }
+}
