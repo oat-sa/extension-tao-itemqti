5d88975e0bb045624ad6bf1c74cf0c89dafb2297
gitromba <mihaly.hajdu@taotesting.com>
2020-09-11 15:03:04 +0200

    test(ci): detailed code coverage test

---
+++ model/qti/TestClass.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace oat\taoQtiItem\model\qti;
+
+class TestClass
+{
+    private $a;
+
+    public function __construct(int $a)
+    {
+        $this->a = $a;
+    }
+
+    public function add(int $b)
+    {
+        return $this->a = $this->a + $b;
+    }
+
+    public function toString()
+    {
+        return (string)$this->a;
+    }
+}
