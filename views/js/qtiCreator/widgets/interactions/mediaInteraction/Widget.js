b695e73ca8017e83d3b18f5d8e8574e664eda733
Andrey Niahrou <Andrei.Niahrou@1pt.com>
2020-09-14 15:40:59 +0300

    disabled autoplay in authoring

--- views/js/qtiCreator/widgets/interactions/mediaInteraction/Widget.js
+++ views/js/qtiCreator/widgets/interactions/mediaInteraction/Widget.js
@@ -44,11 +44,14 @@ define([
             var $container  = this.$original; 
             var interaction = this.element;
             //disabled autoplay in authoring
+            var autostart = interaction.attributes.autostart;
             interaction.attributes.autostart = false;
             commonRenderer.render.call(interaction.getRenderer(), interaction, {
                 features : 'full',
                 controlPlaying : false
             });
+            //returns the previous autostart value
+            interaction.attributes.autostart = autostart;
         },
 
         destroyInteraction : function(){
