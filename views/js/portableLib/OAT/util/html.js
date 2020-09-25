b98ad9757cbb330f7e71b7e61a2f1be224d16b19
Andrey Niahrou <Andrei.Niahrou@1pt.com>
2020-09-25 16:00:14 +0300

    Label text added right after putting the Custom Interactions to canvas is cleared

--- views/js/portableLib/OAT/util/html.js
+++ views/js/portableLib/OAT/util/html.js
@@ -24,12 +24,6 @@ define([
 
     return {
         render : function render($container){
-            var markup = $container.html();
-
-            //remove xml ns (would break jquery selector otherwise)
-            markup = xml.removeNamespace(markup);
-            $container.html(markup);
-
             //render math
             math.render($container);
 
