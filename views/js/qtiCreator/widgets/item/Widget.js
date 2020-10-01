be26d3a7f860c52eda7c8039c9d4775da0cd579c
andrii.ponamarov <andrii.ponamarov@taotesting.com>
2020-10-01 14:50:57 +0200

    Remove logs

--- views/js/qtiCreator/widgets/item/Widget.js
+++ views/js/qtiCreator/widgets/item/Widget.js
@@ -391,7 +391,7 @@ define([
                 subContainerBody = $subContainer.html();//get serialized body
 
             $originalTextBlock.removeAttr('data-text-block-id').html('{{_container:new}}');
-            console.log('PUSH SUBCONTAINERS');
+
             subContainers.push({
                 body : subContainerBody,
                 elements : subContainerElements,
@@ -405,7 +405,6 @@ define([
         var serializedItemBody = $clonedContainer.find('.qti-itemBody').html(),
             itemBody = item.getBody();
 
-        console.log('subContainers', subContainers.length)
         if(subContainers.length){
 
             containerHelper.createElements(itemBody, serializedItemBody, function(newElts){
@@ -414,28 +413,23 @@ define([
 
                     throw 'number of sub-containers mismatch';
                 }else{
-                    console.log('Something wrong here!!!');
                     _.each(newElts, function(container){
 
                         var containerData = subContainers.shift();//get data in order
                         var containerElements = _detachElements(itemBody, containerData.elements);
 
-                        console.log('container.setElements')
                         container.setElements(containerElements, containerData.body);
-                        console.log('container.initTextWidget')
                         _this.initTextWidget(container, containerData.$original);
 
                     });
 
                     _.defer(function(){
-                        console.log('callback.call');
                         callback.call(_this);
                     });
                 }
             });
 
         }else{
-            console.log('callback.call');
             callback.call(_this);
         }
 
