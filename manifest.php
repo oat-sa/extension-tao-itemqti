664b9298e03095665957c6eeacc349b8755dce98
lecosson <lecosson@gmail.com>
2020-10-01 14:52:04 +0300

    fix/TAO-10387/mime-type-file-upload-extension

--- manifest.php
+++ manifest.php
@@ -43,7 +43,7 @@ return [
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '25.13.2',
+    'version'     => '25.13.3',
     'author'      => 'Open Assessment Technologies',
     'requires' => [
         'taoItems' => '>=10.13.0',
