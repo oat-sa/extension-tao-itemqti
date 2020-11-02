a481977345cfba32bdd62b32a893ebdc40a24219
Hanna Dzmitryieva <bziondik@gmail.com>
2020-11-02 10:45:34 +0300

    remove console.log

--- views/js/qtiCreator/itemCreator.js
+++ views/js/qtiCreator/itemCreator.js
@@ -240,13 +240,10 @@ define([
                     }
                 }).then(function(){
                     //load custom elements
-                    console.log('PCI+Pic start loading');
                     return Promise.all([
                         loadCustomInteractions(usedCustomInteractionIds),
                         loadInfoControls()
-                    ]).then(() => {
-                        console.log('PCI+Pic are loaded');
-                    })
+                    ]);
                     // return true;
                 }).then(function(){
                     //initialize all the plugins
