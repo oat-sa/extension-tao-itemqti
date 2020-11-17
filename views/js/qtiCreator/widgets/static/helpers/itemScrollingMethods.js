15e5602d34196df96053d14d6a280125d80e97ef
Hanna Dzmitryieva <bziondik@gmail.com>
2020-11-17 10:44:57 +0300

    fix eslint errors

--- views/js/qtiCreator/widgets/static/helpers/itemScrollingMethods.js
+++ views/js/qtiCreator/widgets/static/helpers/itemScrollingMethods.js
@@ -16,41 +16,45 @@
  * Copyright (c) 2020 (original work) Open Assessment Technologies SA ;
  */
 
-define([
-    'jquery',
-    'util/typeCaster',
-], function ($, typeCaster) {
+define(['jquery', 'util/typeCaster'], function ($, typeCaster) {
     'use strict';
 
     const wrapperTextCls = 'custom-text-box';
     const wrapperIncludeCls = 'custom-include-box';
     const wrapperFocusCls = 'key-navigation-focusable';
     const newUIclass = 'tao-overflow-y';
-    const options = [{
-        value: '100',
-        name: 'Full height',
-        class: 'tao-full-height'
-    }, {
-        value: '75',
-        name: '3/4 of height',
-        class: 'tao-three-quarters-height'
-    }, {
-        value: '66.6666',
-        name: '2/3 of height',
-        class: 'tao-two-thirds-height'
-    }, {
-        value: '50',
-        name: 'Half height',
-        class: 'tao-half-height'
-    }, {
-        value: '33.3333',
-        name: '1/3 of height',
-        class: 'tao-third-height'
-    }, {
-        value: '25',
-        name: '1/4 of height',
-        class: 'tao-quarter-height'
-    }];
+    const options = [
+        {
+            value: '100',
+            name: 'Full height',
+            class: 'tao-full-height'
+        },
+        {
+            value: '75',
+            name: '3/4 of height',
+            class: 'tao-three-quarters-height'
+        },
+        {
+            value: '66.6666',
+            name: '2/3 of height',
+            class: 'tao-two-thirds-height'
+        },
+        {
+            value: '50',
+            name: 'Half height',
+            class: 'tao-half-height'
+        },
+        {
+            value: '33.3333',
+            name: '1/3 of height',
+            class: 'tao-third-height'
+        },
+        {
+            value: '25',
+            name: '1/4 of height',
+            class: 'tao-quarter-height'
+        }
+    ];
 
     return {
         options: function () {
@@ -68,22 +72,24 @@ define([
         },
         wrapContent: function (widget, value, wrapType) {
             const $form = widget.$form;
-            let $wrapper = wrapType === 'inner' ? widget.$container
-                .find('[data-html-editable]')
-                .children(`.${wrapperTextCls}`) : widget.$container.parent(`.${wrapperIncludeCls}`);
+            let $wrapper =
+                wrapType === 'inner'
+                    ? widget.$container.find('[data-html-editable]').children(`.${wrapperTextCls}`)
+                    : widget.$container.parent(`.${wrapperIncludeCls}`);
 
             if (!$wrapper.length) {
-                $wrapper = wrapType === 'inner' ? widget.$container
-                    .find('[data-html-editable]')
-                    .wrapInner(`<div class="${wrapperTextCls}" />`)
-                    .children() : widget.$container
-                    .wrap(`<div class="${wrapperIncludeCls}" />`)
-                    .parent();
+                $wrapper =
+                    wrapType === 'inner'
+                        ? widget.$container
+                              .find('[data-html-editable]')
+                              .wrapInner(`<div class="${wrapperTextCls}" />`)
+                              .children()
+                        : widget.$container.wrap(`<div class="${wrapperIncludeCls}" />`).parent();
             }
 
             value ? $form.find('.scrollingSelect').show() : $form.find('.scrollingSelect').hide();
 
-            // add attr for curGen plugin itemScrolling
+            // add attr for curGen plugin itemScrolling, and class for keynavigation
             $wrapper.attr('data-scrolling', value);
             value ? $wrapper.addClass(wrapperFocusCls) : $wrapper.removeClass(wrapperFocusCls);
 
@@ -94,7 +100,7 @@ define([
                 widget.$container.parents('.grid-row').addClass(options[0].class);
             } else {
                 $wrapper.removeClass(newUIclass);
-                options.forEach((opt) => {
+                options.forEach(opt => {
                     $wrapper.removeClass(opt.class);
                 });
                 //remove tao-full-height from grid-row if no tao-full-height children
@@ -106,13 +112,13 @@ define([
         },
         setScrollingHeight: function ($wrapper, value) {
             $wrapper.attr('data-scrolling-height', value);
-            const opt = options.find((opt) => opt.value === value);
+            const opt = options.find(opt => opt.value === value);
             $wrapper.addClass(opt.class);
         },
         cutScrollClasses: function (classes) {
             let clearClasses = classes.replace(wrapperFocusCls, '');
             clearClasses = clearClasses.replace(newUIclass, '');
-            options.forEach((opt) => {
+            options.forEach(opt => {
                 clearClasses = clearClasses.replace(opt.class, '');
             });
             return clearClasses;
