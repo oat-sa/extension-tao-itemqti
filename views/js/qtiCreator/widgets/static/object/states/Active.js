df2a93d73e02ee6bd722a4d0cbe6f1c7456028cb
Hanna Dzmitryieva <bziondik@gmail.com>
2021-01-16 18:02:29 +0300

    refactor: apply codestyles rules

--- views/js/qtiCreator/widgets/static/object/states/Active.js
+++ views/js/qtiCreator/widgets/static/object/states/Active.js
@@ -29,48 +29,53 @@ define([
     'ui/previewer',
     'ui/resourcemgr',
     'ui/tooltip'
-], function(_, $, __, stateFactory, Active, formTpl, formElement, inlineHelper, mediaEditorComponent){
+], function (_, $, __, stateFactory, Active, formTpl, formElement, inlineHelper, mediaEditorComponent) {
     'use strict';
     /**
      * media Editor instance if has been initialized
      * @type {null}
      */
-    var mediaEditor = null;
+    let mediaEditor = null;
 
-    var _config = {
-        renderingThrottle : 1000,
-        fileFilters : 'image/jpeg,image/png,image/gif,image/svg+xml,video/mp4,video/avi,video/ogv,video/mpeg,video/ogg,video/quicktime,video/webm,video/x-ms-wmv,video/x-flv,audio/mp3,audio/vnd.wav,audio/ogg,audio/vorbis,audio/webm,audio/mpeg,application/ogg,audio/aac,application/pdf'
+    const _config = {
+        renderingThrottle: 1000,
+        fileFilters:
+            'image/jpeg,image/png,image/gif,image/svg+xml,video/mp4,video/avi,video/ogv,video/mpeg,video/ogg,video/quicktime,video/webm,video/x-ms-wmv,video/x-flv,audio/mp3,audio/vnd.wav,audio/ogg,audio/vorbis,audio/webm,audio/mpeg,application/ogg,audio/aac,application/pdf'
     };
 
-    var ObjectStateActive = stateFactory.extend(Active, function(){
-        this.initForm();
-    }, function(){
-        if (mediaEditor) {
-            mediaEditor.destroy();
+    const ObjectStateActive = stateFactory.extend(
+        Active,
+        function () {
+            this.initForm();
+        },
+        function () {
+            if (mediaEditor) {
+                mediaEditor.destroy();
+            }
+            this.widget.$original.off('playerready');
+            this.widget.$form.empty();
         }
-        this.widget.$original.off('playerready');
-        this.widget.$form.empty();
-    });
+    );
 
-    var refreshRendering = _.throttle(function refreshRendering(widget){
-        var obj = widget.element;
-        var $container = widget.$original;
-        var previewOptions = {
-            url : obj.renderer.resolveUrl(obj.attr('data')),
-            mime : obj.attr('type')
+    const refreshRendering = _.throttle(function refreshRendering(widget) {
+        const obj = widget.element;
+        const $container = widget.$original;
+        const previewOptions = {
+            url: obj.renderer.resolveUrl(obj.attr('data')),
+            mime: obj.attr('type')
         };
-        if(obj.attr('height')){
+        if (obj.attr('height')) {
             previewOptions.height = obj.attr('height');
         }
-        if(obj.attr('width')){
+        if (obj.attr('width')) {
             previewOptions.width = obj.attr('width');
         }
-        if(previewOptions.url && previewOptions.mime){
+        if (previewOptions.url && previewOptions.mime) {
             $container.previewer(previewOptions);
         }
     }, _config.renderingThrottle);
 
-    const setMediaSizeEditor = (widget) => {
+    const setMediaSizeEditor = widget => {
         const $form = widget.$form;
         const qtiObject = widget.element;
         const $panelObjectSize = $('.size-panel', $form);
@@ -96,7 +101,9 @@ define([
                     height = 0;
                 } else if (height) {
                     // for old format (px and height is set) the default % is calculated on rendered width and height
-                    const scaleHeight = (Math.max(height || 0, 200) - $container.find('.mediaplayer .controls').height()) / originalSize.height;
+                    const scaleHeight =
+                        (Math.max(height || 0, 200) - $container.find('.mediaplayer .controls').height()) /
+                        originalSize.height;
                     const scaleWidth = Math.max(width || 0, 200) / originalSize.width;
                     const scale = Math.min(scaleHeight, scaleWidth);
                     width = Math.round(100 / (containerWidth / (scale * originalSize.width)));
@@ -104,9 +111,9 @@ define([
                     height = 0;
                 }
             }
-            const onChange = _.debounce((nMedia) => {
-                if (qtiObject.attr('width') !== (nMedia['width'] + '%')) {
-                    const newWidth = Math.round(nMedia['width']) + '%';
+            const onChange = _.debounce(nMedia => {
+                if (qtiObject.attr('width') !== `${nMedia['width']}%`) {
+                    const newWidth = `${Math.round(nMedia['width'])}%`;
                     qtiObject.attr('width', newWidth);
                     mediaplayer.resize(newWidth, 'auto');
                 }
@@ -114,7 +121,8 @@ define([
             if (mediaEditor) {
                 mediaEditor.destroy();
             }
-            mediaEditor = mediaEditorComponent($panelMediaSize,
+            mediaEditor = mediaEditorComponent(
+                $panelMediaSize,
                 {
                     $node: $container.find('.mediaplayer .media'),
                     $container: $container,
@@ -128,113 +136,55 @@ define([
                         active: true,
                         showResponsiveToggle: false
                     }
-                })
-            .on('change', onChange);
+                }
+            ).on('change', onChange);
         } else {
             $panelObjectSize.show();
             $panelMediaSize.hide();
         }
     };
-    ObjectStateActive.prototype.initForm = function(){
-        var _widget = this.widget,
-            $form = _widget.$form,
-            qtiObject = _widget.element,
-            baseUrl = _widget.options.baseUrl;
-        const $container = _widget.$original;
 
-        $form.html(formTpl({
-            baseUrl : baseUrl || '',
-            src : qtiObject.attr('data'),
-            alt : qtiObject.attr('alt'),
-            height : qtiObject.attr('height'),
-            width : qtiObject.attr('width')
-        }));
-
-        //init resource manager
-        _initUpload(_widget);
-
-        //init standard ui widget
-        formElement.initWidget($form);
-
-        $container.off('playerready').on('playerready', function() {
-            setMediaSizeEditor(_widget);
-        });
-
-        //init data change callbacks
-        formElement.setChangeCallbacks($form, qtiObject, {
-            src : function(object, value){
-                qtiObject.attr('data', value);
-                inlineHelper.togglePlaceholder(_widget);
-                refreshRendering(_widget);
-            },
-            width : function(object, value){
-                var val = parseInt(value, 10);
-                if(_.isNaN(val)){
-                    qtiObject.removeAttr('width');
-                }else{
-                    qtiObject.attr('width', val);
-                }
-                refreshRendering(_widget);
-            },
-            height : function(object, value){
-                var val = parseInt(value, 10);
-                if(_.isNaN(val)){
-                    qtiObject.removeAttr('height');
-                }else{
-                    qtiObject.attr('height', val);
-                }
-                refreshRendering(_widget);
-            },
-            alt : function(qtiObject, value){
-                qtiObject.attr('alt', value);
-            },
-            align : function(qtiObject, value){
-                inlineHelper.positionFloat(_widget, value);
-            }
-        });
-
-    };
-
-    var _initUpload = function(widget){
-
-        var $form = widget.$form,
+    const _initUpload = function (widget) {
+        const $form = widget.$form,
             options = widget.options,
             qtiObject = widget.element,
             $uploadTrigger = $form.find('[data-role="upload-trigger"]'),
             $src = $form.find('input[name=src]');
 
-        var _openResourceMgr = function _openResourceMgr(){
+        const _openResourceMgr = function _openResourceMgr() {
             $uploadTrigger.resourcemgr({
-                title : __('Please select a media file from the resource manager. You can add files from your computer with the button "Add file(s)".'),
-                appendContainer : options.mediaManager.appendContainer,
-                mediaSourcesUrl : options.mediaManager.mediaSourcesUrl,
-                browseUrl : options.mediaManager.browseUrl,
-                uploadUrl : options.mediaManager.uploadUrl,
-                deleteUrl : options.mediaManager.deleteUrl,
-                downloadUrl : options.mediaManager.downloadUrl,
-                fileExistsUrl : options.mediaManager.fileExistsUrl,
-                params : {
-                    uri : options.uri,
-                    lang : options.lang,
-                    filters : _config.fileFilters
+                title: __(
+                    'Please select a media file from the resource manager. You can add files from your computer with the button "Add file(s)".'
+                ),
+                appendContainer: options.mediaManager.appendContainer,
+                mediaSourcesUrl: options.mediaManager.mediaSourcesUrl,
+                browseUrl: options.mediaManager.browseUrl,
+                uploadUrl: options.mediaManager.uploadUrl,
+                deleteUrl: options.mediaManager.deleteUrl,
+                downloadUrl: options.mediaManager.downloadUrl,
+                fileExistsUrl: options.mediaManager.fileExistsUrl,
+                params: {
+                    uri: options.uri,
+                    lang: options.lang,
+                    filters: _config.fileFilters
                 },
-                pathParam : 'path',
-                select : function(e, files){
-                    var file, type;
-                    if(files && files.length){
+                pathParam: 'path',
+                select: function (e, files) {
+                    let file, type;
+                    if (files && files.length) {
                         file = files[0].file;
                         type = files[0].mime;
                         qtiObject.attr('type', type);
                         $src.val(file).trigger('change');
                     }
                 },
-                open : function(){
+                open: function () {
                     //hide tooltip if displayed
-                    if($src.data('$tooltip')){
+                    if ($src.data('$tooltip')) {
                         $src.blur().data('$tooltip').hide();
                     }
                 },
-                close : function(){
+                close: function () {
                     //triggers validation :
                     $src.blur();
                 }
@@ -244,9 +194,69 @@ define([
         $uploadTrigger.on('click', _openResourceMgr);
 
         //if empty, open file manager immediately
-        if(!$src.val()){
+        if (!$src.val()) {
             _openResourceMgr();
         }
+    };
+    ObjectStateActive.prototype.initForm = function () {
+        const _widget = this.widget,
+            $form = _widget.$form,
+            qtiObject = _widget.element,
+            baseUrl = _widget.options.baseUrl;
+        const $container = _widget.$original;
+
+        $form.html(
+            formTpl({
+                baseUrl: baseUrl || '',
+                src: qtiObject.attr('data'),
+                alt: qtiObject.attr('alt'),
+                height: qtiObject.attr('height'),
+                width: qtiObject.attr('width')
+            })
+        );
+
+        //init resource manager
+        _initUpload(_widget);
+
+        //init standard ui widget
+        formElement.initWidget($form);
+
+        $container.off('playerready').on('playerready', function () {
+            setMediaSizeEditor(_widget);
+        });
+
+        //init data change callbacks
+        formElement.setChangeCallbacks($form, qtiObject, {
+            src: function (object, value) {
+                qtiObject.attr('data', value);
+                inlineHelper.togglePlaceholder(_widget);
+                refreshRendering(_widget);
+            },
+            width: function (object, value) {
+                const val = parseInt(value, 10);
+                if (_.isNaN(val)) {
+                    qtiObject.removeAttr('width');
+                } else {
+                    qtiObject.attr('width', val);
+                }
+                refreshRendering(_widget);
+            },
+            height: function (object, value) {
+                const val = parseInt(value, 10);
+                if (_.isNaN(val)) {
+                    qtiObject.removeAttr('height');
+                } else {
+                    qtiObject.attr('height', val);
+                }
+                refreshRendering(_widget);
+            },
+            alt: function (object, value) {
+                qtiObject.attr('alt', value);
+            },
+            align: function (object, value) {
+                inlineHelper.positionFloat(_widget, value);
+            }
+        });
     };
 
     return ObjectStateActive;
