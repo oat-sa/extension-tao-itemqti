4048678c6577e966dd3b8d16c6f293fc0c2b3888
Hanna Dzmitryieva <bziondik@gmail.com>
2021-01-28 15:34:28 +0300

    fix: event click outside

--- views/js/qtiCreator/editor/blockAdder/blockAdder.js
+++ views/js/qtiCreator/editor/blockAdder/blockAdder.js
@@ -82,8 +82,8 @@ define([
                 });
 
             //when clicking outside of the selector popup, consider it done
-            $editorPanel.on('ready.qti-widget', function (e) {
-                $itemEditorPanel.off(`click${_ns} mousedown${_ns}`).on(`click${_ns} mousedown${_ns}`, function () {
+            $editorPanel.on('ready.qti-widget', function () {
+                $itemEditorPanel.off(`click${_ns} mousedown${_ns}`).on(`click${_ns} mousedown${_ns}`, function (e) {
                     const popup = selector.getPopup()[0];
                     if (widget && widget.element && popup !== e.target && !$.contains(popup, e.target)) {
                         _done($wrap);
