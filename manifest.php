2af8f89f84cf09f1dc81ed88cfbf54eed98d5508
Hanna Dzmitryieva <bziondik@gmail.com>
2021-01-28 16:11:42 +0300

    chore: bump version

--- manifest.php
+++ manifest.php
@@ -43,7 +43,7 @@ return [
     'name'        => 'taoQtiItem',
     'label'       => 'QTI item model',
     'license'     => 'GPL-2.0',
-    'version'     => '25.23.1',
+    'version'     => '25.23.3',
     'author'      => 'Open Assessment Technologies',
     'requires' => [
         'taoItems' => '>=10.19.0',
