c0f610022e742363eca20a4606d475ae75e9a9f1
Aleksej Tikhanovich <aleksej@taotesting.com>
2021-02-05 13:44:30 +0300

    fix: plugins array without indexes in the config

---
+++ migrations/Version202102051019351101_taoQtiItem.php
@@ -0,0 +1,46 @@
+<?php
+
+declare(strict_types=1);
+
+namespace oat\taoQtiItem\migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use oat\tao\model\ClientLibConfigRegistry;
+use oat\tao\scripts\tools\migrations\AbstractMigration;
+use oat\taoQtiItem\model\QtiCreatorClientConfigRegistry;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version202102051019351101_taoQtiItem extends AbstractMigration
+{
+
+    public function getDescription(): string
+    {
+        return 'Actualize the plugins is client_lib_config_registry.conf';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $registry = ClientLibConfigRegistry::getRegistry();
+
+        if ($registry->isRegistered(QtiCreatorClientConfigRegistry::CREATOR)) {
+            /** @var array $config */
+            $config = $registry->get(QtiCreatorClientConfigRegistry::CREATOR);
+            $plugins = [];
+            if (!empty($config['plugins'])) {
+                foreach ($config['plugins'] as $plugin) {
+                    $plugins[] = $plugin;
+                }
+            }
+            $config['plugins'] = $plugins;
+            $registry->set(QtiCreatorClientConfigRegistry::CREATOR, $config);
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+
+    }
+}
