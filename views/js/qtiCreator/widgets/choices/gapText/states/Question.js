88ca92429d1145c0e78052100ed9f011886ca1cf
anastasia <nastya.razum96@gmail.com>
2021-02-21 04:08:10 +0300

    fix: add check of ckeditor existence in gaptext

--- views/js/qtiCreator/widgets/choices/gapText/states/Question.js
+++ views/js/qtiCreator/widgets/choices/gapText/states/Question.js
@@ -24,70 +24,77 @@ define([
     'taoQtiItem/qtiCreator/widgets/choices/states/Question',
     'taoQtiItem/qtiItem/core/Element',
     'taoQtiItem/qtiCreator/editor/ckEditor/htmlEditor',
-    'taoQtiItem/qtiCreator/editor/gridEditor/content',
-], function($, _, CKEditor, stateFactory, Question, Element, htmlEditor, contentHelper){
+    'taoQtiItem/qtiCreator/editor/gridEditor/content'
+], function ($, _, CKEditor, stateFactory, Question, Element, htmlEditor, contentHelper) {
     'use strict';
 
-    var GapTextStateQuestion = stateFactory.extend(Question, function(){
-        this.buildEditor();
-    }, function(){
-        this.destroyEditor();
-    });
+    const GapTextStateQuestion = stateFactory.extend(
+        Question,
+        function () {
+            this.buildEditor();
+        },
+        function () {
+            this.destroyEditor();
+        }
+    );
 
     GapTextStateQuestion.prototype.initForm = _.noop();
 
-    GapTextStateQuestion.prototype.buildEditor = function(){
-
-        var _widget = this.widget,
+    GapTextStateQuestion.prototype.buildEditor = function () {
+        const _widget = this.widget,
             container = _widget.element.getBody(),
             $editableContainer = _widget.$container;
 
         $editableContainer.attr('data-html-editable-container', true);
 
-        if(!htmlEditor.hasEditor($editableContainer)){
-
+        if (!htmlEditor.hasEditor($editableContainer)) {
             htmlEditor.buildEditor($editableContainer, {
-                change : contentHelper.getChangeCallback(container),
-                data : {
-                    container : container,
-                    widget : _widget
+                change: contentHelper.getChangeCallback(container),
+                data: {
+                    container: container,
+                    widget: _widget
                 },
-                toolbar: [{
-                    name : 'basicstyles',
-                    items : ['Bold', 'Italic', 'Subscript', 'Superscript']
-                }, {
-                    name : 'insert',
-                    items : ['SpecialChar']
-                }],
+                toolbar: [
+                    {
+                        name: 'basicstyles',
+                        items: ['Bold', 'Italic', 'Subscript', 'Superscript']
+                    },
+                    {
+                        name: 'insert',
+                        items: ['SpecialChar']
+                    }
+                ],
                 qtiMedia: false,
                 qtiInclude: false,
-                enterMode : CKEditor.ENTER_BR
+                enterMode: CKEditor.ENTER_BR
             });
         }
 
-        $editableContainer.on('keypress.qti-widget', function(e){
-            if(e.which === 13){
+        $editableContainer.on('keypress.qti-widget', function (e) {
+            if (e.which === 13) {
                 e.preventDefault();
                 $(this).blur();
             }
-
         });
     };
 
-    GapTextStateQuestion.prototype.destroyEditor = function(){
-        var _widget = this.widget,
+    GapTextStateQuestion.prototype.destroyEditor = function () {
+        const _widget = this.widget,
             interaction = _widget.interaction,
             $editableContainer = _widget.$container,
-            $editable = $editableContainer.find('[data-html-editable]'),
-            placeholder;
+            $editable = $editableContainer.find('[data-html-editable]');
+
+        if (!htmlEditor.hasEditor($editableContainer)) {
+            return;
+        }
 
         /**
          * Workaround for ckEditor. When adding text to an empty choice, when initialising the widget on an empty content,
          * ck automatically wraps it in a <p>, which breaks QTI compatibility.
          * Therefore, we replace an empty choice with a placeholder text.
          */
-        if (! htmlEditor.getData($editable) || htmlEditor.getData($editable) === '') {
-            placeholder = interaction.getNextPlaceholder();
+        if (!htmlEditor.getData($editable) || htmlEditor.getData($editable) === '') {
+            const placeholder = interaction.getNextPlaceholder();
             htmlEditor.setData($editable, placeholder);
         }
 
