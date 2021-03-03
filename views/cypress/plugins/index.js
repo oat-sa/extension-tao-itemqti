17ca2305fa53887f0639d4004c7c475aebdb0a67
anastasia <nastya.razum96@gmail.com>
2021-03-03 20:49:53 +0300

    feat: add e2e runner

---
+++ views/cypress/plugins/index.js
@@ -0,0 +1,5 @@
+const plugins = require('@oat-sa-private/e2e-runner/plugins/index.js');
+
+module.exports = function (on, config) {
+    plugins(on, config);
+}
---
+++ views/cypress/support/index.js
@@ -0,0 +1,2 @@
+require('@oat-sa-private/e2e-runner/support/index.js'); // import shared resources
+require('./commands'); // import local commands
