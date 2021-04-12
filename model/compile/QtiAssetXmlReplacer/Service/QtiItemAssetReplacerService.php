bbbd2107abc37ce793bbb9adfe954fe06f157616
uncleempty <ilya@taotesting.com>
2021-04-12 11:20:08 +0300

    feat: Created QtiItemAssetReplacer service

---
+++ model/compile/QtiAssetXmlReplacer/Service/QtiItemAssetReplacerService.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace oat\taoQtiItem\model\compile\QtiAssetXmlReplacer\Service;
+
+use oat\oatbox\service\ConfigurableService;
+use \DOMDocument;
+use oat\taoQtiItem\model\compile\QtiAssetXmlReplacer\Replacer\QtiItemAssetReplacerInterface;
+
+class QtiItemAssetReplacerService extends ConfigurableService implements QtiItemAssetReplacerServiceInterface
+{
+    public function getType(): int
+    {
+        return $this->getOption(QtiItemAssetReplacerServiceInterface::OPTION_TYPE);
+    }
+
+    public function replace(DOMDocument $dom, array $packedAssets): void
+    {
+        if ($this->getType() === QtiItemAssetReplacerServiceInterface::TYPE_SINGLE) {
+            /** @var QtiItemAssetReplacerServiceInterface $callable */
+            $callable = $this->getOption(QtiItemAssetReplacerServiceInterface::OPTION_CALLABLE);
+
+            $this->callReplacer($callable, $dom, $packedAssets);
+        } else {
+            $callables = $this->getOption(QtiItemAssetReplacerServiceInterface::OPTION_CALLABLE_CHAIN);
+
+            /** @var QtiItemAssetReplacerServiceInterface $callable */
+            foreach ($callables as $callable) {
+                $this->callReplacer($callable, $dom, $packedAssets);
+            }
+        }
+    }
+
+    private function callReplacer($replacer, DOMDocument $dom, array $packedAssets): void
+    {
+        if ($replacer instanceof QtiItemAssetReplacerInterface) {
+            $this->logInfo('Going to replace asset link');
+            $replacer->replace($dom, $packedAssets);
+        } else {
+            $this->logInfo("Failed to replace link in `" . get_class($replacer) . "`");
+            throw new \Exception('dead on exec step');
+        }
+    }
+}
---
+++ model/compile/QtiAssetXmlReplacer/Service/QtiItemAssetReplacerServiceInterface.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace oat\taoQtiItem\model\compile\QtiAssetXmlReplacer\Service;
+
+use \DOMDocument;
+use oat\taoQtiItem\model\pack\QtiAssetPacker\PackedAsset;
+
+interface QtiItemAssetReplacerServiceInterface
+{
+    public const SERVICE_ID = 'taoQtiItem/QtiItemAssetReplacer';
+
+    public const OPTION_TYPE = 'type';
+    public const OPTION_CALLABLE = 'callable';
+    public const OPTION_CALLABLE_CHAIN = 'chain';
+
+    public const TYPE_SINGLE = 1;
+
+    public const TYPE_CHAINED = 2;
+
+    /**
+     * return type of the current replacer implemented
+     *
+     * @return int
+     */
+    public function getType(): int;
+
+    /**
+     * replaces the assets
+     *
+     * @param \DOMDocument $dom
+     * @param PackedAsset[] $packedAssets
+     *
+     * @throws \Exception
+     */
+    public function replace(DOMDocument $dom, array $packedAssets): void;
+}
