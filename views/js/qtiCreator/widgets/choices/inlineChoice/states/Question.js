0533e053c4428895e719f93115801de7f150dd67
anastasia <nastya.razum96@gmail.com>
2021-04-13 12:39:31 +0300

    feat: add ckeditor call in inline choice

--- views/js/qtiCreator/widgets/choices/inlineChoice/states/Question.js
+++ views/js/qtiCreator/widgets/choices/inlineChoice/states/Question.js
@@ -1,10 +1,13 @@
 define([
     'jquery',
+    'lodash',
+    'ckeditor',
     'taoQtiItem/qtiCreator/widgets/states/factory',
     'taoQtiItem/qtiCreator/widgets/choices/states/Question',
     'taoQtiItem/qtiCreator/widgets/choices/helpers/formElement',
-    'lodash'
-], function($, stateFactory, QuestionState, formElement, _){
+    'taoQtiItem/qtiCreator/editor/ckEditor/htmlEditor',
+    'taoQtiItem/qtiCreator/editor/gridEditor/content'
+], function($, _, CKEditor, stateFactory, QuestionState, formElement, htmlEditor, contentHelper) {
     'use strict';
 
     var ChoiceStateQuestion = stateFactory.extend(QuestionState, function initStateQuestion(){
@@ -27,23 +30,47 @@ define([
 
     ChoiceStateQuestion.prototype.buildEditor = function(){
 
-        var _widget = this.widget;
+        var _widget = this.widget,
+            container = _widget.element.getBody(),
+            $editableContainer = _widget.$container.find('.editable-content');
 
-        _widget.$container.find('.editable-content')
+        if (!htmlEditor.hasEditor($editableContainer)) {
+            htmlEditor.buildEditor($editableContainer, {
+                change : contentHelper.getChangeCallback(container),
+                data : {
+                    container,
+                    widget : _widget
+                },
+                toolbar: [{
+                    name : 'basicstyles',
+                    items : ['Bold', 'Italic', 'Subscript', 'Superscript']
+                }, {
+                    name : 'insert',
+                    items : ['SpecialChar']
+                }],
+                qtiMedia: false,
+                qtiInclude: false,
+                enterMode : CKEditor.ENTER_BR
+            });
+        }
+
+        $editableContainer
             .attr('contentEditable', true)
-            .on('keyup.qti-widget', _.throttle(function(){
-
-                //update model
-                _widget.element.val(_.escape($(this).text()));
-
-                //update placeholder
-                _widget.$original.width($(this).width());
-
-            }, 200)).on('focus.qti-widget', function(){
+            // .on('keyup.qti-widget', _.throttle(function() {
+            //
+            //     //update model
+            //     _widget.element.val(_.escape($(this).text()));
+            //
+            //     //update placeholder
+            //     _widget.$original.width($(this).width());
+            //
+            // }, 200))
+            .on('focus.qti-widget', function(){
 
                 _widget.changeState('choice');
 
-            }).on('keypress.qti-widget', function(e){
+            })
+            .on('keypress.qti-widget', function(e){
 
                 if(e.which === 13){
                     e.preventDefault();
