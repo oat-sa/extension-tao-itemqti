20b555afc7bbaf0a8e2dd8d375e8e2490fdf5764
AnastasiaArcadia <nastya.razum96@gmail.com>
2021-05-13 01:00:28 +0300

    fix: return rows/cols deletion from table

--- views/js/qtiCreator/widgets/static/table/states/Active.js
+++ views/js/qtiCreator/widgets/static/table/states/Active.js
@@ -68,6 +68,18 @@ define([
         }, 400);
     }
 
+    // delete all widgets from column or row
+    function deleteInnerWidgets(container, colRows) {
+        colRows.each(function () {
+            $(this).find('.widget-box').each(function () {
+                const serial = $(this).data('serial');
+                if (container.elements[serial]) {
+                    delete container.elements[serial];
+                }
+            });
+        });
+    }
+
     TableStateActive.prototype.initForm = function initForm(){
         var _widget     = this.widget,
             $form       = _widget.$form,
@@ -164,6 +176,9 @@ define([
                     deleteTitle: 'Delete column'
                 })
                     .on('delete', function() {
+                        const cols = getCurrentColCells(editor);
+                        deleteInnerWidgets(container, cols);
+
                         editor.execCommand('columnDelete');
                         updateTable(editor, $editableContainer);
                         hideTableActions();
@@ -194,7 +209,11 @@ define([
                     insertRow: true,
                     deleteTitle: 'Delete row'
                 })
+
                     .on('delete', function() {
+                        const rows = getCurrentRowCells(editor);
+                        deleteInnerWidgets(container, rows);
+
                         editor.execCommand('rowDelete');
                         updateTable(editor, $editableContainer);
                         hideTableActions();
@@ -359,6 +378,20 @@ define([
                 x: currentCell.cellIndex,
                 y: currentCell.parentNode.rowIndex
             };
+        }
+    }
+
+    function getCurrentRowCells(editor) {
+        const currentCellPos = getCurrentCellPos(editor);
+        if (currentCellPos) {
+            return tableModel.getRowCells(currentCellPos.y)
+        }
+    }
+
+    function getCurrentColCells(editor) {
+        const currentCellPos = getCurrentCellPos(editor);
+        if (currentCellPos) {
+            return tableModel.getColCells(currentCellPos.x)
         }
     }
 
