6290cf405b6653582af540e64de686c09c5cc94c
Gabriel Felipe Soares <gabriel.felipe.soares@taotesting.com>
2021-05-18 13:36:28 +0200

    chore: fix unit test

--- test/unit/model/import/Report/ErrorReportFormatterTest.php
+++ test/unit/model/import/Report/ErrorReportFormatterTest.php
@@ -18,33 +18,49 @@
  * Copyright (c) 2021  (original work) Open Assessment Technologies SA;
  */
 
+declare(strict_types=1);
+
 namespace oat\taoQtiItem\model\import\Report;
 
+use oat\oatbox\reporting\Report;
 use oat\taoQtiItem\model\import\Parser\Exception\InvalidImportException;
 use PHPUnit\Framework\TestCase;
 
 class ErrorReportFormatterTest extends TestCase
 {
+    /** @var ErrorReportFormatter */
+    private $subject;
+
+    public function setUp(): void
+    {
+        $this->subject = new ErrorReportFormatter();
+    }
+
     public function testFormat(): void
     {
-        $sut = new ErrorReportFormatter();
+        $error1 = new InvalidImportException();
+        $error1->addError(1, 'e1.1');
+        $error1->addError(1, 'e1.2');
 
         $error2 = new InvalidImportException();
-        $error2->addError(2, 'oh');
-        $error2->addError(2, 'ah');
-        $error2->addError(2, 'oi');
+        $error2->addError(2, 'e2.1');
+        $error2->addError(2, 'e2.2');
 
-        $error1 = new InvalidImportException();
-        $error1->addError(1, 'e1');
-        $error1->addError(1, 'e2');
-        $error1->addError(1, 'e3');
+        $output = $this->subject->format(
+            [
+                1 => $error1,
+                2 => $error2,
+            ]
+        );
 
-        $reports = [
-            2 => $error2,
-            1 => $error1
-        ];
-        $output = $sut->format($reports);
+        /** @var Report $report1 */
+        $report1 = $output->getChildren()[0];
+
+        /** @var Report $report2 */
+        $report2 = $output->getChildren()[1];
+
         $this->assertCount(2, $output->getChildren());
-        $this->assertContains('e1', $output->getChildren()[0]->getMessage());
+        $this->assertSame('line 1: e1.1, e1.2', $report1->getMessage());
+        $this->assertSame('line 2: e2.1, e2.2', $report2->getMessage());
     }
 }
