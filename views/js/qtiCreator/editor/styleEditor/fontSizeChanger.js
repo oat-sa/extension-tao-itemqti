3138d613c9cac2448dc13593382b5b1dca4afdd5
Hanna Dzmitryieva <bziondik@gmail.com>
2021-07-20 13:04:23 +0300

    fix: revert changes in fontSizeChanger

--- views/js/qtiCreator/editor/styleEditor/fontSizeChanger.js
+++ views/js/qtiCreator/editor/styleEditor/fontSizeChanger.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2015-2021 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2015 (original work) Open Assessment Technologies SA ;
  *
  */
 
@@ -21,38 +21,44 @@
  *
  * @author dieter <dieter@taotesting.com>
  */
-define(['jquery', 'lodash', 'taoQtiItem/qtiCreator/editor/styleEditor/styleEditor'], function ($, _, styleEditor) {
+ define([
+    'jquery',
+    'lodash',
+    'taoQtiItem/qtiCreator/editor/styleEditor/styleEditor'
+], function ($, _, styleEditor) {
     'use strict';
+
 
     /**
      * Changes the font size in the Style Editor
      */
-    const fontSizeChanger = function () {
-        const $fontSizeChanger = $('#item-editor-font-size-changer'),
+    var fontSizeChanger = function () {
+        var $fontSizeChanger = $('#item-editor-font-size-changer'),
             itemSelector = $fontSizeChanger.data('target'),
             $item = $(itemSelector),
-            $resetBtn = $fontSizeChanger.parents('.reset-group').find('[data-role="font-size-reset"]'),
+            itemFontSize = parseInt($item.css('font-size'), 10),
+            $resetBtn =  $fontSizeChanger.parents('.reset-group').find('[data-role="font-size-reset"]'),
             $input = $('#item-editor-font-size-text');
-        let itemFontSize = parseInt($item.css('font-size'), 10);
 
         /**
          * Writes new font size to virtual style sheet
          */
-        const resizeFont = function () {
-            styleEditor.apply(itemSelector, 'font-size', `${itemFontSize.toString()}px`);
+        var resizeFont = function() {
+            styleEditor.apply(itemSelector + ' *', 'font-size', itemFontSize.toString() + 'px');
         };
 
         /**
          * Handle input field
          */
-        $fontSizeChanger.find('a').on('click', function (e) {
+        $fontSizeChanger.find('a').on('click', function(e) {
             e.preventDefault();
-            if ($(this).data('action') === 'reduce') {
-                if (itemFontSize <= 10) {
+            if($(this).data('action') === 'reduce') {
+                if(itemFontSize <= 10) {
                     return;
                 }
                 itemFontSize--;
-            } else {
+            }
+            else {
                 itemFontSize++;
             }
             resizeFont();
@@ -63,15 +69,17 @@ define(['jquery', 'lodash', 'taoQtiItem/qtiCreator/editor/styleEditor/styleEdito
         /**
          * Disallows invalid characters
          */
-        $input.on('keydown', function (e) {
-            const c = e.keyCode;
-            return _.contains([8, 37, 39, 46], c) || (c >= 48 && c <= 57) || (c >= 96 && c <= 105);
+        $input.on('keydown', function(e) {
+            var c = e.keyCode;
+            return (_.contains([8, 37, 39, 46], c) ||
+                (c >= 48 && c <= 57) ||
+                (c >= 96 && c <= 105));
         });
 
         /**
          * Apply font size on blur
          */
-        $input.on('blur', function () {
+        $input.on('blur', function() {
             itemFontSize = parseInt(this.value, 10);
             resizeFont();
         });
@@ -79,9 +87,9 @@ define(['jquery', 'lodash', 'taoQtiItem/qtiCreator/editor/styleEditor/styleEdito
         /**
          * Apply font size on enter
          */
-        $input.on('keydown', function (e) {
-            const c = e.keyCode;
-            if (c === 13) {
+        $input.on('keydown', function(e) {
+            var c = e.keyCode;
+            if(c === 13) {
                 $input.trigger('blur');
             }
         });
@@ -91,25 +99,18 @@ define(['jquery', 'lodash', 'taoQtiItem/qtiCreator/editor/styleEditor/styleEdito
          */
         $resetBtn.on('click', function () {
             $input.val('');
-            styleEditor.apply(itemSelector, 'font-size');
+            styleEditor.apply(itemSelector + ' *', 'font-size');
         });
 
         /**
          * style loaded from style sheet
          */
-        $(document).on('customcssloaded.styleeditor', function (e, style) {
-            if (style[itemSelector] && style[itemSelector]['font-size']) {
+        $(document).on('customcssloaded.styleeditor', function(e, style) {
+            if(style[itemSelector] && style[itemSelector]['font-size']) {
                 $input.val(parseInt(style[itemSelector]['font-size'], 10));
                 $input.trigger('blur');
-            } if (style[`${itemSelector} *`] && style[`${itemSelector} *`]['font-size']) {
-                const fontSize = style[`${itemSelector} *`]['font-size'];
-                // apply new selector
-                styleEditor.apply(itemSelector, 'font-size', fontSize);
-                // remove old selector from styles
-                styleEditor.apply(`${itemSelector} *`, 'font-size');
-                $input.val(parseInt(fontSize, 10));
-                $input.trigger('blur');
-            } else {
+            }
+            else {
                 $input.val(parseInt($item.css('font-size'), 10));
             }
         });
