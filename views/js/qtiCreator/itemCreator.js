7404b1e0f780a60bf40ff121e224aef23ca6381c
AnastasiaArcadia <nastya.razum96@gmail.com>
2021-08-25 01:23:41 +0300

    refactor: code style

--- views/js/qtiCreator/itemCreator.js
+++ views/js/qtiCreator/itemCreator.js
@@ -44,9 +44,9 @@ define([
     'taoQtiItem/qtiCreator/editor/propertiesPanel',
     'taoQtiItem/qtiCreator/model/helper/event',
     'taoQtiItem/qtiCreator/editor/styleEditor/styleEditor'
-], function($, _, __, module, eventifier, Promise, ciRegistry, icRegistry, qtiCreatorContextFactory, itemLoader,
-            creatorRenderer, commonRenderer, xincludeRenderer,
-            interactionPanel, propertiesPanel, eventHelper, styleEditor){
+], function ($, _, __, module, eventifier, Promise, ciRegistry, icRegistry, qtiCreatorContextFactory, itemLoader,
+             creatorRenderer, commonRenderer, xincludeRenderer,
+             interactionPanel, propertiesPanel, eventHelper, styleEditor) {
     'use strict';
 
     /**
@@ -58,10 +58,15 @@ define([
      *
      * @returns {Promise} that resolve with the loaded item model
      */
-    var loadItem = function loadItem(uri, label, itemDataUrl, perInteractionRp){
-        return new Promise(function(resolve, reject){
-            itemLoader.loadItem({uri : uri, label : label, itemDataUrl: itemDataUrl, perInteractionRp }, function(item){
-                if(!item){
+    var loadItem = function loadItem(uri, label, itemDataUrl, perInteractionRp) {
+        return new Promise(function (resolve, reject) {
+            itemLoader.loadItem({
+                uri: uri,
+                label: label,
+                itemDataUrl: itemDataUrl,
+                perInteractionRp
+            }, function (item) {
+                if (!item) {
                     reject(new Error('Unable to load the item'));
                 }
 
@@ -77,9 +82,9 @@ define([
      *
      * @returns {Promise} that resolve with the loaded item model
      */
-    var loadCustomInteractions = function loadCustomInteractions(interactionsIds){
+    var loadCustomInteractions = function loadCustomInteractions(interactionsIds) {
         return ciRegistry.loadCreators({
-            include : interactionsIds
+            include: interactionsIds
         });
     };
 
@@ -88,7 +93,7 @@ define([
      *
      * @returns {Promise} that resolve with the loaded item model
      */
-    var loadInfoControls = function loadInfoControls(){
+    var loadInfoControls = function loadInfoControls() {
         return icRegistry.loadCreators();
     };
 
@@ -106,7 +111,7 @@ define([
      * @returns {itemCreator} an event emitter object, with the usual lifecycle
      * @throws {TypeError}
      */
-    var itemCreatorFactory = function itemCreatorFactory(config, areaBroker, pluginFactories){
+    var itemCreatorFactory = function itemCreatorFactory(config, areaBroker, pluginFactories) {
 
         var itemCreator;
         var qtiCreatorContext = qtiCreatorContextFactory();
@@ -118,11 +123,11 @@ define([
          * @param {String} method - the method to run
          * @returns {Promise} once that resolve when all plugins are done
          */
-        var pluginRun =  function pluginRun(method){
+        var pluginRun = function pluginRun(method) {
             var execStack = [];
 
-            _.forEach(plugins, function (plugin){
-                if(_.isFunction(plugin[method])){
+            _.forEach(plugins, function (plugin) {
+                if (_.isFunction(plugin[method])) {
                     execStack.push(plugin[method]());
                 }
             });
@@ -131,13 +136,13 @@ define([
         };
 
         //validate parameters
-        if(!_.isPlainObject(config)){
+        if (!_.isPlainObject(config)) {
             throw new TypeError('The item creator configuration is required');
         }
-        if(!config.properties || _.isEmpty(config.properties.uri) || _.isEmpty(config.properties.label) || _.isEmpty(config.properties.baseUrl)){
+        if (!config.properties || _.isEmpty(config.properties.uri) || _.isEmpty(config.properties.label) || _.isEmpty(config.properties.baseUrl)) {
             throw new TypeError('The creator configuration must contains the required properties triples: uri, label and baseUrl');
         }
-        if(!areaBroker){
+        if (!areaBroker) {
             throw new TypeError('Without an areaBroker there are no chance to see something you know');
         }
 
@@ -156,11 +161,11 @@ define([
              * @fires itemCreator#init - once initialized
              * @fires itemCreator#error - if something went wrong
              */
-            init: function init(){
+            init: function init() {
                 var self = this;
 
                 //instantiate the plugins first
-                _.forEach(pluginFactories, function(pluginFactory){
+                _.forEach(pluginFactories, function (pluginFactory) {
                     var plugin = pluginFactory(self, areaBroker);
                     plugins[plugin.getName()] = plugin;
                 });
@@ -176,37 +181,37 @@ define([
                  * @fires itemCreator#saved once the save is done
                  * @fires itemCreator#error
                  */
-                this.on('save', function(silent){
+                this.on('save', function (silent) {
                     var item = this.getItem();
                     var itemWidget = item.data('widget');
 
                     //do the save
                     styleEditor.save()
                         .then(() => itemWidget.save())
-                        .then(function(){
-                            if(!silent){
+                        .then(function () {
+                            if (!silent) {
                                 self.trigger('success', __('Your item has been saved'));
                             }
 
                             self.trigger('saved');
-                        }).catch(function(err){
+                        }).catch(function (err) {
                         self.trigger('error', err);
                     });
                 });
 
-                this.on('exit', function() {
+                this.on('exit', function () {
                     $('.item-editor-item', areaBroker.getItemPanelArea()).empty();
                 });
 
                 var usedCustomInteractionIds = [];
-                loadItem(config.properties.uri, config.properties.label, config.properties.itemDataUrl, config.properties.perInteractionRp).then(function(item){
-                    if(! _.isObject(item)){
+                loadItem(config.properties.uri, config.properties.label, config.properties.itemDataUrl, config.properties.perInteractionRp).then(function (item) {
+                    if (!_.isObject(item)) {
                         self.trigger('error', new Error('Unable to load the item ' + config.properties.label));
                         return;
                     }
 
-                    _.forEach(item.getComposingElements(), function(element){
-                        if(element.is('customInteraction')){
+                    _.forEach(item.getComposingElements(), function (element) {
+                        if (element.is('customInteraction')) {
                             usedCustomInteractionIds.push(element.typeIdentifier);
                         }
                     });
@@ -231,22 +236,22 @@ define([
                             if (!item.getOutcomeDeclaration(outcomeIdentifier)) {
                                 item
                                     .createOutcomeDeclaration({
-                                        cardinality : 'single',
-                                        baseType : 'float'
+                                        cardinality: 'single',
+                                        baseType: 'float'
                                     })
                                     .attr('identifier', outcomeIdentifier);
                             }
                         });
                     }
-                }).then(function(){
+                }).then(function () {
                     //load custom elements
                     return Promise.all([
                         loadCustomInteractions(usedCustomInteractionIds),
                         loadInfoControls()
                     ]);
-                }).then(function(){
+                }).then(function () {
                     //initialize all the plugins
-                    return pluginRun('init').then(function(){
+                    return pluginRun('init').then(function () {
 
                         /**
                          * @event itemCreator#init the initialization is done
@@ -254,13 +259,13 @@ define([
                          */
                         self.trigger('init', self.item);
                     });
-                }).then(function() {
+                }).then(function () {
                     // forward context error
-                    qtiCreatorContext.on('error', function(err) {
+                    qtiCreatorContext.on('error', function (err) {
                         self.trigger('error', err);
                     });
                     return qtiCreatorContext.init();
-                }).catch(function(err){
+                }).catch(function (err) {
                     self.trigger('error', err);
                 });
 
@@ -276,11 +281,11 @@ define([
              * @fires itemCreator#ready - once the creator's components' are ready (not yet reliable)
              * @fires itemCreator#error - if something went wrong
              */
-            render : function render(){
+            render: function render() {
                 var self = this;
                 var item = this.getItem();
 
-                if(!item || !_.isFunction(item.getUsedClasses)){
+                if (!item || !_.isFunction(item.getUsedClasses)) {
                     return this.trigger('error', new Error('We need an item to render.'));
                 }
 
@@ -294,8 +299,8 @@ define([
 
                 //the renderers' widgets do not handle async yet, so we rely on this event
                 //TODO ready should be triggered once every renderer's widget is done (ie. promisify everything)
-                $(document).on('ready.qti-widget', function(e, elt){
-                    if(elt.element.qtiClass === 'assessmentItem'){
+                $(document).on('ready.qti-widget', function (e, elt) {
+                    if (elt.element.qtiClass === 'assessmentItem') {
                         self.trigger('ready');
                     }
                 });
@@ -306,7 +311,7 @@ define([
                 creatorRenderer
                     .get(true, config, areaBroker)
                     .setOptions(config.properties)
-                    .load(function(){
+                    .load(function () {
                         var widget;
 
                         //set renderer
@@ -318,14 +323,14 @@ define([
                         //"post-render it" to initialize the widget
                         Promise
                             .all(item.postRender(_.clone(config.properties)))
-                            .then(function(){
+                            .then(function () {
 
                                 //set reference to item widget object
                                 areaBroker.getContainer().data('widget', item);
 
                                 widget = item.data('widget');
-                                _.each(item.getComposingElements(), function(element){
-                                    if(element.qtiClass === 'include'){
+                                _.each(item.getComposingElements(), function (element) {
+                                    if (element.qtiClass === 'include') {
                                         xincludeRenderer.render(element.data('widget'), config.properties.baseUrl);
                                     }
                                 });
@@ -335,11 +340,11 @@ define([
                                 //init event listeners:
                                 eventHelper.initElementToWidgetListeners();
 
-                                return pluginRun('render').then(function(){
+                                return pluginRun('render').then(function () {
                                     self.trigger('render');
                                 });
                             })
-                            .catch(function(err){
+                            .catch(function (err) {
                                 self.trigger('error', err);
                             });
 
@@ -353,16 +358,16 @@ define([
              *
              * @returns {itemCreator} chains
              */
-            destroy : function destroy(){
+            destroy: function destroy() {
                 var self = this;
 
                 $(document).off('.qti-widget');
 
-                pluginRun('destroy').then(function() {
+                pluginRun('destroy').then(function () {
                     return qtiCreatorContext.destroy();
-                }).then(function() {
+                }).then(function () {
                     self.trigger('destroy');
-                }).catch(function(err){
+                }).catch(function (err) {
                     self.trigger('error', err);
                 });
                 return this;
@@ -374,7 +379,7 @@ define([
              * Give an access to the loaded item
              * @returns {Object} the item
              */
-            getItem : function getItem(){
+            getItem: function getItem() {
                 return this.item;
             },
 
@@ -382,7 +387,7 @@ define([
              * Give an access to the config
              * @returns {Object} the config
              */
-            getConfig : function getConfig(){
+            getConfig: function getConfig() {
                 return config;
             }
         });
