3ea71852c0a48085e1f281351e777e8575c8432a
Hanna Dzmitryieva <bziondik@gmail.com>
2021-09-20 20:54:58 +0300

    feat: add interactions to canvas

--- views/cypress/tests/item-authoring.spec.js
+++ views/cypress/tests/item-authoring.spec.js
@@ -18,24 +18,34 @@
 
 import urls from '../utils/urls';
 import selectors from '../utils/selectors';
-
+import paths from '../utils/paths';
+import { selectUploadLocalAssest } from '../utils/resource-manager';
+import { addShapeToImage } from '../utils/graphic-interactions';
 
 describe('Items', () => {
     const className = 'Test E2E class';
     const itemName = 'Test E2E item 1';
     const interactionList = {
-        choice:      'choiceInteraction',
-        order:       'orderInteraction',
-        asociate:    'associateInteraction',
-        match:       'matchInteraction',
-        hottext:     'hottextInteraction',
-        gapMatch:    'gapMatchInteraction',
-        slider:      'sliderInteraction',
-        extendedText:'extendedTextInteraction',
-        upload:      'uploadInteraction',
-        media:       'mediaInteraction'
+        choice: 'choiceInteraction',
+        order: 'orderInteraction',
+        asociate: 'associateInteraction',
+        match: 'matchInteraction',
+        hottext: 'hottextInteraction',
+        gapMatch: 'gapMatchInteraction',
+        slider: 'sliderInteraction',
+        extendedText: 'extendedTextInteraction',
+        upload: 'uploadInteraction',
+        aBlock: '_container'
     };
-
+    const mediaInteraction = 'mediaInteraction';
+    const graphicInteractions = {
+        hotspotInteraction: 'hotspotInteraction',
+        graphicOrderInteraction: 'graphicOrderInteraction',
+        graphicAssociateInteraction: 'graphicAssociateInteraction',
+        graphicGapMatchInteraction: 'graphicGapMatchInteraction',
+        selectPointInteraction: 'selectPointInteraction'
+    };
+    const dropSelector = 'div.qti-itemBody.item-editor-drop-area';
     /**
      * Log in
      * Visit the page
@@ -44,13 +54,13 @@ describe('Items', () => {
         cy.loginAsAdmin();
 
         cy.intercept('POST', '**/edit*').as('edit');
-        cy.intercept('POST', `**/${ selectors.editClassLabelUrl }`).as('editClassLabel')
+        cy.intercept('POST', `**/${selectors.editClassLabelUrl}`).as('editClassLabel');
         cy.viewport(1000, 660);
         cy.visit(urls.items);
         cy.wait('@edit');
 
         cy.get(selectors.root).then(root => {
-            if ((root.find(`li[title="${className}"] a`).length)) {
+            if (root.find(`li[title="${className}"] a`).length) {
                 cy.deleteClassFromRoot(
                     selectors.root,
                     selectors.itemClassForm,
@@ -83,23 +93,9 @@ describe('Items', () => {
             cy.renameSelectedItem(selectors.itemForm, selectors.editItemUrl, itemName);
 
             cy.get(selectors.authoring).click();
-            cy.location().should((loc) => {
+            cy.location().should(loc => {
                 expect(`${loc.pathname}${loc.search}`).to.eq(urls.itemAuthoring);
             });
-        });
-
-        it('can add interactions to canvas', () => {
-            cy.getSettled('.qti-item.item-editor-item.edit-active').should('exist');
-            const dropSelector = 'div.qti-itemBody.item-editor-drop-area';
-            for (const interaction in interactionList) {
-                const dragSelector = `[data-qti-class="${interactionList[interaction]}"]`;
-                cy.dragAndDrop(
-                    dragSelector,
-                    dropSelector
-                );
-                // check that widget is initialized
-                cy.getSettled(`${dropSelector} .widget-box.edit-active${dragSelector}`).should('exist');
-            }
         });
 
         it('should be "Manage Items" button', function () {
@@ -110,8 +106,8 @@ describe('Items', () => {
             cy.get('[data-testid="save-the-item"]').should('have.length', 1);
         });
 
-        it('should be "Preview Item" button', function () {
-            cy.get('[data-testid="preview-the-item"]').should('have.length', 1);
+        it('should be disabled "Preview Item" button', function () {
+            cy.get('[data-testid="preview-the-item"]').should('have.class', 'disabled');
         });
 
         it('should be item editor panel', function () {
@@ -124,6 +120,71 @@ describe('Items', () => {
 
         it('should be interactions left panel', function () {
             cy.get('#item-editor-interaction-bar').should('have.length', 1);
+            // open all interactions
+            cy.get('#sidebar-left-section-inline-interactions').click();
+            cy.get('#sidebar-left-section-graphic-interactions').click();
+        });
+
+        it('can add common interactions to canvas', () => {
+            cy.getSettled('.qti-item.item-editor-item.edit-active').should('exist');
+            for (const interaction in interactionList) {
+                cy.log('ADDING INTERACTION', interaction);
+                const interactionSelector = `[data-qti-class="${interactionList[interaction]}"]`;
+                cy.dragAndDrop(interactionSelector, dropSelector);
+                // check that widget is initialized
+                cy.getSettled(`${dropSelector} .widget-box.edit-active${interactionSelector}`).should('exist');
+                cy.log(interaction, 'IS ADDED');
+            }
+        });
+        it('can add media interaction to canvas and upload video', () => {
+            cy.log('ADDING INTERACTION', mediaInteraction);
+            const interactionSelector = `[data-qti-class="${mediaInteraction}"]`;
+            const videoName = 'sample-mp4-file.mp4';
+            cy.dragAndDrop(interactionSelector, dropSelector);
+            // check that widget is initialized
+            cy.getSettled(`${dropSelector} .widget-box.edit-active${interactionSelector}`).should('exist');
+            // resource selector
+            selectUploadLocalAssest(videoName, `${paths.assetsPath}${videoName}`);
+            cy.log(mediaInteraction, 'IS ADDED');
+        });
+
+        it('can add graphic interactions to canvas and upload image', () => {
+            for (const interaction in graphicInteractions) {
+                cy.log('ADDING INTERACTION', interaction);
+                const interactionSelector = `[data-qti-class="${graphicInteractions[interaction]}"]`;
+                const imageName = 'cats.jpg';
+                const imageOptionName = 'img-option.png';
+                cy.dragAndDrop(interactionSelector, dropSelector);
+                // check that widget is initialized
+                cy.getSettled(`${dropSelector} .widget-box.edit-active${interactionSelector}`).should('exist');
+                selectUploadLocalAssest(imageName, `${paths.assetsPath}${imageName}`);
+
+                // image is loaded to widget
+                cy.getSettled(`.widget-box.edit-active${interactionSelector} .main-image-box image`).should('exist');
+                cy.get(`.widget-box.edit-active${interactionSelector} .image-editor`).then(imageEditor => {
+                    // add shape to image if needed
+                    if (imageEditor.find('li[data-type="rect"]').length) {
+                        addShapeToImage(interactionSelector, 'rect');
+                    }
+                    // add image option if needed
+                    if (imageEditor.find('li.add-option').length) {
+                        cy.get(`.widget-box.edit-active${interactionSelector} .image-editor li.add-option`).click();
+                        selectUploadLocalAssest(imageOptionName, `${paths.assetsPath}${imageOptionName}`);
+                        cy.getSettled(`.widget-box.edit-active${interactionSelector} .source .qti-choice img`).should('exist');
+                    }
+                });
+                cy.log(interaction, 'IS ADDED');
+            }
+        });
+
+        it('can save item with interactions', () => {
+            cy.intercept('POST', '**/saveItem*').as('saveItem');
+            cy.get('[data-testid="save-the-item"]').click();
+            cy.wait('@saveItem').its('response.body').its('success').should('eq', true);
+        });
+
+        it('should be enabled "Preview Item" button', () => {
+            cy.get('[data-testid="preview-the-item"]').should('not.have.class', 'disabled');
         });
     });
 });
---
+++ views/cypress/utils/graphic-interactions.js
@@ -0,0 +1,50 @@
+/**
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2021 Open Assessment Technologies SA ;
+ */
+
+/**
+ * Select file in resource manager or upload it
+ * @param {String} interactionSelector
+ * @param {String} shapeType
+ */
+export function addShapeToImage(interactionSelector, shapeType) {
+    cy.log('ADD SHAPE TO IMAGE', interactionSelector, shapeType);
+    cy.getSettled(
+        `.widget-box.edit-active${interactionSelector} .image-editor li[data-type="${shapeType}"]`
+    ).click();
+    cy.get(`.widget-box.edit-active${interactionSelector} .main-image-box image`)
+        .then($image => {
+            const coords = $image[0].getBoundingClientRect();
+            const pageX = Math.round(coords.left + coords.width / 2);
+            const pageY = Math.round(coords.top + coords.height / 2);
+            cy.wrap($image)
+                .trigger('mouseover', { force: true })
+                .trigger('mousedown', {
+                    which: 1,
+                    pageX,
+                    pageY,
+                    force: true
+                })
+                .trigger('mousemove', {
+                    which: 1,
+                    pageX: pageX + 20,
+                    pageY: pageY + 20,
+                    force: true
+                })
+                .trigger('mouseup', { force: true });
+        });
+}
