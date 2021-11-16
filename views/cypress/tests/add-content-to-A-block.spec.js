1936e8fff7aeaadd5e80ffa89d2cebc433c73815
Hanna Dzmitryieva <bziondik@gmail.com>
2021-11-16 15:47:40 +0300

    fix: loading video and audio

--- views/cypress/tests/add-content-to-A-block.spec.js
+++ views/cypress/tests/add-content-to-A-block.spec.js
@@ -19,71 +19,58 @@
 import urls from '../utils/urls';
 import selectors from '../utils/selectors';
 
-import {selectUploadLocalAsset} from "../utils/resource-manager";
-import {addAblock} from "../utils/authoring-add-interactions";
-import {editText} from "../utils/Asset-edit-text-Ablock";
+import { selectUploadLocalAsset } from '../utils/resource-manager';
+import { addAblock } from '../utils/authoring-add-interactions';
+import { editText } from '../utils/asset-edit-text-Ablock';
 
-import paths from "../utils/paths";
-
-
+import paths from '../utils/paths';
+import { getRandomNumber } from '../../../../tao/views/cypress/utils/helpers';
 
 describe('Item Authoring', () => {
-    const className = 'Test E2E class';
+    const className = `Test E2E class ${getRandomNumber()}`;
     const itemName = 'Test E2E item 1';
     const ablockContainerParagraph = '.widget-box[data-qti-class="_container"] p';
     const aBlockContainer = '.widget-box[data-qti-class="_container"]';
-
 
     /**
      * Log in
      * Visit the page
      */
     before(() => {
-        cy.loginAsAdmin();
+        cy.setup(
+            selectors.treeRenderUrl,
+            selectors.editClassLabelUrl,
+            urls.items,
+            selectors.root,
+            selectors.editItemUrl
+        );
 
-        cy.intercept('POST', '**/edit*').as('edit');
-        cy.intercept('POST', `**/${selectors.editClassLabelUrl}`).as('editClassLabel');
-        cy.viewport(1000, 660);
-        cy.visit(urls.items);
-        cy.wait('@edit');
-
-        cy.get(selectors.root).then(root => {
-            if (root.find(`li[title="${className}"] a`).length) {
-                cy.deleteClassFromRoot(
-                    selectors.root,
-                    selectors.itemClassForm,
-                    selectors.deleteClass,
-                    selectors.deleteConfirm,
-                    className,
-                    selectors.deleteClassUrl,
-                    selectors.resourceRelations,
-                    false,
-                    true
-                );
-            }
-        });
+        cy.addClassToRoot(
+            selectors.root,
+            selectors.itemClassForm,
+            className,
+            selectors.editClassLabelUrl,
+            selectors.treeRenderUrl,
+            selectors.addSubClassUrl
+        );
+        cy.addNode(selectors.itemForm, selectors.addItem);
+        cy.renameSelectedNode(selectors.itemForm, selectors.editItemUrl, itemName);
     });
 
     after(() => {
-        cy.intercept('POST', '**/edit*').as('edit');
+        cy.registerAliasForRequests(selectors.treeRenderUrl, selectors.editClassLabelUrl, selectors.editItemUrl);
         cy.visit(urls.items);
         cy.wait('@edit');
 
-        cy.get(selectors.root).then(root => {
-            if (root.find(`li[title="${className}"] a`).length) {
-                cy.deleteClassFromRoot(
-                    selectors.root,
-                    selectors.itemClassForm,
-                    selectors.deleteClass,
-                    selectors.deleteConfirm,
-                    className,
-                    selectors.deleteClassUrl,
-                    selectors.resourceRelations,
-                    false,
-                    true
-                );
-            }
-        });
+        cy.deleteClassFromRoot(
+            selectors.root,
+            selectors.itemClassForm,
+            selectors.deleteClass,
+            selectors.deleteConfirm,
+            className,
+            selectors.deleteClassUrl,
+            true
+        );
     });
 
     /**
@@ -91,17 +78,6 @@ describe('Item Authoring', () => {
      */
     describe('Item authoring', () => {
         it('can open item authoring', function () {
-            cy.addClassToRoot(
-                selectors.root,
-                selectors.itemClassForm,
-                className,
-                selectors.editClassLabelUrl,
-                selectors.treeRenderUrl,
-                selectors.addSubClassUrl
-            );
-            cy.addNode(selectors.itemForm, selectors.addItem);
-            cy.renameSelectedNode(selectors.itemForm, selectors.editItemUrl, itemName);
-
             cy.get(selectors.authoring).click();
             cy.location().should(loc => {
                 expect(`${loc.pathname}${loc.search}`).to.eq(urls.itemAuthoring);
@@ -117,53 +93,42 @@ describe('Item Authoring', () => {
             cy.getSettled(`${aBlockContainer}`).click();
             cy.get('[id="toolbar-top"]')
                 .find('[class="cke_button cke_button__taoqtiimage cke_button_off"]')
-                .click({force: true});
+                .click({ force: true });
+            cy.get('.resourcemgr.modal').should('be.visible');
             selectUploadLocalAsset(imageName, `${paths.assetsPath}${imageName}`).then(() => {
                 cy.log(`${paths.assetsPath}${imageName}`, 'IS ADDED');
             });
         });
         it('can add audio to A-block', () => {
             const audioName = 'sampleAudioSmall.mp3';
-            cy.getSettled(`${ablockContainerParagraph}`).click({force:true});
+            cy.getSettled(`${ablockContainerParagraph}`).click({ force: true });
             cy.get('[id="toolbar-top"]')
                 .find('[class="cke_button cke_button__taoqtimedia cke_button_off"]')
-                .click({force: true});
-            cy.getSettled(`${ablockContainerParagraph}`).click({force:true});
+                .click({ force: true });
+            cy.get('.resourcemgr.modal').should('be.visible');
             selectUploadLocalAsset(audioName, `${paths.assetsPath}${audioName}`).then(() => {
-                cy.get('div[class="previewer"]').should('exist');
+                cy.getSettled('.qti-object-container.previewer  div[data-type="audio/mpeg"]').should('have.length', 1);
                 cy.log(`${paths.assetsPath}${audioName}`, 'IS ADDED');
-                cy.get('a.tlb-button-off.select').last().click();
-                //close modal if it's still open
-                if (cy.get('button.modal-close#modal-close-btn')){
-                    cy.get('button.modal-close#modal-close-btn').click({force:true, multiple: true});
-                }
             });
-            cy.getSettled(`${ablockContainerParagraph}`).click({force:true});
+            cy.getSettled(`${ablockContainerParagraph}`).click({ force: true });
         });
         it('can add video to A-block', () => {
-                const videoName = 'sampleSmall.mp4';
-                 cy.getSettled(`${ablockContainerParagraph}`).click({force:true});
-                 cy.get('[id="toolbar-top"]')
-                     .find('[class="cke_button cke_button__taoqtimedia cke_button_off"]')
-                     .click({force:true});
-                cy.getSettled(`${ablockContainerParagraph}`).click({force:true});
-                selectUploadLocalAsset(videoName, `${paths.assetsPath}${videoName}`).then(() => {
-                    cy.get('div[class="previewer"]').should('exist');
-                    cy.get('a.tlb-button-off.select').last().click();
-                    cy.log(`${paths.assetsPath}${videoName}`, 'IS ADDED');
-                    //close modal if it's still open
-                    if (cy.get('button.modal-close#modal-close-btn')){
-                          cy.get('button.modal-close#modal-close-btn').click({force:true, multiple: true});
-                    }
-                });
-                cy.getSettled(`${ablockContainerParagraph}`).click({force:true});
+            const videoName = 'sampleSmall.mp4';
+            cy.getSettled(`${ablockContainerParagraph}`).click({ force: true });
+            cy.get('[id="toolbar-top"]')
+                .find('[class="cke_button cke_button__taoqtimedia cke_button_off"]')
+                .click({ force: true });
+            cy.get('.resourcemgr.modal').should('be.visible');
+            selectUploadLocalAsset(videoName, `${paths.assetsPath}${videoName}`).then(() => {
+                cy.log(`${paths.assetsPath}${videoName}`, 'IS ADDED');
+                cy.getSettled('.qti-object-container.previewer  div[data-type="video/mp4"]').should('have.length', 1);
             });
+            cy.getSettled(`${ablockContainerParagraph}`).click({ force: true });
+        });
         it('can save item with A block & content', () => {
             cy.intercept('POST', '**/saveItem*').as('saveItem');
-            cy.get('[data-testid="save-the-item"]').click({force: true});
+            cy.get('[data-testid="save-the-item"]').click({ force: true });
+            cy.wait('@saveItem').its('response.body').its('success').should('eq', true);
         });
-
     });
 });
-
-
