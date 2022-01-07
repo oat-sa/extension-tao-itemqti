0f75135fbdf0bce500ded02ec0639a45eafe0642
Silvia_Lozano <sylvilozano@gmail.com>
2022-01-07 15:32:11 +0100

    fix: Refactor2

--- views/js/qtiCreator/widgets/static/helpers/itemScrollingMethods.js
+++ views/js/qtiCreator/widgets/static/helpers/itemScrollingMethods.js
@@ -74,7 +74,7 @@ define(['jquery', 'util/typeCaster'], function ($, typeCaster) {
             const $form = widget.$form;
             let $wrapper =
                 wrapType === 'inner'
-                    ? widget.$container.children('[data-html-editable]').children(`.${wrapperTextCls}`)
+                    ? widget.$container.children('[data-html-editable]').first(`.${wrapperTextCls}`)
                     : widget.$container.parent(`.${wrapperIncludeCls}`);
 
             if (!$wrapper.length) {
