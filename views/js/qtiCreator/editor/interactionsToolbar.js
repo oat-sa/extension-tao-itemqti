87f75d8e2d8e2ff13a64156d9a727301fd2f1be5
maria.cano <maria.cano@taotesting.com>
2022-04-06 09:51:07 +0200

    fix: refactor layout to 50% width by default

--- views/js/qtiCreator/editor/interactionsToolbar.js
+++ views/js/qtiCreator/editor/interactionsToolbar.js
@@ -74,20 +74,6 @@ define([
 
         _.each(interactions, function(interactionAuthoringData){
             add($sidebar, interactionAuthoringData);
-
-            const $interactionContainer = $(`li[data-qti-class="${interactionAuthoringData.qtiClass}"]`);
-            const $interactionText = $interactionContainer.find('div');
-
-            const interactionContainerWidth = $interactionContainer.width();
-            const interactionTextWidth = $interactionText.outerWidth(true);
-
-            if (interactionTextWidth >= interactionContainerWidth) {
-                const fontSize = 100 * (interactionContainerWidth / interactionTextWidth);
-                $interactionText.css(
-                    'fontSize',
-                    `${fontSize}%`
-                );
-            }
         });
 
         buildSubGroups($sidebar);
