0b80c8bf8132b9475eb8ad35aeb56b3134b4ac76
Hanna Dzmitryieva <bziondik@gmail.com>
2022-04-26 17:40:03 +0300

    fix: set cursor

--- views/js/qtiCreator/widgets/choices/inlineChoice/states/Question.js
+++ views/js/qtiCreator/widgets/choices/inlineChoice/states/Question.js
@@ -75,10 +75,9 @@ define([
             .on('input.qti-widget', function (e) {
                 if (e.originalEvent.inputType === 'insertFromPaste') {
                     // save range and calculate offset for cursor
-                    let range;
                     let offset;
                     if (window.getSelection) {
-                        range = window.getSelection().getRangeAt(0);
+                        const range = window.getSelection().getRangeAt(0);
                         const preCaretRange = range.cloneRange();
                         preCaretRange.selectNodeContents(this);
                         preCaretRange.setEnd(range.endContainer, range.endOffset);
@@ -87,8 +86,13 @@ define([
                     // clean format of paste text
                     $(this).html($(this).text());
                     // set cursor after inserted text
-                    if (range) {
+                    if (offset) {
+                        const range = document.createRange();
+                        const sel = window.getSelection();
                         range.setStart(this.childNodes[0], offset);
+                        range.collapse(true);
+                        sel.removeAllRanges();
+                        sel.addRange(range);
                     }
                 }
             });
