8d19f950a3565504172c37cdcffc4150cdeece6c
Héctor Arroyo <hector.arroyo@taotesting.com>
2022-06-20 12:23:07 +0200

    chore: Revert coding style fixes in OntologyMetadataInjector

--- model/qti/metadata/ontology/OntologyMetadataInjector.php
+++ model/qti/metadata/ontology/OntologyMetadataInjector.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2014-2022 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ * Copyright (c) 2014 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
  *
  */
 
@@ -149,7 +149,7 @@ class OntologyMetadataInjector implements MetadataInjector
         if (isset($rules[$pathKey]) === true) {
             return $rules[$pathKey][0];
         }
-
+        
         return false;
     }
 }
