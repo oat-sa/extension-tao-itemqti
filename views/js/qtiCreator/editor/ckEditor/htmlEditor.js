a6136a6013ae3ec8af57f93b7ef29fb6248d5824
Yuriy Synyaiev <yuriy.synyaiev@taotesting.com>
2022-07-18 16:09:43 +0200

    chore: code style

--- views/js/qtiCreator/editor/ckEditor/htmlEditor.js
+++ views/js/qtiCreator/editor/ckEditor/htmlEditor.js
@@ -52,7 +52,7 @@ define([
     };
 
     const placeholderClass = 'cke-placeholder';
-    const languagePluginEnabled = features.isVisible('taoQtiItem/creator/editor/ckEditor/languagePlugin' , false);
+    const languagePluginEnabled = features.isVisible('taoQtiItem/creator/editor/ckEditor/languagePlugin', false);
 
     let editorFactory;
 
