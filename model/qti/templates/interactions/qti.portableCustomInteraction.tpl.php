903675cb7ab74f2dee9b9cb558da104f44a2c283
Camille Moyon <camille@taotesting.com>
2023-01-06 16:22:16 +0100

    fix: allow to inject new namespace for PCI

--- model/qti/templates/interactions/qti.portableCustomInteraction.tpl.php
+++ model/qti/templates/interactions/qti.portableCustomInteraction.tpl.php
@@ -1,5 +1,5 @@
 <?php
-/*
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -14,13 +14,13 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  * 
- * Copyright (c) 2013 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
- *               
- * 
+ * Copyright (c) 2013-2023 (original work) Open Assessment Technologies SA (under the project TAO-PRODUCT);
+ *
  */
 ?>
+
 <customInteraction <?=get_data('attributes')?>>
-    <portableCustomInteraction customInteractionTypeIdentifier="<?=get_data('typeIdentifier')?>" hook="<?=get_data('entryPoint')?>" version="<?=get_data('version')?>" xmlns="http://www.imsglobal.org/xsd/portableCustomInteraction">
+    <portableCustomInteraction xmlns="<?=get_data('xmlns')?>" customInteractionTypeIdentifier="<?=get_data('typeIdentifier')?>" hook="<?=get_data('entryPoint')?>" version="<?=get_data('version')?>">
         <resources>
             <?php if(count(get_data('libraries'))):?><libraries>
                 <?php foreach(get_data('libraries') as $lib):?>
