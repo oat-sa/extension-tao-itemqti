373984551213a5cccfcefd8a5367f05bd73d5765
Janos Pribelszki <janos.pribelszki@taotesting.com>
2023-08-22 17:49:42 +0200

    fix: added unit tests

---
+++ test/unit/model/qti/datatype/LanguageValidateLanguageCodesTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace oat\taoQtiItem\test\unit\model\qti\datatype;
+
+use oat\generis\test\TestCase;
+use oat\taoQtiItem\model\qti\datatype\Language;
+
+class LanguageValidateLanguageCodesTest extends TestCase
+{
+    private const LANGUAGE_CODES_JSON_PATH = __DIR__ . '/../../../../resources/pisa-qa-translation-locales.json';
+
+    /**
+     * @dataProvider provideLanguageCodes
+     */
+    public function testValidate(string $languageCode): void
+    {
+        self::assertSame(1, Language::validate($languageCode));
+    }
+
+    public function provideLanguageCodes()
+    {
+        return json_decode(file_get_contents(realpath(self::LANGUAGE_CODES_JSON_PATH)), true);
+    }
+}
