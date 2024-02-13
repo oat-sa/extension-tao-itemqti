293c4833b81acf6100d8f0d327c1c0827fefd7ee
Martin Nicholson <martin@taotesting.com>
2024-02-13 20:09:11 +0100

    feat: define qtiCreator config for new install

--- config/default/qtiCreator.conf.php
+++ config/default/qtiCreator.conf.php
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2014 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2014-2024 (original work) Open Assessment Technologies SA;
  *
  *
  */
@@ -23,4 +23,9 @@
 /**
  * Default qtiCreator config
  */
-return ['multi-column' => false, 'scrollable-multi-column' => false, 'perInteractionRp' => false];
+return [
+    'multi-column' => false,
+    'scrollable-multi-column' => false,
+    'separator-between-columns' => false,
+    'perInteractionRp' => false
+];
