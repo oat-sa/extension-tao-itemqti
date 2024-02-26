16697e0bef984e170491aec45bbc3211da437fa3
Martin Nicholson <martin@taotesting.com>
2024-02-26 15:38:50 +0100

    refactor: event - modernise code style

--- views/js/portableLib/OAT/util/event.js
+++ views/js/portableLib/OAT/util/event.js
@@ -1,20 +1,35 @@
-define(['taoQtiItem/portableLib/OAT/util/EventMgr'], function(EventMgr){
-
+/*
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2017-2024 (original work) Open Assessment Technologies SA;
+ *
+ */
+define(['taoQtiItem/portableLib/OAT/util/EventMgr'], function(EventMgr) {
     return {
-        addEventMgr : function(instance){
+        addEventMgr: function(instance) {
+            const eventMgr = new EventMgr();
 
-            var eventMgr = new EventMgr();
-
-            instance.on = function on(event, callback){
+            instance.on = (event, callback) => {
                 eventMgr.on(event, callback);
             };
-            instance.off = function off(event){
+            instance.off = event => {
                 eventMgr.off(event);
             };
-            instance.trigger = function trigger(event, data){
+            instance.trigger = (event, data) => {
                 eventMgr.trigger(event, data);
             };
-
         }
     };
 });
