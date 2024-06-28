f74e13c57d66be368fca4d7e2165211add8eacf3
jsconan <jsconan@gmail.com>
2024-06-28 16:20:03 +0200

    feat: support multiple preview buttons in the item creator

--- views/js/qtiCreator/plugins/menu/preview.js
+++ views/js/qtiCreator/plugins/menu/preview.js
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2016-2019 (original work) Open Assessment Technologies SA ;
+ * Copyright (c) 2016-2024 (original work) Open Assessment Technologies SA ;
  */
 
 /**
@@ -36,20 +36,37 @@ define([
     'use strict';
 
     /**
+     * We assume the ClientLibConfigRegistry is filled up with something like this:
+     * 'taoQtiItem/qtiCreator/plugins/menu/preview' => [
+     *     'provider' => 'qtiItem',
+     * ],
+     *
+     * Or, with something like this for allowing multiple buttons in case of several providers are available:
+     * 'taoQtiItem/qtiCreator/plugins/menu/preview' => [
+     *     'provider' => 'qtiItem',
+     *     'providers' => [
+     *         ['id' => 'qtiItem', 'label' => 'Preview'],
+     *         ['id' => 'xxxx', 'label' => 'xxxx'],
+     *         ...
+     *     ],
+     * ],
+     */
+
+    /**
      * Handler for preview
      * @param {Object} e - Preview event fired
      * @param {Object} plugin - Context of preview
      */
-    function previewHandler(e, plugin) {
-        if (!plugin.$element.hasClass('disabled')) {
+    function previewHandler(e, plugin, provider) {
+        console.log(e, plugin, provider)
+        if (!$(e.currentTarget).hasClass('disabled')) {
             const itemCreator = plugin.getHost();
             $(document).trigger('open-preview.qti-item');
             e.preventDefault();
             plugin.disable();
-            itemCreator.trigger('preview', itemCreator.getItem().data('uri'));
+            itemCreator.trigger('preview', itemCreator.getItem().data('uri'), provider);
             plugin.enable();
         }
-
     }
 
     /**
@@ -83,17 +100,17 @@ define([
          * Initialize the plugin (called during itemCreator's init)
          * @fires {itemCreator#preview}
          */
-        init : function init(){
+        init(){
             const itemCreator = this.getHost();
+            const config = module.config();
 
             /**
              * Preview an item
              * @event itemCreator#preview
              * @param {String} uri - the uri of this item to preview
              */
-            itemCreator.on('preview', function(uri) {
-                const config = module.config();
-                const type = config.provider || 'qtiItem';
+            itemCreator.on('preview', function(uri, provider) {
+                const type = provider || config.provider || 'qtiItem';
 
                 if (!this.isEmpty()) {
                     previewerFactory(type, uri, {}, {
@@ -106,14 +123,18 @@ define([
 
             itemCreator.on('saved', () => enablePreviewIfNotEmpty(this));
 
+            const createButton = ({id, label, title} = {}) => {
+                return $(buttonTpl({
+                    icon: 'preview',
+                    title: title || __('Preview the item'),
+                    text : label || __('Preview'),
+                    cssClass: 'preview-trigger',
+                    testId: 'preview-the-item'
+                })).on('click', e => previewHandler(e, this, id))
+            }
+
             //creates the preview button
-            this.$element = $(buttonTpl({
-                icon: 'preview',
-                title: __('Preview the item'),
-                text : __('Preview'),
-                cssClass: 'preview-trigger',
-                testId: 'preview-the-item'
-            })).on('click', e => previewHandler(e, this));
+            this.elements = config.providers ? config.providers.map(createButton) : [createButton()];
 
             this.getAreaBroker()
                 .getItemPanelArea()
@@ -123,51 +144,48 @@ define([
         /**
          * Initialize the plugin (called during itemCreator's render)
          */
-        render : function render(){
-
+        render(){
             //attach the element to the menu area
             const $container = this.getAreaBroker().getMenuArea();
             if (this.getHost().isEmpty()) {
                 this.disable();
             }
-            $container.append(this.$element);
+            this.elements.forEach($element => $container.append($element));
         },
 
         /**
          * Called during the itemCreator's destroy phase
          */
-        destroy : function destroy (){
-            this.$element.remove();
+        destroy() {
+            this.elements.forEach($element => $element.remove());
         },
 
         /**
          * Enable the button
          */
-        enable : function enable (){
-            this.$element.removeProp('disabled')
-                         .removeClass('disabled');
+        enable() {
+            this.elements.forEach($element => $element.removeProp('disabled').removeClass('disabled'));
         },
 
         /**
          * Disable the button
          */
-        disable : function disable (){
-            this.$element.prop('disabled', true)
-                         .addClass('disabled');
+        disable() {
+            this.elements.forEach($element => $element.prop('disabled', true).addClass('disabled'));
         },
 
         /**
          * Show the button
          */
-        show: function show(){
-            hider.show(this.$element);
+        show(){
+            this.elements.forEach($element => hider.show($element));
         },
 
         /**
          * Hide the button
          */
-        hide: function hide(){
-            hider.hide(this.$element);
+        hide() {
+            this.elements.forEach($element => hider.hide($element));
         }
     });
 });
