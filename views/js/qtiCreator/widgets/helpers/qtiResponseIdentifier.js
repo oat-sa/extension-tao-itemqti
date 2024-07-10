31978d1ab706a235fcdac3513ab7a8f3e2440803
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2024-07-10 15:42:58 +0200

    fix: llicnece update

--- views/js/qtiCreator/widgets/helpers/qtiResponseIdentifier.js
+++ views/js/qtiCreator/widgets/helpers/qtiResponseIdentifier.js
@@ -13,7 +13,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2022 (original work) Open Assessment Technologies SA
+ * Copyright (c) 2024 (original work) Open Assessment Technologies SA
  *
  */
 define(['module', 'i18n'], function (module, __) {
