82cbafa0603a77104a1ce96db47833b4a2380b74
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2025-04-01 08:30:47 +0200

    fix: secure from empty values

--- views/js/qtiCreator/widgets/interactions/mediaInteraction/Widget.js
+++ views/js/qtiCreator/widgets/interactions/mediaInteraction/Widget.js
@@ -63,10 +63,11 @@ define([
             //disabled autoplay in authoring
             var autostart = interaction.attributes.autostart;
             interaction.attributes.autostart = false;
-            interaction.object.metaData.transcriptionUrl = this.options.resourceMetadataUrl +
-                '?metadataUri=' + encodeURIComponent(this.options.transcriptionMetadata) +
-                '&resourceUri=' + interaction.object.attr('data').replace('taomedia://mediamanager/', '');
-
+            if (interaction.object.attr('data')) {
+                interaction.object.metaData.transcriptionUrl = this.options.resourceMetadataUrl +
+                    '?metadataUri=' + encodeURIComponent(this.options.transcriptionMetadata) +
+                    '&resourceUri=' + interaction.object.attr('data').replace('taomedia://mediamanager/', '');
+            }
             commonRenderer.render.call(interaction.getRenderer(), interaction, {
                 features : 'full',
                 controlPlaying : false
