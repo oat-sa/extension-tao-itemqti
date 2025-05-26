2ec88048a8265331bc1e00a1c2904ba5ce953923
Andrei Shapiro <shpr.andrew@gmail.com>
2025-05-26 14:45:36 +0300

    chore: sync unique ID

--- model/qti/Service.php
+++ model/qti/Service.php
@@ -27,7 +27,7 @@ use oat\oatbox\filesystem\File;
 use oat\oatbox\filesystem\FilesystemException;
 use oat\tao\model\featureFlag\FeatureFlagChecker;
 use oat\tao\model\featureFlag\FeatureFlagCheckerInterface;
-use oat\taoQtiItem\model\qti\exception\QtiModelException;
+use oat\tao\model\TaoOntology;
 use oat\taoQtiItem\model\qti\parser\XmlToItemParser;
 use tao_helpers_Uri;
 use common_exception_FileSystemError;
@@ -146,13 +146,7 @@ class Service extends ConfigurableService
      */
     public function saveDataItemToRdfItem(Item $qtiItem, core_kernel_classes_Resource $rdfItem)
     {
-        if ($this->getFeatureFlagChecker()->isEnabled('FEATURE_FLAG_UNIQUE_NUMERIC_QTI_IDENTIFIER')) {
-            $currentQtiItem = $this->getDataItemByRdfItem($rdfItem);
-
-            if ($currentQtiItem && $currentQtiItem->getIdentifier() !== $qtiItem->getIdentifier()) {
-                throw new QtiModelException('QTI identifier is unique and cannot be modified');
-            }
-        }
+        $this->syncUniqueId($qtiItem, $rdfItem);
 
         $label = mb_substr($rdfItem->getLabel(), 0, 256, 'UTF-8');
         //set the current data lang in the item content to keep the integrity
@@ -343,6 +337,19 @@ class Service extends ConfigurableService
     public static function singleton()
     {
         return ServiceManager::getServiceManager()->get(self::class);
+    }
+
+    private function syncUniqueId(Item $qtiItem, core_kernel_classes_Resource $rdfItem): void
+    {
+        if (!$this->getFeatureFlagChecker()->isEnabled('FEATURE_FLAG_UNIQUE_NUMERIC_QTI_IDENTIFIER')) {
+            return;
+        }
+
+        $uniqueId = (string) $rdfItem->getOnePropertyValue($this->getProperty(TaoOntology::PROPERTY_UNIQUE_IDENTIFIER));
+
+        if (!empty($uniqueId) && $uniqueId !== $qtiItem->getIdentifier()) {
+            $qtiItem->setIdentifier($uniqueId);
+        }
     }
 
     private function getItemService(): taoItems_models_classes_ItemsService
