08693a89fb91fc8eee9481650c41105a493df720
Alexey Radyuk <alexey.radyuk@gmail.com>
2025-08-06 17:09:20 +0300

    fix: copyright

--- test/unit/model/qti/ParserFactoryTest.php
+++ test/unit/model/qti/ParserFactoryTest.php
@@ -1,6 +1,6 @@
 <?php
 
-/**
+/*
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -15,7 +15,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  *
- * Copyright (c) 2022 (original work) Open Assessment Technologies SA;
+ * Copyright (c) 2025 (original work) Open Assessment Technologies SA;
  */
 
 declare(strict_types=1);
@@ -188,7 +188,6 @@ class ParserFactoryTest extends TestCase
 
         $this->assertInstanceOf(TemplatesDriven::class, $response);
         $this->assertTrue($parser->test_isCalled_buildIsNullFeedbackRule);
-
     }
 
     /**
