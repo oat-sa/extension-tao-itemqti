10848a3133e6ce8b9ff399c9df6aa7b1518d4f13
Karol Stelmaczonek <karol.stelmaczonek@taotesting.com>
2025-11-12 16:48:21 +0100

    fix: move wrapping multimedia to dedicated interactions

--- views/js/qtiXmlRenderer/renderers/interactions/GapMatchInteraction.js
+++ views/js/qtiXmlRenderer/renderers/interactions/GapMatchInteraction.js
@@ -1,6 +1,15 @@
-define(['tpl!taoQtiItem/qtiXmlRenderer/tpl/interactions/blockInteraction'], function(tpl){
+define([
+    'taoQtiItem/qtiXmlRenderer/renderers/Container',
+    'tpl!taoQtiItem/qtiXmlRenderer/tpl/interactions/blockInteraction'
+], function(ContainerRenderer, tpl){
     return {
-        qtiClass : 'gapMatchInteraction',
-        template : tpl
+        qtiClass: 'gapMatchInteraction',
+        template: tpl,
+        getData: function(interaction, data) {
+            if (data.body && typeof ContainerRenderer.wrapMediaElements === 'function') {
+                data.body = ContainerRenderer.wrapMediaElements(data.body);
+            }
+            return data;
+        }
     };
 });
