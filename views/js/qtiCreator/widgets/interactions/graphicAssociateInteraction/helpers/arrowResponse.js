3cbbd88daaf697ec2e5dce9f4084a8e12a74cd90
Karol Stelmaczonek <karol.stelmaczonek@taotesting.com>
2026-02-26 13:10:15 +0100

    fix: update comments

--- views/js/qtiCreator/widgets/interactions/graphicAssociateInteraction/helpers/arrowResponse.js
+++ views/js/qtiCreator/widgets/interactions/graphicAssociateInteraction/helpers/arrowResponse.js
@@ -1,4 +1,4 @@
-/*
+/**
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License
  * as published by the Free Software Foundation; under version 2
@@ -11,10 +11,9 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * Foundation, Inc., 31 Milk St # 960789 Boston, MA 02196 USA.
  *
- * Copyright (c) 2015-2022 (original work) Open Assessment Technologies SA;
- *
+ * Copyright (c) 2026 (original work) Open Assessment Technologies SA;
  */
 
 define([
