35bcca6c63d9557f6ffc152b76eed1df1ab0e6c5
Bartłomiej Marszał <bartlomiejmarszal@gmail.com>
2026-07-08 16:06:57 +0200

    chore: update GPL license headers to Boston Milk St address
    Align NIV-271 touched files with the current FSF mailing address and add
    missing headers to new UMFI XML renderer unit tests.

--- views/js/qtiCreator/itemCreator.js
+++ views/js/qtiCreator/itemCreator.js
@@ -11,7 +11,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
- * 31 Milk St # 960789 Boston, MA 02196 USA.
+ * Foundation, Inc., 31 Milk St # 960789 Boston, MA 02196 USA.
  *
  * Copyright (c) 2016-2026 (original work) Open Assessment Technologies SA ;
  *
