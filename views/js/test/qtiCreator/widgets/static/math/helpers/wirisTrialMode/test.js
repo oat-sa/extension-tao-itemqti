2d883d7a7a5322ff331f64aa2254f4e93a805a25
KirylHatalski <kirill.gatalsky@taotesting.com>
2026-08-11 15:28:24 +0300

    chore: restore GPL license headers for PCR-346 files

--- views/js/test/qtiCreator/widgets/static/math/helpers/wirisTrialMode/test.js
+++ views/js/test/qtiCreator/widgets/static/math/helpers/wirisTrialMode/test.js
@@ -1,8 +1,19 @@
 /**
- * SPDX-FileCopyrightText: 2026-2026 Open Assessment Technologies S.A.
- * Copyright (C) 2026 (original work) Open Assessment Technologies S.A.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; under version 2
+ * of the License (non-upgradable).
  *
- * SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-TAO-Commercial-License
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ *
+ * Copyright (c) 2026 (original work) Open Assessment Technologies SA;
  */
 define([
     'taoQtiItem/qtiCreator/widgets/static/math/helpers/wirisTrialMode'
